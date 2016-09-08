468be9824917cb4914d4769f67eefc11550193dc
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-09-08 09:53:45 +0300

    enable production mode for builds

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -2,15 +2,15 @@ import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { FORM_DIRECTIVES }   from '@angular/forms';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { HTTP_PROVIDERS, Http } from '@angular/http';
-//import { enableProdMode } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { enableProdMode } from '@angular/core';
 import { PLATFORM_PIPES } from '@angular/core';
 
 import {TRANSLATE_PROVIDERS, TranslatePipe } from 'ng2-translate/ng2-translate';
 
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { NewsApi, UserService } from './app/shared'
+import { UserService } from './app/shared'
 import {PersonTokenApi} from "./app/shared/api/PersonTokenApi";
 import {PersonApi} from "./app/shared/api/PersonApi";
 import {MetadataApi} from "./app/shared/api/MetadataApi";
@@ -19,7 +19,9 @@ import {WarehouseValueMappingService} from "./app/shared/service/warehouse-value
 import {WarehouseApi} from "./app/shared/api/WarehouseApi";
 import {LabelPipe} from "./app/shared/pipe/label.pipe";
 
-//enableProdMode();
+if (process.env.NODE_ENV === 'production') {
+  enableProdMode();
+}
 
 bootstrap(AppComponent, [
   disableDeprecatedForms(),
