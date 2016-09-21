526374896c013070ad796d2b2d7c38e9058af155
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-09-21 09:10:14 +0300

    remove lang change listener from news list component on destroy

--- src/app/shared/news-list/news-list.component.ts
+++ src/app/shared/news-list/news-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { TranslateService } from 'ng2-translate/ng2-translate';
 
 import { NewsApi } from "../api/NewsApi";
 import { News } from "../model/News";
 import { PagedResult } from "../model/PagedResult";
+import {Subscription} from "rxjs";
 
 @Component({
   selector:'laji-news-list',
@@ -11,16 +12,19 @@ import { PagedResult } from "../model/PagedResult";
   styleUrls: ['./news-list.component.css'],
   providers: [ ]
 })
-export class NewsListComponent {
+export class NewsListComponent implements OnInit, OnDestroy{
   private pageSize = '5';
   private currentPage = 1;
 
   public news:PagedResult<News>;
 
+  private subLang:Subscription;
+  private subNews:Subscription;
+
   constructor(private newsService: NewsApi, private translate:TranslateService) {}
 
   ngOnInit() {
-    this.translate.onLangChange.subscribe(
+    this.subLang = this.translate.onLangChange.subscribe(
       () => {
         this.currentPage = 1;
         this.initNews()
@@ -29,16 +33,27 @@ export class NewsListComponent {
     this.initNews();
   }
 
+  ngOnDestroy() {
+    if (this.subNews) {
+      this.subNews.unsubscribe();
+    }
+    this.subLang.unsubscribe();
+  }
+
   gotoPage(page:number):void {
     this.currentPage = page;
     this.initNews();
   }
 
   private initNews() {
-    this.newsService.findAll(this.translate.currentLang, '' + this.currentPage, this.pageSize).subscribe(
-      news => { this.news = news },
-      err => console.log(err)
-    );
+    if (this.subNews) {
+      this.subNews.unsubscribe();
+    }
+    this.subNews = this.newsService
+      .findAll(this.translate.currentLang, '' + this.currentPage, this.pageSize)
+      .subscribe(
+        news => { this.news = news },
+        err => console.log(err)
+      );
   }
-
 }
