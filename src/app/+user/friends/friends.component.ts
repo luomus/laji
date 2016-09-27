c266f05420059dbff8ea91978f110daf322c6bb8
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-09-27 14:39:55 +0300

    profile fine tuning

--- src/app/+user/friends/friends.component.ts
+++ src/app/+user/friends/friends.component.ts
@@ -1,19 +1,21 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, OnChanges} from '@angular/core';
 import {Profile} from "../../shared/model/Profile";
 import {UserService} from "../../shared/service/user.service";
 import {PersonApi} from "../../shared/api/PersonApi";
+import {Observer, Observable} from "rxjs";
 
 @Component({
   selector: 'friends',
   templateUrl: 'friends.component.html'
 })
-export class FriendsComponent implements OnInit {
+export class FriendsComponent implements OnInit, OnChanges {
 
   @Input() profile:Profile;
   @Input() usersProfile:Profile;
 
   public user;
   public requestSend = false;
+  public friends = [];
 
   constructor(
     private userService:UserService,
@@ -23,6 +25,11 @@ export class FriendsComponent implements OnInit {
 
   ngOnInit() {
     this.requestSend = false;
+    this.initFriends();
+  }
+
+  ngOnChanges() {
+    this.initFriends();
   }
 
   isLoggedIn() {
@@ -31,6 +38,21 @@ export class FriendsComponent implements OnInit {
 
   isCurrentUser() {
     return this.profile.userID === this.usersProfile.userID
+  }
+
+  alreadyFriends() {
+    return this.usersProfile.friends && this.usersProfile.friends.indexOf(this.profile.userID) > -1;
+  }
+
+  initFriends() {
+    if (!this.usersProfile.friends || this.usersProfile.friends.length == 0) {
+      return;
+    }
+    let requests = [];
+    this.usersProfile.friends.map(qname => {
+      requests.push(this.personService.personFindByUserId(qname));
+    });
+    return Observable.forkJoin(requests).subscribe(data => this.friends = data)
   }
 
   sendFriendRequest(profileKy:string) {
