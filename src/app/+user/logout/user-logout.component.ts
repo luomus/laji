0277d239442a3dede54c13b62fdf4056e0e68b58
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-10-11 09:40:19 +0300

    remove logout text and redirect to front

--- src/app/+user/logout/user-logout.component.ts
+++ src/app/+user/logout/user-logout.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'laji-logout',
   templateUrl: 'user-logout.component.html'
 })
 export class UserLogoutComponent implements OnInit {
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
   }
 
   ngOnInit() {
     if (this.userService.isLoggedIn()) {
       this.userService.logout();
     }
+    this.router.navigate(['/'], {
+      queryParams: {}
+    });
   }
 
 }
