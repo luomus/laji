ded2e97c1927a5123b0a8864e534865990751b6f
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-11-18 16:18:31 +0200

    add delay on sending error message

--- src/app/shared/error/laji-error-handler.ts
+++ src/app/shared/error/laji-error-handler.ts
@@ -2,16 +2,26 @@ import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { ToastsService } from '../service/toasts.service';
 import { TranslateService } from 'ng2-translate';
 
+const pauseBeforeResendError = 1000;
+
 @Injectable()
 export class LajiErrorHandler implements ErrorHandler {
 
   private toastsService;
   private translate;
+  private pause = false;
 
   constructor(private injector: Injector) {}
 
   handleError(error) {
     console.log(error);
+    if (this.pause) {
+      return;
+    }
+    this.pause = true;
+    setTimeout(() => {
+      this.pause = false;
+    }, pauseBeforeResendError);
     this.getTranslateService()
       .get(['error.500.title', 'error.500.intro'])
       .subscribe(tranlations => {
