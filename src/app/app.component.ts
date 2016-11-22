22ee768c1210282c832fc52661822cc7694ccc92
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-11-22 07:38:02 +0200

    fix toasts problem introduced in ng v2.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewContainerRef } from '@angular/core';
 import { TaxonomyApi, CollectionApi } from './shared';
+import { ToastsManager } from 'ng2-toastr';
 
 @Component({
   selector: 'laji-app',
@@ -14,7 +15,8 @@ export class AppComponent {
 
   public viewContainerRef: ViewContainerRef;
 
-  constructor(viewContainerRef: ViewContainerRef) {
+  constructor(public toastr: ToastsManager, viewContainerRef: ViewContainerRef) {
     this.viewContainerRef = viewContainerRef;
+    this.toastr.setRootViewContainerRef(viewContainerRef);
   }
 }
