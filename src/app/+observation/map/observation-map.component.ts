b5fc19c27b549549b132a0af671002781161f2da
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-12-01 10:11:30 +0200

    fix map not reacting to coordinate accuracy when area is selected

--- src/app/+observation/map/observation-map.component.ts
+++ src/app/+observation/map/observation-map.component.ts
@@ -304,7 +304,7 @@ export class ObservationMapComponent implements OnInit, OnChanges {
     const count$ = this.warehouseService
       .warehouseQueryCountGet(query)
       .switchMap(cnt => {
-        if (typeof query.coordinates !== 'undefined') {
+        if (typeof query.coordinates !== 'undefined' && typeof query.coordinateAccuracyMax === 'undefined') {
           query.coordinateAccuracyMax = this.accuracy;
         }
         if (cnt.total < this.showItemsWhenLessThan) {
