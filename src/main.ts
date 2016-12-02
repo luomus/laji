9b3446aebd330a977533f277a93f04ed7204da8d
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-12-02 15:02:42 +0200

    remove un used import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
 import { AppModule } from './app/';
 const config = require('../config.json');
 
