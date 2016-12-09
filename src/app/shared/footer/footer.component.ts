bf76a80c9f8ad2e54364d31388c3824c2a485069
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-12-09 13:54:45 +0200

    [completes #126384147] Footerin toteutus

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { FooterService } from '../service/footer.service';
 
 @Component({
   selector: 'laji-footer',
+  styleUrls: ['./footer.component.css'],
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
