9e2285f5804129fb81b40db31874d561af2ddd5a
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-12-12 13:08:22 +0200

    [completes #135939515] url information -> about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { NotFoundComponent } from './shared/not-found/not-found.component';
 const routes: Routes = [
   {path: '', pathMatch: 'full', loadChildren: './+home/home.module#HomeModule'},
   {path: 'news', loadChildren: './+news/news.module#NewsModule'},
-  {path: 'information', loadChildren: './+information/information.module#InformationModule'},
+  {path: 'about', loadChildren: './+information/information.module#InformationModule'},
   {path: 'user', loadChildren: './+user/user.module#UserModule'},
   {path: 'vihko', loadChildren: './+haseka/haseka.module#HasekaModule'},
   {path: 'observation', loadChildren: './+observation/observation.module#ObservationModule'},
