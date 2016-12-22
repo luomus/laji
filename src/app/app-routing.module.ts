e43509f59bed6d7430b888494abf236ca7d37dfb
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2016-12-22 07:47:31 +0200

    wrap everything into one bundle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 const routes: Routes = [
