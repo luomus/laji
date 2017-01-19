2255024bcffbbe6e8766fbeff1e5ae359ce2f981
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-01-19 14:12:48 +0200

    work on the view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: []
 })
