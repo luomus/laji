933638ce07eb2846cf1b206e6cfa416e4412bdad
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-01-27 08:19:13 +0200

    fix qname pipe too eagerly splitting data

--- src/app/shared/pipe/to-qname.pipe.ts
+++ src/app/shared/pipe/to-qname.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { IdService } from '../service/id.service';
 
 /**
  * QName picker
@@ -12,9 +13,7 @@ export class ToQNamePipe implements PipeTransform {
   transform(value: string): any {
     if (Array.isArray(value)) {
       return value.map(val => this.transform(val));
-    } else if (!value || typeof value !== 'string') {
-      return value;
     }
-    return value.split('/').pop();
+    return IdService.getId(value);
   }
 }
