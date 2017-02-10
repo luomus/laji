5abdb89038b17a30627c3ee8e94dba16e9f37ee8
aorin <aorin@users.noreply.github.com>
2017-02-10 10:48:41 +0200

    merge np

--- src/app/+haseka/named-place/np-list/np-list.component.ts
+++ src/app/+haseka/named-place/np-list/np-list.component.ts
@@ -1,53 +1,20 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { NamedPlacesService } from '../named-places.service';
+import { Component, Input, Output, EventEmitter, OnChanges, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { NamedPlace } from '../../../shared/model/NamedPlace';
-import { FormService } from '../../form/form.service';
 
 @Component({
   selector: 'laji-np-list',
   templateUrl: './np-list.component.html',
   styleUrls: ['./np-list.component.css']
 })
-export class NpListComponent implements OnInit, OnChanges {
+export class NpListComponent {
 
-  @Input() collectionId: string;
-  @Input() formId: string;
+  @Input() namedPlaces: NamedPlace[];
+  @Output() onActivePlaceChange = new EventEmitter<NamedPlace>();
 
-  namedPlaces$: Observable<NamedPlace[]>;
+  constructor() { }
 
-  constructor(
-    private namedPlaceService: NamedPlacesService,
-    private formService: FormService
-  ) { }
-
-  ngOnInit() {
-    this.updateNP();
+  changeActivePlace(idx) {
+    this.onActivePlaceChange.emit(idx);
   }
-
-  ngOnChanges() {
-    this.updateNP();
-  }
-
-  updateNP() {
-    if (this.collectionId) {
-      this.namedPlaces$ = this.namedPlaceService
-        .getAllNamePlacesByCollectionId(this.collectionId)
-        .map(result => result.results);
-    }
-  }
-
-  populateForm(np: NamedPlace) {
-    this.formService.populate({
-      gatherings: [{
-        geometry: {
-          'type': 'GeometryCollection',
-          'geometries': [
-            np.geometry
-          ]
-        }
-      }]
-    });
-  }
-
 }
