93237116b1173552b0e387afceefd059aa5e8e64
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2017-02-10 19:50:58 +0200

    added viewer to invasive page

--- src/app/+invasive/invasive.module.ts
+++ src/app/+invasive/invasive.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { InvasiveComponent } from './invasive.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { routing } from './invasive.routes';
+import { ViewerModule } from '../+viewer/viewer.module';
 
 @NgModule({
   imports: [
-    routing, SharedModule, RouterModule
+    routing, SharedModule, RouterModule, ViewerModule
   ],
   declarations: [InvasiveComponent, InvasiveComponent]
 })
