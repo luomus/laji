cd5f226b3843aefb12fc490ede81a631c8d23c90
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-02-27 13:57:18 +0200

    update angular cli to rc.0

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -11,27 +11,35 @@ import 'core-js/es6/array';
 import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
-import 'core-js/es6/reflect';
-// import 'intl/index';
-// import 'intl/locale-data/jsonp/en.js';
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
+
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 // If you need to support the browsers/features below, uncomment the import
 // and run `npm install import-name-here';
 // Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
 
-// Needed for: IE9
-// import 'classlist.js';
-
 // Animations
 // Needed for: All but Chrome and Firefox, Not supported in IE9
 import 'web-animations-js';
 
-// Date, currency, decimal and percent pipes
-// Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
-import 'intl';
+/** Evergreen browsers require these. **/
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
 
-// NgClass on SVG elements
-// Needed for: IE10, IE11
-// import 'classlist.js';
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+/**
+ * Date, currency, decimal and percent pipes.
+ * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
+ */
+import 'intl';;
