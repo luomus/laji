9632aa2ba92fd9eb2fa500c196abcd996200356b
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-03-14 11:37:37 +0200

    cs fix

--- src/app/+taxonomy/info-card/info-card.component.ts
+++ src/app/+taxonomy/info-card/info-card.component.ts
@@ -18,13 +18,13 @@ export class InfoCardComponent implements OnInit, OnDestroy {
   public taxon: Taxonomy;
   public taxonDescription: Array<TaxonomyDescription>;
   public taxonImages: Array<TaxonomyImage>;
-  public activePanel: number = 0;
-  public activeImage: number = 1;
+  public activePanel = 0;
+  public activeImage = 1;
   public activeImageTab: string;
   public hasCollectionImages = true;
   public hasTaxonImages = true;
   public hasDescription = true;
-  public activeDescription: number = 0;
+  public activeDescription = 0;
 
   @Input() public taxonId: string;
 
