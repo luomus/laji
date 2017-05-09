fc1231600a6131ca0f70fa632fb23dd61e9c8a8f
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-05-09 10:19:52 +0300

    remove admin route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,6 @@ const routes: Routes = [
   {path: 'kartta', loadChildren: './+map/map.module#MapModule'},
   {path: 'map', loadChildren: './+map/map.module#MapModule'},
   {path: 'error', loadChildren: './+error/error.module#ErrorModule'},
-  {path: 'theme', loadChildren: './+theme/theme.module#ThemeModule'},
   {path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
   {path: 'nafi', redirectTo: '/theme/nafi', pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
