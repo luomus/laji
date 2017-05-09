2b8398eed70e4ce45f999238b3590ad6f596850c
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-05-09 09:19:17 +0300

    fix missing shcemaContext

--- src/app/shared/document-form/document-form.component.ts
+++ src/app/shared/document-form/document-form.component.ts
@@ -227,9 +227,10 @@ export class DocumentFormComponent implements AfterViewInit, OnChanges, OnDestro
             data.formData.id = undefined;
             data.formData.hasChanges = undefined;
           }
-          data.uiSchemaContext = {
-            formID: this.formId
-          };
+          if (typeof data.uiSchemaContext === 'undefined') {
+            data.uiSchemaContext = {};
+          }
+          data.uiSchemaContext.formID = this.formId;
           this.form = data;
           this.formService.hasUnsavedData()
             .subscribe(hasChanges => {
