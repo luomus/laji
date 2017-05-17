dd877e69eba3823b335e7475b72fc4cf21196fc1
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-05-17 15:03:47 +0300

    terms of service placeholder for english

--- src/app/+haseka/terms-of-service/terms-of-service.component.ts
+++ src/app/+haseka/terms-of-service/terms-of-service.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'terms-of-service',
   templateUrl: './terms-of-service-of-service.component.html'
 })
 export class HaSeKaTermsOfServiceComponent implements OnInit {
-  constructor() {
+  constructor(public translate: TranslateService) {
   }
 
   ngOnInit() {
