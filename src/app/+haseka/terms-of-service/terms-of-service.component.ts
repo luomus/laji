c14afe24346467b37f614f946215afba0c56c75d
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-05-18 10:07:01 +0300

    add style to terms-of-service page

--- src/app/+haseka/terms-of-service/terms-of-service.component.ts
+++ src/app/+haseka/terms-of-service/terms-of-service.component.ts
@@ -3,7 +3,8 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'terms-of-service',
-  templateUrl: './terms-of-service-of-service.component.html'
+  templateUrl: './terms-of-service-of-service.component.html',
+  styleUrls: ['./terms-of-service-of-service.component.css']
 })
 export class HaSeKaTermsOfServiceComponent implements OnInit {
   constructor(public translate: TranslateService) {
