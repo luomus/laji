159af349a102501d7352bbfaf15cd24915786983
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-06-07 19:55:27 +0300

    fix dev

--- src/app/+observation/map/observation-map.component.ts
+++ src/app/+observation/map/observation-map.component.ts
@@ -21,7 +21,7 @@ const maxCoordinateAccuracy = 10000;
   selector: 'laji-observation-map',
   templateUrl: './observation-map.component.html',
   styleUrls: ['./observation-map.component.css'],
-  providers: [ValueDecoratorService]
+  providers: [ValueDecoratorService, LabelPipe, ToQNamePipe, CollectionNamePipe]
 })
 export class ObservationMapComponent implements OnInit, OnChanges {
   @ViewChild(MapComponent) lajiMap: MapComponent;
