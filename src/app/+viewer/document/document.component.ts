9200fa86d23ac93a30ca3c2f29f7b0926b0f7981
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-06-09 09:43:19 +0300

    added interval to recheck for the document if nothing is found with the given id

--- src/app/+viewer/document/document.component.ts
+++ src/app/+viewer/document/document.component.ts
@@ -1,16 +1,17 @@
-import { AfterViewInit, Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnDestroy, ViewChild } from '@angular/core';
 import { WarehouseApi } from '../../shared/api/WarehouseApi';
 import { Observable } from 'rxjs/Observable';
 import { TriplestoreLabelService } from '../../shared/service/triplestore-label.service';
 import { ViewerMapComponent } from '../viewer-map/viewer-map.component';
 import { SessionStorage } from 'ng2-webstorage';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'laji-document',
   templateUrl: './document.component.html',
   styleUrls: ['./document.component.css']
 })
-export class DocumentComponent implements AfterViewInit, OnChanges {
+export class DocumentComponent implements AfterViewInit, OnChanges, OnDestroy {
   @ViewChild(ViewerMapComponent) map: ViewerMapComponent;
   @Input() uri: string;
   @Input() highlight: string;
@@ -21,35 +22,46 @@ export class DocumentComponent implements AfterViewInit, OnChanges {
   mapData: any = [];
   hasDoc: boolean;
   active = 0;
+  isViewInited = false;
   @SessionStorage() showFacts = false;
   private _uri: string;
+  private readonly recheckIterval = 10000; // check every 10sec if document not found
+  private interval: Subscription;
 
   constructor(private warehouseApi: WarehouseApi, private labelService: TriplestoreLabelService) { }
 
   ngAfterViewInit() {
+    this.isViewInited = true;
     this.updateDocument();
   }
 
   ngOnChanges() {
-    this.updateDocument();
+    this.hasDoc = undefined;
+    if (this.isViewInited) {
+      this.updateDocument();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      this.interval.unsubscribe();
+    }
   }
 
   updateDocument() {
-    this.hasDoc = undefined;
     if (!this.uri) {
-      this.parseDoc({}, false);
       return;
     }
     const findDox$ = this.uri === this._uri ?
       Observable.of(this.document) :
       this.labelService.get('MY.person')
         .switchMap(() => this.warehouseApi.warehouseQuerySingleGet(this.uri))
-        .map(doc => doc.document);
-    this._uri = this.uri;
+        .map(doc => doc.document)
+        .do(() => this._uri = this.uri);
     findDox$
       .subscribe(
-        doc => this.parseDoc(doc, true),
-        err => this.parseDoc({}, false)
+        doc => this.parseDoc(doc, doc),
+        err => this.parseDoc(undefined, false)
       );
   }
 
@@ -69,27 +81,36 @@ export class DocumentComponent implements AfterViewInit, OnChanges {
 
   private parseDoc(doc, found) {
     this.hasDoc = found;
-    this.document = doc;
-    this.mapData = [];
-    let activeIdx = 0;
-    if (doc && doc.gatherings) {
-      doc.gatherings.map((gathering, idx) => {
-        if (gathering.conversions && gathering.conversions.wgs84Geo) {
-          this.mapData[idx] = gathering.conversions.wgs84Geo;
-        }
-        if (this.highlight && gathering.gatheringId === this.highlight) {
-          activeIdx = idx;
-        }
-        if (gathering.units) {
-          gathering.units.map(unit => {
-            if (this.highlight && unit.unitId === this.highlight) {
-              activeIdx = idx;
-            }
-          });
-        }
-      });
+    if (found) {
+      this.document = doc;
+      this.mapData = [];
+      let activeIdx = 0;
+      if (doc && doc.gatherings) {
+        doc.gatherings.map((gathering, idx) => {
+          if (gathering.conversions && gathering.conversions.wgs84Geo) {
+            this.mapData[idx] = gathering.conversions.wgs84Geo;
+          }
+          if (this.highlight && gathering.gatheringId === this.highlight) {
+            activeIdx = idx;
+          }
+          if (gathering.units) {
+            gathering.units.map(unit => {
+              if (this.highlight && unit.unitId === this.highlight) {
+                activeIdx = idx;
+              }
+            });
+          }
+        });
+      }
+      this.setActive(activeIdx);
+      if (this.interval) {
+        this.interval.unsubscribe();
+      }
+    } else if (!this.interval) {
+      this.interval = Observable
+        .interval(this.recheckIterval)
+        .subscribe(() => this.updateDocument());
     }
-    this.setActive(activeIdx);
   }
 
 }
