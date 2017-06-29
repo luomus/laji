ed28ddf013ab6a52e62d1d39208429bac457008a
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-06-29 09:42:28 +0300

    tweaks to emk and ykj

--- src/app/+theme/emk/emk.component.ts
+++ src/app/+theme/emk/emk.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'laji-emk',
-  templateUrl: './emk.component.html'
+  templateUrl: './emk.component.html',
+  styles: [`
+    .alternative {
+      background-color: #fffacf;
+    }
+  `]
 })
 export class EmkComponent {
 
