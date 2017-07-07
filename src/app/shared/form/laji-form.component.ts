84183edc4342333c546888ed8e6df06ebc98d267
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-07-07 10:09:46 +0300

    debug settings

--- src/app/shared/form/laji-form.component.ts
+++ src/app/shared/form/laji-form.component.ts
@@ -100,7 +100,6 @@ export class LajiFormComponent implements OnDestroy, OnChanges, AfterViewInit {
     }
     this.userService.getUserSetting(this.settingsKey)
       .subscribe(settings => {
-        console.log(settings);
         try {
           this.ngZone.runOutsideAngular(() => {
             const uiSchemaContext = this.formData.uiSchemaContext || {};
@@ -124,6 +123,7 @@ export class LajiFormComponent implements OnDestroy, OnChanges, AfterViewInit {
               renderSubmit: false
             });
           });
+          console.log(settings);
         } catch (err) {
           this.logger.error('Failed to load lajiForm', err);
         }
