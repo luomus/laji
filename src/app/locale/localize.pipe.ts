6b6b7b5d856ed9b6444ddef80442c777e7fc376d
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2017-07-10 11:16:22 +0300

    add lang param to localize pipe and use them on land switch links

--- src/app/locale/localize.pipe.ts
+++ src/app/locale/localize.pipe.ts
@@ -1,8 +1,8 @@
 import { PipeTransform, Pipe, Injectable, ChangeDetectorRef } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/observable/forkJoin';
 import { TranslateService } from '@ngx-translate/core';
 import { LocalizeRouterService } from './localize-router.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 @Pipe({
@@ -13,11 +13,11 @@ export class LocalizePipe implements PipeTransform {
   private value: string | any[] = '';
   private lastKey: string | any[];
   private lastLanguage: string;
-  private subscription: Subscription;
 
   constructor(
     private localizeRouterService: LocalizeRouterService,
     private translateService: TranslateService,
+    private router: Router,
     private _ref: ChangeDetectorRef
   ) {
     this.translateService.onLangChange.subscribe(() => {
@@ -28,20 +28,28 @@ export class LocalizePipe implements PipeTransform {
   /**
    * Transform current url to localized one
    * @param query
+   * @param lang
    * @returns {string | any[]}
    */
-  transform(query: string | any[]): string | any[] {
-    if (!query || query.length === 0 || !this.translateService.currentLang) {
-      return query;
+  transform(query: string | any[], lang?: string): string | any[] {
+    if (!lang) {
+      lang = this.translateService.currentLang;
     }
-    if (query === this.lastKey && this.lastLanguage === this.translateService.currentLang) {
+    if (!query || query.length === 0 || !lang) {
+      if (Array.isArray(query)) {
+        query = this.localizeRouterService.getPathWithoutLocale(this.router.url);
+      } else {
+        return query;
+      }
+    }
+    if (query === this.lastKey && this.lastLanguage === lang) {
       return this.value;
     }
     this.lastKey = query;
-    this.lastLanguage = this.translateService.currentLang;
+    this.lastLanguage = lang;
 
     /** translate key and update values */
-    this.value = this.localizeRouterService.translateRoute(query);
+    this.value = this.localizeRouterService.translateRoute(query, lang);
     this.lastKey = query;
     this._ref.markForCheck();
 
