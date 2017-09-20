09bdee068e11f1369f12f4211f7170ae0e544a96
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-09-20 14:53:40 +0300

    more text translations and ignore own filters on download page

--- src/app/+observation/download/observation-download.component.ts
+++ src/app/+observation/download/observation-download.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { SearchQuery } from '../search-query.model';
 import { UserService } from '../../shared/service/user.service';
 import { TranslateService } from '@ngx-translate/core';
@@ -10,6 +10,7 @@ import { Util } from '../../shared/service/util.service';
 import { WarehouseQueryInterface } from '../../shared/model/WarehouseQueryInterface';
 import queryString from 'query-string';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs/Observable';
 
 enum RequestStatus {
   error = <any> 'error',
@@ -19,12 +20,16 @@ enum RequestStatus {
 
 @Component({
   selector: 'laji-observation-download',
-  templateUrl: './observation-download.component.html'
+  templateUrl: './observation-download.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ObservationDownloadComponent implements OnInit, OnDestroy {
 
   @Input() taxaLimit = 1000;
   @Input() loadLimit = 2000000;
+
+  _query: WarehouseQueryInterface;
+  hasPersonalData = false;
 
   public requests: {[place: string]: RequestStatus} = {};
   public requestStatus = RequestStatus;
@@ -42,7 +47,6 @@ export class ObservationDownloadComponent implements OnInit, OnDestroy {
     'sv': 'unit.linkings.taxon.speciesId,unit.linkings.taxon.speciesScientificName,unit.linkings.taxon.speciesNameSwedish'
   };
   private queryCache: string;
-  private subQueryUpdate: Subscription;
   private subLang: Subscription;
   private messages = {};
 
@@ -51,60 +55,75 @@ export class ObservationDownloadComponent implements OnInit, OnDestroy {
               public translate: TranslateService,
               private toastsService: ToastsService,
               private warehouseService: WarehouseApi,
-              private logger: Logger
+              private logger: Logger,
+              private cd: ChangeDetectorRef
   ) {
     if (environment.production) {
       this.showRequest = false;
     }
   }
 
+  @Input() set query(query: WarehouseQueryInterface) {
+    this.hasPersonalData = !!query.editorPersonToken || !!query.observerPersonToken;
+    const warehouseQuery: WarehouseQueryInterface = Util.clone(query);
+    if (warehouseQuery.editorPersonToken) {
+      delete warehouseQuery.editorPersonToken;
+    }
+    if (warehouseQuery.observerPersonToken) {
+      delete warehouseQuery.observerPersonToken;
+    }
+    console.log(warehouseQuery);
+    this._query = warehouseQuery;
+    const cacheKey = JSON.stringify(this._query);
+    if (this.queryCache !== cacheKey) {
+      this.queryCache = cacheKey;
+      this.requests = {};
+      this.updateCount();
+      this.updateCsvLink();
+    }
+  }
+
   ngOnInit() {
-    this.translate.get([
-      'observation.download.error',
-      'result.load.thanksPublic',
-      'result.load.thanksRequest'
-    ]).subscribe((translations) => {
-      this.messages = translations;
-    });
-    this.subLang = this.translate.onLangChange.subscribe(() => this.updateCsvLink());
-    this.subQueryUpdate = this.searchQuery.queryUpdated$.subscribe(
-      () => {
-        const cacheKey = JSON.stringify(this.searchQuery.query);
-        if (this.queryCache !== cacheKey) {
-          this.queryCache = cacheKey;
-          this.requests = {};
-          this.updateCount();
-          this.updateCsvLink();
-        }
-      }
-    );
-    this.updateCount();
-    this.updateCsvLink();
+    this.subLang = this.translate.onLangChange
+      .startWith({})
+      .switchMap(() => this.translate.get([
+          'observation.download.error',
+          'result.load.thanksPublic',
+          'result.load.thanksRequest'
+        ]))
+      .subscribe((translations) => {
+        this.messages = translations;
+        this.updateCsvLink();
+        this.cd.markForCheck();
+      });
   }
 
   ngOnDestroy() {
     if (this.subLang) {
       this.subLang.unsubscribe();
     }
-    if (this.subQueryUpdate) {
-      this.subQueryUpdate.unsubscribe();
-    }
   }
 
   updateCount() {
-    const secretQuery: WarehouseQueryInterface = Util.clone(this.searchQuery.query);
-    secretQuery.secured = true;
-    this.warehouseService.warehouseQueryCountGet(this.searchQuery.query)
-      .combineLatest(this.warehouseService.warehouseQueryCountGet(secretQuery),
-        (count, priva) => ({'count': count.total, 'private': priva.total}))
-      .subscribe(res => this.count = res);
+    const secretQuery: WarehouseQueryInterface = Util.clone(this._query);
+    const speciesQuery: WarehouseQueryInterface = Util.clone(this._query);
 
-    const speciesQuery: WarehouseQueryInterface = Util.clone(this.searchQuery.query);
+    secretQuery.secured = true;
     speciesQuery.taxonRankId = 'MX.species';
     speciesQuery.includeNonValidTaxa = false;
-    this.warehouseService.warehouseQueryAggregateGet(
-      speciesQuery, ['unit.linkings.taxon.id'], undefined, 1
-    ).subscribe(cnt => this.speciesCount = cnt.total);
+
+    Observable.forkJoin(
+      this.warehouseService.warehouseQueryCountGet(this._query)
+        .combineLatest(this.warehouseService.warehouseQueryCountGet(secretQuery),
+          (count, priva) => ({'count': count.total, 'private': priva.total})),
+      this.warehouseService.warehouseQueryAggregateGet(
+        speciesQuery, ['unit.linkings.taxon.id'], undefined, 1
+      )
+    ).subscribe((data) => {
+        this.count = data[0];
+        this.speciesCount = data[1] && data[1].total;
+        this.cd.markForCheck();
+    });
   }
 
   updateCsvLink() {
@@ -112,6 +131,12 @@ export class ObservationDownloadComponent implements OnInit, OnDestroy {
     queryParams['aggregateBy'] = this.taxaDownloadAggregateBy[this.translate.currentLang];
     queryParams['includeNonValidTaxa'] = 'false';
     queryParams['pageSize'] = this.taxaLimit;
+    if (queryParams['editorPersonToken']) {
+      delete queryParams['editorPersonToken'];
+    }
+    if (queryParams['observerPersonToken']) {
+      delete queryParams['observerPersonToken'];
+    }
     this.csvParams = queryString.stringify(queryParams);
   }
 
@@ -133,7 +158,7 @@ export class ObservationDownloadComponent implements OnInit, OnDestroy {
       this.userService.getToken(),
       'CSV_FLAT',
       'DOCUMENT_FACTS,GATHERING_FACTS,UNIT_FACTS',
-      this.searchQuery.query,
+      this._query,
       this.translate.currentLang
     ).subscribe(
       () => {
@@ -141,11 +166,13 @@ export class ObservationDownloadComponent implements OnInit, OnDestroy {
           'result.load.thanksPublic' : 'result.load.thanksRequest'
         ]);
         this.requests[type] = RequestStatus.done;
+        this.cd.markForCheck();
       },
       err => {
         this.requests[type] = RequestStatus.error;
         this.toastsService.showError(this.messages['observation.download.error']);
         this.logger.warn('Failed to make download request', err);
+        this.cd.markForCheck();
       }
     );
   }
