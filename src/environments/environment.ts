009a5673f7e919a37689fa5913b9d8e8fecd6b3a
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2017-09-29 11:11:44 +0300

    change the systemID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   forceLogin: false,
   disableAnalytics: true,
   forAuthorities: false,
-  systemID: 'KE.501',
+  systemID: 'KE.389',
   formWhitelist: ['JX.519', 'JX.652', 'JX.111715', 'MHL.1', 'JX.123759', 'JX.123757', 'JX.123659'],
   defaultForm: 'JX.519',
   nafiForm: 'JX.123648',
