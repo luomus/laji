adbd57afce97152cf1f73eeef0902faf2b52cced
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-10-03 08:32:25 +0300

    fix empty field fetching

--- src/app/shared-modules/observation-result/observation-table/observation-table.component.ts
+++ src/app/shared-modules/observation-result/observation-table/observation-table.component.ts
@@ -77,7 +77,7 @@ export class ObservationTableComponent implements OnInit, OnChanges {
       'unit.linkings.taxon.nameFinnish,' +
       'unit.linkings.taxon.nameEnglish,' +
       'unit.linkings.taxon.nameSwedish,' +
-      'unit.linkings.taxon.scientificName,',
+      'unit.linkings.taxon.scientificName',
       width: 300
     },
     { name: 'unit.taxonVerbatim',
