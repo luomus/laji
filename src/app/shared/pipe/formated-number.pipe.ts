d6fc33135f97c467a1e5debc5db41caae473b951
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2017-10-17 06:21:21 +0300

    [fixes #152003910] Luettelon individualCount sarake jättää näyttämättä arvon 0

--- src/app/shared/pipe/formated-number.pipe.ts
+++ src/app/shared/pipe/formated-number.pipe.ts
@@ -9,8 +9,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'formattedNumber'
 })
 export class FormattedNumber implements PipeTransform {
-  transform(value: string, thousandSeparator: string = ''): string {
-    if (!value) {
+  transform(value: string|number, thousandSeparator: string = ''): string {
+    if (typeof value !== 'number' && typeof value !== 'string') {
       return '';
     }
     return ('' + value).replace(/(\d)(?=(\d{3})+(\.\d*)?$)/g, '$1' + thousandSeparator);
