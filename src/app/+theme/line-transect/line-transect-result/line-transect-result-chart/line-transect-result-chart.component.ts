f0257afe50569f0f3b4abc0e9824e70d1d6563e2
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2018-07-12 11:31:13 +0300

    remove NaN from query

--- src/app/+theme/line-transect/line-transect-result/line-transect-result-chart/line-transect-result-chart.component.ts
+++ src/app/+theme/line-transect/line-transect-result/line-transect-result-chart/line-transect-result-chart.component.ts
@@ -75,7 +75,7 @@ export class LineTransectResultChartComponent implements OnInit, OnDestroy {
     this.router.navigate([], {queryParams: {
       taxonId: taxonId,
       birdAssociationAreas: birdAssociationAreas.join(','),
-      fromYear: fromYear
+      fromYear: fromYear || ''
     }});
   }
 
