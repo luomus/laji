5d71bac039b469f82cdd7420d295a56237a0aeb9
aorin <aorin@users.noreply.github.com>
2018-08-14 14:47:01 +0300

    use localized router links, add occurrenceFilters to router parameters

--- src/app/+taxonomy/taxon-browse/species-browse-observations/species-browse-observations.component.ts
+++ src/app/+taxonomy/taxon-browse/species-browse-observations/species-browse-observations.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router'
 import { TaxonomySearchQuery } from '../service/taxonomy-search-query';
+import { WarehouseQueryInterface } from '../../../shared/model/WarehouseQueryInterface';
+import { LocalizeRouterService } from '../../../locale/localize-router.service';
 
 @Component({
   selector: 'laji-species-browse-observations',
@@ -11,24 +13,27 @@ export class SpeciesBrowseObservationsComponent {
   @Input() searchQuery: TaxonomySearchQuery;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private localizeRouterService: LocalizeRouterService
   ) { }
 
   browse() {
     const query = this.searchQuery.query;
 
-    const parameters = {
+    const parameters: WarehouseQueryInterface = {
       informalTaxonGroupId: query.informalGroupFilters ? [query.informalGroupFilters] : undefined,
-      target: query.target,
+      target: query.target ? [query.target] : undefined,
       finnish: query.onlyFinnish,
       invasive: query.invasiveSpeciesFilter,
       redListStatusId: query.redListStatusFilters,
-      administrativeStatusId: query.adminStatusFilters
+      typesOfOccurrenceFilters: query.typesOfOccurrenceFilters,
+      typesOfOccurrenceNotFilters: query.typesOfOccurrenceNotFilters
     };
 
     this.router.navigate(
-      ['/observation/map'],
-      {queryParams: parameters}
-    );
+      this.localizeRouterService.translateRoute(
+        ['/observation/map']
+      ), {queryParams: parameters}
+    )
   }
 }
