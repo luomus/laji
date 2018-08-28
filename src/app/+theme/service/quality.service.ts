73bde8deb198306863bb3a9b9e871f75d2b968a2
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2018-08-28 09:10:42 +0300

    /aggregate -> /annotation/aggregate

--- src/app/+theme/service/quality.service.ts
+++ src/app/+theme/service/quality.service.ts
@@ -63,7 +63,7 @@ export class QualityService {
     const cacheKey = JSON.stringify({maxLength, informalTaxonGroup, lastDate});
 
     return this._fetch('users', cacheKey,
-      this.warehouseApi.warehouseQueryAggregateGet(
+      this.warehouseApi.warehouseQueryAnnotationAggregateGet(
         query,
         ['unit.annotations.annotationByPerson', 'unit.annotations.annotationByPersonName'],
         undefined,
