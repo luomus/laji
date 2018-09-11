7a855813a5a20d3149e9f8df59ac69b285cd1664
aorin <aorin@users.noreply.github.com>
2018-09-11 15:28:37 +0300

    add show facts button to private viewer

--- src/app/shared-modules/document-viewer/document-local/document-object/document-object.component.ts
+++ src/app/shared-modules/document-viewer/document-local/document-object/document-object.component.ts
@@ -1,39 +1,55 @@
-import { Component, OnInit, Input, ViewChild, TemplateRef } from '@angular/core';
-import { DocumentObjectField } from './document-object-field';
+import { Component, Input, ViewChild, TemplateRef, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'laji-document-object',
   templateUrl: './document-object.component.html',
   styleUrls: ['./document-object.component.css']
 })
-export class DocumentObjectComponent implements OnInit {
+export class DocumentObjectComponent implements OnChanges {
   @Input() object: any;
-  @Input() fields: any;
   @Input() hideTooltips = false;
+  @Input() showFacts = false;
+  @Input() fields: any;
 
-  _fields: DocumentObjectField[];
+  templates = {};
+  hasFacts = false;
 
   @ViewChild('checkbox') checkboxTpl: TemplateRef<any>;
   @ViewChild('select') selectTpl: TemplateRef<any>;
   @ViewChild('taxonCensus') taxonCensusTpl: TemplateRef<any>;
   @ViewChild('fieldset') fieldsetTpl: TemplateRef<any>;
+  @ViewChild('facts') factsTpl: TemplateRef<any>;
   @ViewChild('default') defaultTpl: TemplateRef<any>;
 
   constructor() { }
 
-  ngOnInit() {
-    this._fields = this.fields.reduce((res, field) => {
-      res.push({
-        name: field.name,
-        label: field.label,
-        type: field.type,
-        enums: field.type === 'select' ? field.options.value_options : undefined,
-        fields: field.fields,
-        template: this[field.name + 'Tpl'] ? this[field.name + 'Tpl'] : (
-          this[field.type + 'Tpl'] ? this[field.type + 'Tpl'] : this.defaultTpl
-        )
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.fields || changes.object) {
+      this.fields.map(field => {
+        this.templates[field.name] = this[field.name + 'Tpl'] ? this[field.name + 'Tpl'] : (
+          field.type === 'fieldset' && field.name.slice(-4) === 'Fact' ? this.factsTpl : (
+          this[field.type + 'Tpl'] ? this[field.type + 'Tpl'] : this.defaultTpl))
+
+        if (this.templates[field.name] === this.factsTpl) {
+          this.updateHasFacts(this.object[field.name], field.fields);
+        }
       });
-      return res;
-    }, []);
+    }
+  }
+
+  private updateHasFacts(facts: any, fields: any[]) {
+    if (!facts || !fields) {
+      this.hasFacts = false;
+    }
+
+    for (let i = 0; i < fields.length; i++) {
+      const field = fields[i];
+      if (facts[field.name] && (field.type !== 'collection' || facts[field.name].length > 0)) {
+        this.hasFacts = true;
+        return;
+      }
+    }
+
+    this.hasFacts = false;
   }
 }
