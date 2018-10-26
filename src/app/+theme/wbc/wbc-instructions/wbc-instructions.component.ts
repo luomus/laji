18f8d01b60b6d29cbf5135c2ef883aeb7f92ea7c
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2018-10-26 09:58:33 +0300

    [completes #153772546] MUST Talvilintulaskennan ohjeiden tarkistus

--- src/app/+theme/wbc/wbc-instructions/wbc-instructions.component.ts
+++ src/app/+theme/wbc/wbc-instructions/wbc-instructions.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { isPlatformBrowser } from '@angular/common';
+import { WINDOW } from '@ng-toolkit/universal';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'laji-wbc-instructions',
@@ -8,9 +11,29 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class WbcInstructionsComponent implements OnInit {
 
-  constructor(public translate: TranslateService) { }
+  showMapInfo = false;
+
+  constructor(
+    @Inject(WINDOW) private window: Window,
+    public translate: TranslateService,
+    private route: ActivatedRoute,
+    @Inject(PLATFORM_ID) private platformID: object
+  ) { }
 
   ngOnInit() {
+    if (isPlatformBrowser(this.platformID)) {
+      this.route.fragment.subscribe((frag) => {
+        if (frag) {
+          window.location.hash = frag;
+        }
+      });
+    }
+  }
+
+  toFragment(fragment) {
+    if (isPlatformBrowser(this.platformID)) {
+      window.location.hash = fragment;
+    }
   }
 
 }
