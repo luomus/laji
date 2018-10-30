7268ac43335077550e179b06ab9e798a6fa26741
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2018-10-30 08:50:36 +0200

    move nafi down in the production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   forceLogin: false,
   disableAnalytics: false,
   systemID: 'KE.389',
-  formWhitelist: ['JX.519', 'MHL.9', 'MHL.25', 'JX.652', 'MHL.6', 'MHL.1', 'MHL.3'],
+  formWhitelist: ['JX.519', 'MHL.9', 'MHL.25', 'JX.652', 'MHL.1', 'MHL.3', 'MHL.6'],
   massForms: ['JX.519', 'JX.652'],
   defaultForm: 'JX.519',
   nafiForm: 'MHL.6',
