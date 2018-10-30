5f167f88188f66b9f3f27f293dc702bcbc606bb7
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2018-10-30 08:27:23 +0200

    test google bot fix

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,16 +1,4 @@
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+import 'core-js/es6';
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 // If you need to support the browsers/features below, uncomment the import
@@ -21,7 +9,6 @@ import 'core-js/es7/array';
 // Needed for: All but Chrome and Firefox, Not supported in IE9
 import 'web-animations-js';
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
 import 'classlist.js';
@@ -37,6 +24,4 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';
-import 'core-js/es6/promise';
-import 'core-js/es6/weak-set';
-import 'core-js/es6/weak-map';
+
