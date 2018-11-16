8fb867ec9e170431358526bbca3f829ed168f2fe
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2018-11-16 16:29:47 +0200

    invasive-control-instructions: use async pipe

--- src/app/+theme/invasive-control/invasive-control-instructions/invasive-control-instructions.container.ts
+++ src/app/+theme/invasive-control/invasive-control-instructions/invasive-control-instructions.container.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { UserService } from 'app/shared/service/user.service';
 import { FormService } from 'app/shared/service/form.service';
 import { environment } from 'environments/environment';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { FormPermissionService } from 'app/+haseka/form-permission/form-permission.service';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -15,39 +15,38 @@ export enum Rights {
 
 @Component({
   template: `<laji-invasive-control-instructions
-    [loggedIn]="loggedIn | async"
-    [rights]="rights">
+    [loggedIn]="loggedIn$ | async"
+    [rights]="rights$ | async">
   </laji-invasive-control-instructions>`
 })
 export class InvasiveControlInstructionsContainerComponent
       implements OnInit {
-  loggedIn: Observable<boolean>;
-  rights: Rights;
+  loggedIn$: Observable<boolean>;
+  rights$: Observable<Rights>;
 
   constructor(private userService: UserService,
               private formService: FormService,
               private formPermissionService: FormPermissionService,
-              private cd: ChangeDetectorRef,
               private translateService: TranslateService) {}
 
   ngOnInit() {
-    this.loggedIn = this.userService.isLoggedIn$.take(1);
+    this.loggedIn$ = this.userService.isLoggedIn$.take(1);
 
-    this.formService.getForm(environment.invasiveControlForm, this.translateService.currentLang)
+    this.rights$ = this.formService
+    .getForm(environment.invasiveControlForm,
+             this.translateService.currentLang)
     .switchMap(form => this.formPermissionService.getRights(form))
     .pipe(
       catchError(() => {
-        this.rights = Rights.NotDefined;
         return of({edit: false, admin: false})
+      }),
+      map((rights) => {
+        if (rights.edit === true) {
+          return Rights.Allowed;
+        } else {
+          return Rights.NotAllowed
+        }
       })
-    )
-    .subscribe((rights) => {
-      if (rights.edit === true) {
-        this.rights = Rights.Allowed;
-      } else {
-        this.rights = Rights.NotAllowed
-      }
-      this.cd.markForCheck();
-    })
+    );
   }
 }
