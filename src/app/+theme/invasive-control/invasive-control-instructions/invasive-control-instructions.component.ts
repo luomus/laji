c50992bb114c0ca67244bff8b2f71652316f9e45
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2018-11-20 11:11:38 +0200

    invasive-control-instructions: remove frags

--- src/app/+theme/invasive-control/invasive-control-instructions/invasive-control-instructions.component.ts
+++ src/app/+theme/invasive-control/invasive-control-instructions/invasive-control-instructions.component.ts
@@ -1,9 +1,4 @@
-import { WINDOW } from '@ng-toolkit/universal';
-import { Component, Inject, OnInit, PLATFORM_ID, ChangeDetectorRef, Input } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { ActivatedRoute } from '@angular/router';
-import { isPlatformBrowser } from '@angular/common';
-import { UserService } from '../../../shared/service/user.service';
+import { Component, Input } from '@angular/core';
 import { Rights } from './invasive-control-instructions.container';
 
 @Component({
@@ -11,27 +6,9 @@ import { Rights } from './invasive-control-instructions.container';
   templateUrl: './invasive-control-instructions.component.html',
   styleUrls: ['./invasive-control-instructions.component.css']
 })
-export class InvasiveControlInstructionsComponent implements OnInit {
+export class InvasiveControlInstructionsComponent {
   Rights = Rights;
 
   @Input() rights: Rights = Rights.NotDefined;
   @Input() loggedIn: boolean;
-
-  constructor(
-    @Inject(WINDOW) private window: Window,
-    public translate: TranslateService,
-    public userService: UserService,
-    private route: ActivatedRoute,
-    @Inject(PLATFORM_ID) private platformID: object
-  ) {}
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformID)) {
-      this.route.fragment.subscribe((frag) => {
-        if (frag) {
-          window.location.hash = frag;
-        }
-      });
-    }
-  }
 }
