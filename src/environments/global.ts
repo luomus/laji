07d0dad49d48796d4d9319d0a3aafb22c61365c9
Olli Raitio <olli.raitio@helsinki.fi>
2018-11-30 17:13:51 +0200

    update default np form id to base id, rename namedPlaceOptions formId -> formID

--- src/environments/global.ts
+++ src/environments/global.ts
@@ -15,7 +15,7 @@ export const Global = {
     lineTransectEiVakio: 'MHL.27',
     lineTransectKartoitus: 'MHL.28',
     whichSpecies: 'MHL.9',
-    namedPlace: 'JX.123608',
+    namedPlace: 'MHL.36',
     collectionContest: 'MHL.25'
   },
   collections: {
