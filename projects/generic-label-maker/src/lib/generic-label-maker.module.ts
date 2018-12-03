c18fb0f02ed7292a911af2483791099fc3b30d20
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2018-12-03 14:54:33 +0200

    start for the actual label maker component

---
+++ projects/generic-label-maker/src/lib/generic-label-maker.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { GenericLabelMakerComponent } from './generic-label-maker.component';
+
+@NgModule({
+  declarations: [GenericLabelMakerComponent],
+  imports: [
+  ],
+  exports: [GenericLabelMakerComponent]
+})
+export class GenericLabelMakerModule { }
---
+++ projects/generic-label-maker/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of generic-label-maker
+ */
+
+export * from './lib/generic-label-maker.service';
+export * from './lib/generic-label-maker.component';
+export * from './lib/generic-label-maker.module';
