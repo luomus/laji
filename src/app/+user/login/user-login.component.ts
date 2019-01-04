35136ad88c41fb3e8a6d6a910eeadb86ba3e4675
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2019-01-04 16:16:00 +0200

    [fixes #162823621] Kirjautumisen uudelleen ohjaus ei toimi kunnolla silloin kun menee suoraan sivulle joka vaatii kirjautumista

--- src/app/+user/login/user-login.component.ts
+++ src/app/+user/login/user-login.component.ts
@@ -1,28 +1,9 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
-import { UserService } from '../../shared/service/user.service';
-import { isPlatformBrowser, Location } from '@angular/common';
-import { URLSearchParams } from '@angular/http';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'laji-user-login',
   templateUrl: './user-login.component.html'
 })
-export class UserLoginComponent implements OnInit {
-
-  constructor(private router: Router,
-              private userService: UserService,
-              private location: Location,
-              @Inject(PLATFORM_ID) private platformId: object
-  ) {
-  }
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      const url = new URLSearchParams(this.location.path(true).replace('?', '?skip=true&'));
-      this.location.replaceState('/', '');
-      this.userService.login(url.get('token'));
-      this.userService.returnToPageBeforeLogin();
-    }
-  }
+export class UserLoginComponent {
 }
