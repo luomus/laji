286645ad30eed38cc13080b4ee0b886f7fba2db3
Meeri <meeri.rannisto@gmail.com>
2019-03-01 13:08:52 +0200

    info-card synonym view fixed

--- src/app/+taxonomy/taxon/info-card/info-card.component.ts
+++ src/app/+taxonomy/taxon/info-card/info-card.component.ts
@@ -1,5 +1,5 @@
-import {catchError, concat, take, delay, retryWhen, map, tap, switchMap} from 'rxjs/operators';
-import {Observable, of, Subscription, throwError} from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import {Observable, of, Subscription} from 'rxjs';
 import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
@@ -12,17 +12,10 @@ import {
   SimpleChanges,
   PLATFORM_ID, EventEmitter,
 } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { Logger } from '../../../shared/logger/logger.service';
 import { Taxonomy, TaxonomyDescription, TaxonomyImage } from '../../../shared/model/Taxonomy';
-import { TaxonomyApi } from '../../../shared/api/TaxonomyApi';
-import { Title } from '@angular/platform-browser';
-import { CacheService } from '../../../shared/service/cache.service';
-import {WarehouseQueryInterface} from '../../../shared/model/WarehouseQueryInterface';
 import {GalleryService} from '../../../shared/gallery/service/gallery.service';
+import {WarehouseQueryInterface} from '../../../shared/model/WarehouseQueryInterface';
 
-// const CACHE_KEY = 'info-card-boxes';
-// interface Settings { [key: string]: {open: boolean}; }
 
 @Component({
   selector: 'laji-info-card',
@@ -31,12 +24,11 @@ import {GalleryService} from '../../../shared/gallery/service/gallery.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InfoCardComponent implements OnInit, OnChanges {
-  // private static settings: Settings;
-  @Input() taxonId: string;
+  @Input() taxon: Taxonomy;
+  @Input() isFromMasterChecklist: boolean;
   @Input() context: string;
   @Input() activeTab: 'overview'|'images'|'biology'|'taxonomy'|'occurrence';
 
-  taxon: Taxonomy;
   taxonDescription: Array<TaxonomyDescription>;
   taxonImages: Array<TaxonomyImage>;
 
@@ -44,20 +36,13 @@ export class InfoCardComponent implements OnInit, OnChanges {
   hasBiologyData: boolean;
   images = [];
 
-  loading = false;
   activatedTabs = {};
-  // public settings: Settings;
-  private initTaxonSub: Subscription;
+
   private imageSub: Subscription;
 
   @Output() routeUpdate = new EventEmitter();
 
   constructor(
-    public translate: TranslateService,
-    private taxonService: TaxonomyApi,
-    private cacheService: CacheService,
-    private logger: Logger,
-    private title: Title,
     private cd: ChangeDetectorRef,
     private galleryService: GalleryService,
     @Inject(PLATFORM_ID) private platformId: object,
@@ -65,109 +50,38 @@ export class InfoCardComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     this.hasImageData = this.activeTab === 'images';
-    // this.initSettings();
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes.taxonId) {
-      if (this.initTaxonSub) {
-        this.initTaxonSub.unsubscribe();
-      }
-
-      this.loading = true;
-      this.initTaxonSub = this.initTaxon().subscribe(() => {
-        this.loading = false;
-        this.cd.markForCheck();
-      });
-    }
-
     if (changes.activeTab) {
       this.activatedTabs[this.activeTab] = true;
+    }
+
+    if (changes.taxon) {
+      this.taxonImages = (this.taxon.multimedia || []).map(img => {
+        if (img['taxon']) {
+          img = {...img['taxon'], ...img};
+        }
+        return img;
+      });
+      this.taxonDescription = (this.taxon.descriptions || []).reduce((prev, current) => {
+        if (current.title) {
+          prev.push(current);
+        }
+        return prev;
+      }, []);
+
+      this.hasBiologyData = !!this.taxon.primaryHabitat || !!this.taxon.secondaryHabitats || this.taxonDescription.length > 0;
+      if (!this.hasBiologyData && this.activeTab === 'biology') {
+        this.updateRoute(this.taxon.id, 'overview');
+      }
+
+      this.setImages();
     }
   }
 
   updateRoute(id: string, tab = this.activeTab, context = this.context) {
     this.routeUpdate.emit({id: id, tab: tab, context: context});
-  }
-
-/*  private initSettings() {
-    const settings$ = InfoCardComponent.settings ?
-      ObservableOf(InfoCardComponent.settings) :
-      this.cacheService.getItem<Settings>(CACHE_KEY)
-        .pipe(
-          map(value => value || {}),
-          tap(value => InfoCardComponent.settings = value)
-        );
-
-    settings$.subscribe(settings => {
-      this.settings = settings;
-      this.cd.markForCheck();
-    });
-  }
-
-  updateSettings(boxName: string, open: boolean) {
-    this.settings[boxName] = {open: open};
-    InfoCardComponent.settings[boxName] = {open: open};
-    this.cacheService.setItem<Settings>(CACHE_KEY, InfoCardComponent.settings)
-      .subscribe(() => {}, () => {});
-  }
-*/
-
- /* private updateMap() {
-    if (!this.map || !isPlatformBrowser(this.platformId)) {
-      return;
-    }
-    setTimeout(() => {
-      if (this.map) {
-        this.map.invalidateSize();
-        this.cd.markForCheck();
-      }
-    }, 100);
-  }*/
-
-  private initTaxon(): Observable<any> {
-    return this.getTaxon(this.taxonId).pipe(
-      tap(taxon => {
-        this.taxon = taxon;
-        this.taxonImages = (taxon.multimedia || []).map(img => {
-          if (img.taxon) {
-            img = {...img.taxon, ...img};
-          }
-          return img;
-        });
-        this.taxonDescription = (taxon.descriptions || []).reduce((prev, current) => {
-          if (current.title) {
-            prev.push(current);
-          }
-          return prev;
-        }, []);
-
-        this.hasBiologyData = !!this.taxon.primaryHabitat || !!this.taxon.secondaryHabitats || this.taxonDescription.length > 0;
-        if (!this.hasBiologyData && this.activeTab === 'biology') {
-          this.updateRoute(this.taxonId, 'overview');
-        }
-
-        this.setTitle();
-        this.setImages();
-        // this.updateMap();
-      })
-    );
-  }
-
-  private setTitle() {
-    let title = this.taxon.vernacularName && this.taxon.vernacularName[this.translate.currentLang] || '';
-    title += title ? ' (' + this.taxon.scientificName + ')' : this.taxon.scientificName;
-    this.title.setTitle((title ? title + ' | '  : '') + this.title.getTitle());
-  }
-
-  private getTaxon(id) {
-    return this.taxonService
-      .taxonomyFindBySubject(id, 'multi', {includeMedia: true, includeDescriptions: true}).pipe(
-      retryWhen(errors => errors.pipe(delay(1000), take(3), concat(throwError(errors)), ))).pipe(
-      catchError(err => {
-        this.logger.warn('Failed to fetch taxon by id', err);
-        return of({});
-      }));
   }
 
   private setImages() {
@@ -223,7 +137,7 @@ export class InfoCardComponent implements OnInit, OnChanges {
       this.cd.markForCheck();
 
       if (!this.hasImageData && this.activeTab === 'images') {
-        this.updateRoute(this.taxonId, 'overview');
+        this.updateRoute(this.taxon.id, 'overview');
       }
     });
   }
@@ -235,4 +149,3 @@ export class InfoCardComponent implements OnInit, OnChanges {
     ).pipe(map(res => this.galleryService.getImages(res, limit)));
   }
 }
-
