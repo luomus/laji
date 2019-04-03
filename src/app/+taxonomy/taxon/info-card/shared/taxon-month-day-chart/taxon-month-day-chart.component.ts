550fe3a171fcef4b4b7dd5d3fcff43da1759e4c1
aorin <meeri.rannisto@gmail.com>
2019-04-03 17:14:48 +0300

    continue info card observation chart

--- src/app/+taxonomy/taxon/info-card/shared/taxon-month-day-chart/taxon-month-day-chart.component.ts
+++ src/app/+taxonomy/taxon/info-card/shared/taxon-month-day-chart/taxon-month-day-chart.component.ts
@@ -1,5 +1,5 @@
 import {
-  ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, OnChanges, OnDestroy, Output
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnDestroy, Output, ViewChild
 } from '@angular/core';
 import {Subscription, of, Observable, forkJoin} from 'rxjs';
 import {WarehouseApi} from '../../../../../shared/api/WarehouseApi';
@@ -7,6 +7,7 @@ import {map, switchMap, tap} from 'rxjs/operators';
 import {TranslateService} from '@ngx-translate/core';
 import {WarehouseValueMappingService} from '../../../../../shared/service/warehouse-value-mapping.service';
 import {TriplestoreLabelService} from '../../../../../shared/service/triplestore-label.service';
+import {ModalDirective} from 'ngx-bootstrap';
 
 @Component({
   selector: 'laji-taxon-month-day-chart',
@@ -14,9 +15,17 @@ import {TriplestoreLabelService} from '../../../../../shared/service/triplestore
   styleUrls: ['./taxon-month-day-chart.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TaxonMonthDayChartComponent implements OnInit, OnChanges, OnDestroy {
+export class TaxonMonthDayChartComponent implements OnChanges, OnDestroy {
+  @ViewChild('dayChartModal') public modal: ModalDirective;
   @Input() taxonId: string;
-  data: any[];
+
+  monthChartData: any[];
+  dayChartDataByMonth = {};
+
+  dayChartModalVisible = false;
+  activeMonth: number;
+
+  monthFormatting = this.getMonthLabel.bind(this);
 
   private getDataSub: Subscription;
 
@@ -30,10 +39,6 @@ export class TaxonMonthDayChartComponent implements OnInit, OnChanges, OnDestroy
     private cd: ChangeDetectorRef
   ) { }
 
-  ngOnInit() {
-
-  }
-
   ngOnChanges() {
     this.updateData();
   }
@@ -45,7 +50,15 @@ export class TaxonMonthDayChartComponent implements OnInit, OnChanges, OnDestroy
   }
 
   onSelect(e) {
-    console.log(e);
+    if (e && e.series) {
+      this.activeMonth = e.series;
+      this.dayChartModalVisible = true;
+      this.modal.show();
+    }
+  }
+
+  onHideDayChart() {
+    this.dayChartModalVisible = false;
   }
 
   private updateData() {
@@ -53,77 +66,95 @@ export class TaxonMonthDayChartComponent implements OnInit, OnChanges, OnDestroy
       this.getDataSub.unsubscribe();
     }
 
-    this.data = undefined;
+    this.monthChartData = [];
+    this.dayChartDataByMonth = {};
+
     this.getDataSub = this.warehouseApi.warehouseQueryAggregateGet(
       { taxonId: [this.taxonId], cache: true },
-      ['gathering.conversions.month', 'unit.lifeStage'],
-      ['gathering.conversions.month', 'unit.lifeStage'],
+      ['gathering.conversions.month', 'gathering.conversions.day', 'unit.lifeStage'],
+      ['unit.lifeStage'],
       10000,
       1,
       undefined,
       true
     ).pipe(
       map(res => res.results),
-      switchMap(res => this.getData(res))
-    ).subscribe(data => {
-      this.data = data;
-      this.hasData.emit(this.data.length > 0);
+      switchMap(res => this.setData(res))
+    ).subscribe(() => {
+      this.hasData.emit(this.monthChartData.length > 0);
       this.cd.markForCheck();
     });
   }
 
-  yAxisTickFormatting(value: number) {
-    return value.toLocaleString('fi');
-  }
-
-  private getData(result: any[]): Observable<any[]> {
+  private setData(result: any[]): Observable<any[]> {
     if (result.length < 1) {
       return of([]);
     }
 
-    const data = this.getInitData();
+    this.monthChartData = this.initMonthChartData();
     const labelObservables = [];
 
     result.forEach(r => {
       const month = parseInt(r.aggregateBy['gathering.conversions.month'], 10);
+      const day = parseInt(r.aggregateBy['gathering.conversions.day'], 10);
       const lifeStage = r.aggregateBy['unit.lifeStage'];
       const count = r.count;
-      const series = data[month - 1].series;
 
-      if (series.length < 1) {
-        series.push({
-          name: this.translate.instant('all'),
-          value: 0
-        });
+      this.addDataToSeries(lifeStage, count, this.monthChartData[month - 1].series, labelObservables);
+
+      if (day) {
+        if (!this.dayChartDataByMonth[month]) {
+          this.dayChartDataByMonth[month] = this.initDayChartData(month);
+        }
+        this.addDataToSeries(lifeStage, count, this.dayChartDataByMonth[month][day - 1].series, labelObservables);
       }
-      series[0].value += count;
+    });
 
-      if (lifeStage) {
-        const dataItem = {
-          name: lifeStage,
-          value: count
-        };
-        series.push(dataItem);
+    return labelObservables.length > 0 ? forkJoin(labelObservables) : of([]);
+  }
+
+  private addDataToSeries(lifeStage: string, count: number, series: any[], labelObservables: any[]) {
+    if (series.length < 1) {
+      series.push({
+        name: this.translate.instant('all'),
+        value: 0
+      });
+    }
+    series[0].value += count;
+
+    if (lifeStage) {
+      let index = series.findIndex(s => s.name === lifeStage);
+      if (index === -1) {
+        index = series.length;
+        series.push({name: lifeStage, value: 0});
         labelObservables.push(
           this.getLabel(lifeStage).pipe(
             tap(label => {
-              dataItem.name = label;
+              series[index].name = label;
             })
           )
         );
       }
-    });
-
-    return (labelObservables.length > 0 ? forkJoin(labelObservables) : of([]))
-      .pipe(map(() => data));
+      series[index].value += count;
+    }
   }
 
-  private getInitData() {
+  private initMonthChartData() {
     const data = [];
     for (let i = 1; i < 13; i++) {
-      const name = this.translate.instant('m-' + (i < 10 ? '0' : '') + i);
       data.push({
-        name: name,
+        name: i,
+        series: []
+      });
+    }
+    return data;
+  }
+
+  private initDayChartData(month: number) {
+    const data = [];
+    for (let i = 1; i < this.getNbrOfDaysInMonth(month) + 1; i++) {
+      data.push({
+        name: i,
         series: []
       });
     }
@@ -136,5 +167,17 @@ export class TaxonMonthDayChartComponent implements OnInit, OnChanges, OnDestroy
         switchMap(key => this.triplestoreLabelService.get(key, this.translate.currentLang)),
         map(label => label ? label.charAt(0).toUpperCase() + label.slice(1) : '')
       );
+  }
+
+  yAxisTickFormatting(value: number) {
+    return value.toLocaleString('fi');
+  }
+
+  private getMonthLabel(month: number) {
+    return this.translate.instant('m-' + (month < 10 ? '0' : '') + month);
+  }
+
+  private getNbrOfDaysInMonth(month: number) {
+    return new Date(2000, month, 0).getDate();
   }
 }
