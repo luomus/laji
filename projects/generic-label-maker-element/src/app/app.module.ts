726b31e3a0a56415171e9c1018a893cff2af9d9c
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2019-04-15 12:18:27 +0300

    fine tune the custom html element

--- projects/generic-label-maker-element/src/app/app.module.ts
+++ projects/generic-label-maker-element/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, Injector, NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 import { GenericLabelMakerModule } from 'generic-label-maker';
 import { LabelDesignerComponent } from './label-designer/label-designer.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent,
     LabelDesignerComponent
   ],
   imports: [
@@ -17,8 +16,15 @@ import { LabelDesignerComponent } from './label-designer/label-designer.componen
     GenericLabelMakerModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
   entryComponents: [LabelDesignerComponent]
 })
-export class AppModule {
+export class AppModule implements DoBootstrap {
+
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap(appRef: ApplicationRef): void {
+    const LabelDesignerElement = createCustomElement(LabelDesignerComponent, {injector: this.injector});
+    customElements.define('label-designer', LabelDesignerElement);
+  }
+
 }
--- projects/generic-label-maker-element/src/app/label-designer/label-designer.component.ts
+++ projects/generic-label-maker-element/src/app/label-designer/label-designer.component.ts
@@ -1,15 +1,83 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ISetup } from 'generic-label-maker';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ILabelField, ISetup, IViewSettings, Presets } from 'generic-label-maker';
 
 @Component({
-  selector: 'label-maker',
+  selector: 'label-designer',
   templateUrl: './label-designer.component.html',
   styleUrls: ['./label-designer.component.scss']
 })
 export class LabelDesignerComponent implements OnInit {
 
+
   @Input()
-  setup: ISetup;
+  showIntro: boolean;
+
+  @Input()
+  data: object[];
+
+  @Input()
+  defaultDomain = 'http://tun.fi/EXAMPLE.';
+
+  @Input()
+  availableFields: ILabelField[] = [];
+
+  @Input()
+  newAvailableFields: ILabelField[] = [];
+
+  @Input()
+  viewSetting: IViewSettings = {
+    magnification: 2
+  };
+
+  @Input()
+  setup: ISetup = {
+    page: {
+      ...Presets.A4,
+      'paddingTop.mm': 10,
+      'paddingLeft.mm': 10,
+      'paddingBottom.mm': 10,
+      'paddingRight.mm': 10
+    },
+    label: {
+      'height.mm': 20,
+      'width.mm': 50,
+      'marginTop.mm': 1.5,
+      'marginLeft.mm': 1.5,
+      'marginBottom.mm': 1.5,
+      'marginRight.mm': 1.5,
+      'font-family': 'Arial',
+      'font-size.pt': 9
+    },
+    labelItems: [
+      {
+        type: 'field',
+        style: {
+          'width.mm': 13,
+          'height.mm': 13,
+        },
+        x: 0,
+        y: 0,
+        fields: [
+          {field: 'id', content: 'http://example.com/ID', label: 'ID - QRCode', type: 'qr-code'}
+        ]
+      },
+      {
+        type: 'field',
+        style: {
+          'width.mm': 35,
+          'height.mm': 5,
+        },
+        x: 15,
+        y: 0,
+        fields: [
+          {field: 'id', content: 'http://example.com/ID', label: 'ID'}
+        ]
+      }
+    ]
+  };
+
+  @Output()
+  html = new EventEmitter<string>();
 
   constructor() { }
 
