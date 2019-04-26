bf8ac9708d9856a6dcfe16448e64ca4e47e3e58d
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2019-04-26 07:59:30 +0300

    [E2E] added map and protractor config to git

--- e2e/config.ts
+++ e2e/config.ts
@@ -1,6 +1,6 @@
 export default {
   person: {
-    token: '%USER_TOKEN%',
-    name: '%USER_NAME%'
+    token: 'OTFiMzJhNjktMTIyMy00ODkzLTgwNjgtNjg1NzM5N2NhNTJk',
+    name: 'Ville-Matti Riihikoski'
   }
 };
---
+++ e2e/src/+map/map.page.ts
@@ -0,0 +1,15 @@
+import { browser, by, element, promise as wdpromise } from 'protractor';
+
+export class MapPage {
+
+  // closeTile = element(by.css('img[src="https://proxy.laji.fi/osm/5/18/8.png"]'));
+  closeTile = element(by.css('img[src="https://proxy.laji.fi/osm/1/1/0.png"]'));
+
+  navigateToObservationData(): wdpromise.Promise<void> {
+    return browser.get('/map?target=Arion+vulgaris&invasive=true&color=a900aa');
+  }
+
+  isZoomedIn(): wdpromise.Promise<boolean> {
+    return this.closeTile.isPresent();
+  }
+}
