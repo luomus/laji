ea292b63283a8fc85bd6f5081fa077a8e75c6240
Olli Raitio <olli.raitio@helsinki.fi>
2019-06-05 12:44:55 +0300

    HasFormPermission guard finds formID from parents' route data

--- src/app/shared/route/has-form-permission.ts
+++ src/app/shared/route/has-form-permission.ts
@@ -16,12 +16,26 @@ export class HasFormPermission implements CanActivate {
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.formService.getForm(route.data.formID, 'fi').pipe(
+    let {data} = route;
+    // Find data route, which can be in the given route or in some of its parents.
+    do {
+      if (route.data && route.data.formID) {
+        data = route.data;
+        break;
+      }
+      route = route.parent;
+    } while (route);
+
+    if (!data.formID) {
+      console.warn('HasFormPermission guard used without route data with formID');
+    }
+
+    return this.formService.getForm(data.formID, 'fi').pipe(
       take(1),
-      flatMap(data => this.formPermissionService.getRights(data)),
-      map(data => data.edit || data.admin),
+      flatMap(form => this.formPermissionService.getRights(form)),
+      map(fp => fp.edit || fp.admin),
       tap((hasPermission) => {
-        const {noFormPermissionRedirect} = route.data;
+        const {noFormPermissionRedirect} = data;
         if (!hasPermission) {
           this.router.navigate([noFormPermissionRedirect || '/']);
         }
