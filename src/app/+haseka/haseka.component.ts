3c1b4685fd265c8ddc998cf21311959731a0d617
Persichini <persigio@ad.helsinki.fi>
2019-07-04 08:47:36 +0300

    Style Vihko page + links and conditions of Vihko page

--- src/app/+haseka/haseka.component.ts
+++ src/app/+haseka/haseka.component.ts
@@ -45,7 +45,7 @@ export class HasekaComponent implements OnInit, OnDestroy {
       map(event => true), ).pipe(
       startWith(true))
       .subscribe(() => {
-        this.isFront = this.router.isActive('/vihko', true);
+        this.isFront = this.router.isActive('/vihko/forms', true);
       });
   }
 
