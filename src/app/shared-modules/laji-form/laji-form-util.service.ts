2c8a7a29962aeb57499326e83a379acdb351fbba
Persichini <persigio@ad.helsinki.fi>
2019-07-05 08:30:16 +0300

    fix bug popup in page observation

--- src/app/shared-modules/laji-form/laji-form-util.service.ts
+++ src/app/shared-modules/laji-form/laji-form-util.service.ts
@@ -1,7 +1,7 @@
-import { schemaJSONPointer as schemaJSONPointerFunc } from 'laji-form/lib/utils';
+/*import { schemaJSONPointer as schemaJSONPointerFunc } from 'laji-form/lib/utils';
 
 export class LajiFormUtil {
   public static schemaJSONPointer(schema: any, path: string): string {
     return schemaJSONPointerFunc(schema, path);
   }
-}
+}*/
