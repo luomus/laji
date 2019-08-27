18c0bde1768f9ef551328a0497fa300b9b4a4c2f
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2019-08-27 11:20:07 +0300

    place holder for laji-ui component library

---
+++ projects/laji-ui/src/lib/laji-ui.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { LajiUiComponent } from './laji-ui.component';
+
+
+
+@NgModule({
+  declarations: [LajiUiComponent],
+  imports: [
+  ],
+  exports: [LajiUiComponent]
+})
+export class LajiUiModule { }
