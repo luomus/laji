9f89fe221b12e71c65db44c5d5614c4f41b72f80
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2019-09-04 09:28:03 +0300

    [completes #166466615] Add MX.controllingRisksOfInvasiveAlienSpecies as an option to filter species and observations by invasive alien species categories

--- src/app/+observation/view/observation-view.component.ts
+++ src/app/+observation/view/observation-view.component.ts
@@ -39,7 +39,8 @@ export class ObservationViewComponent implements OnInit, OnDestroy {
     'nationalInvasiveSpeciesStrategy',
     'otherInvasiveSpeciesList',
     'euInvasiveSpeciesList',
-    'quarantinePlantPest'
+    'quarantinePlantPest',
+    'controllingRisksOfInvasiveAlienSpecies'
   ];
 
   subQueryUpdate: Subscription;
