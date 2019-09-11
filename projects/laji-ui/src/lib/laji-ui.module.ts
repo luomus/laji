cc154137f3d2b569e820a5a67d52c0e7426b5a81
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-09-11 15:41:49 +0300

    [laji-ui] tabs component functionality

--- projects/laji-ui/src/lib/laji-ui.module.ts
+++ projects/laji-ui/src/lib/laji-ui.module.ts
@@ -3,14 +3,16 @@ import { ButtonComponent } from './button/button.component';
 import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tabs/tab/tab.component';
 
 
 
 @NgModule({
-  declarations: [ButtonComponent, CheckboxComponent, DropdownComponent],
+  declarations: [ButtonComponent, CheckboxComponent, DropdownComponent, TabsComponent, TabComponent],
   imports: [
     CommonModule
   ],
-  exports: [ButtonComponent, CheckboxComponent, DropdownComponent]
+  exports: [ButtonComponent, CheckboxComponent, DropdownComponent, TabsComponent, TabComponent]
 })
 export class LajiUiModule { }
---
+++ projects/laji-ui/src/lib/tabs/tabs.component.ts
@@ -0,0 +1,27 @@
+import { Component, ContentChildren, AfterViewInit, QueryList, ComponentFactoryResolver, ViewChild, ViewContainerRef, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { TabComponent } from './tab/tab.component';
+
+@Component({
+  selector: 'lu-tabs',
+  templateUrl: './tabs.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TabsComponent implements AfterViewInit {
+  @ContentChildren(TabComponent) tabComponents !: QueryList<TabComponent>;
+
+  selectedIndex = 0;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.tabComponents.first.active = true;
+    });
+  }
+
+  onSelect(tabIndex: number) {
+    this.tabComponents.toArray()[this.selectedIndex].active = false;
+    this.selectedIndex = tabIndex;
+    this.tabComponents.toArray()[tabIndex].active = true;
+  }
+}
