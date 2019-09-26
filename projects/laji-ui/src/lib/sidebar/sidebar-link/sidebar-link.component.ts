4708bb63ed0786a5b2fadfc3827af687cd49f482
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-09-26 11:39:23 +0300

    [laji-ui] sidebar-link: unsubscribe on destroy

--- projects/laji-ui/src/lib/sidebar/sidebar-link/sidebar-link.component.ts
+++ projects/laji-ui/src/lib/sidebar/sidebar-link/sidebar-link.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { filter, map, startWith, tap } from 'rxjs/operators';
+import { filter, map, startWith, tap, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'lu-sidebar-link',
   templateUrl: './sidebar-link.component.html',
   styleUrls: ['./sidebar-link.component.scss']
 })
-export class SidebarLinkComponent implements OnInit {
+export class SidebarLinkComponent implements OnInit, OnDestroy {
   @Input() link: Array<string>;
   active = false;
+  unsubscribe$ = new Subject<void>();
   constructor(private router: Router) {}
   ngOnInit() {
     const urls$ = this.router.events.pipe(
@@ -17,9 +19,13 @@ export class SidebarLinkComponent implements OnInit {
       map((e: NavigationEnd) => e.urlAfterRedirects || e.url),
       startWith(this.router.url)
     );
-    urls$.subscribe((u: string) => {
+    urls$.pipe(takeUntil(this.unsubscribe$)).subscribe((u: string) => {
       const substrs = u.split('/');
       this.active = u.includes(this.link[this.link.length - 1]);
     });
+  }
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
