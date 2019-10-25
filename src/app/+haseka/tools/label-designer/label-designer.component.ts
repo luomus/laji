b8f80046026888afe67d6ba0953cfc7272e42d59
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2019-10-25 20:18:40 +0300

    fix ssr build

--- src/app/+haseka/tools/label-designer/label-designer.component.ts
+++ src/app/+haseka/tools/label-designer/label-designer.component.ts
@@ -58,7 +58,7 @@ export class LabelDesignerComponent implements OnInit {
         'marginLeft.mm': 1.5,
         'marginBottom.mm': 1.5,
         'marginRight.mm': 1.5,
-        'font-family': 'Open Sans',
+        'font-family': Presets.DefaultFont,
         'font-size.pt': 9
       },
       labelItems: this.pdfLabelService.defaultFields.map((a, i) => ({
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,20 +1,7 @@
-import * as moment from 'moment';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-const now = moment();
-console.log(now);
+enableProdMode();
 
 export { AppServerModule } from './app/app.server.module';
-
-// Express Engine
 export { ngExpressEngine } from '@nguniversal/express-engine';
-
-// Import module map for lazy loading
 export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
-import { renderModuleFactory } from '@angular/platform-server';
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+export { renderModuleFactory } from '@angular/platform-server';
