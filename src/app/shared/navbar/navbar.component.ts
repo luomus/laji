707af730f8ebfc0c514ec38d9cf97353e695ca31
Mäkinen Pyry V <pyry.makinen@helsinki.fi>
2019-11-21 09:45:59 +0200

    refactor navbar subs to use takeuntil pattern

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { delay, filter, map, startWith, switchMap, take, tap } from 'rxjs/operators';
+import { delay, filter, map, startWith, switchMap, take, tap, takeUntil } from 'rxjs/operators';
 import {
   ApplicationRef,
   ChangeDetectionStrategy,
@@ -15,7 +15,7 @@ import { NavigationEnd, Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
 import { LocalizeRouterService } from '../../locale/localize-router.service';
 import { TranslateService } from '@ngx-translate/core';
-import { interval as ObservableInterval, of as ObservableOf, Subscription, timer } from 'rxjs';
+import { interval as ObservableInterval, of as ObservableOf, Subscription, timer, Subject } from 'rxjs';
 import { BsDropdownDirective } from 'ngx-bootstrap';
 import { DialogService } from '../service/dialog.service';
 import { PagedResult } from '../model/PagedResult';
@@ -32,6 +32,7 @@ import { NotificationsFacade } from './notifications/notifications.facade';
   providers: [NotificationsFacade]
 })
 export class NavbarComponent implements OnInit, OnDestroy {
+  unsubscribe$ = new Subject<null>();
 
   @ViewChild('userMenu', { static: false }) public dropDown: BsDropdownDirective;
 
@@ -43,9 +44,6 @@ export class NavbarComponent implements OnInit, OnDestroy {
   notificationsNotSeen = 0;
   notificationPageSize = 5;
   env = environment.type;
-
-  private subParams: Subscription;
-  private subUser: Subscription;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
@@ -64,18 +62,18 @@ export class NavbarComponent implements OnInit, OnDestroy {
     if (!isPlatformBrowser(this.platformId)) {
       return;
     }
-    this.subParams = this.router.events.subscribe((event) => {
+    this.router.events.pipe(takeUntil(this.unsubscribe$)).subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.closeMenu();
         this.changeDetector.markForCheck();
       }
     });
-    this.notificationsFacade.state$.subscribe((state) => {
+    this.notificationsFacade.state$.pipe(takeUntil(this.unsubscribe$)).subscribe((state) => {
       this.notifications = state.notifications;
       this.notificationsNotSeen = state.unseenCount;
       this.changeDetector.markForCheck();
     });
-    timer(1000, 60000).subscribe(() => {
+    timer(1000, 60000).pipe(takeUntil(this.unsubscribe$)).subscribe(() => {
       this.notificationsFacade.loadAll(0, this.notificationPageSize);
     });
   }
@@ -85,12 +83,8 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    if (this.subUser) {
-      this.subUser.unsubscribe();
-    }
-    if (this.subParams) {
-      this.subParams.unsubscribe();
-    }
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   updateView() {
