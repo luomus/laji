e4ae3a99525d088c958d23ecc4b00095f6b32de3
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2019-11-26 08:03:58 +0200

    fix dev build

--- src/app/shared-modules/annotations/annotation-list/annotation-list.component.ts
+++ src/app/shared-modules/annotations/annotation-list/annotation-list.component.ts
@@ -19,6 +19,7 @@ export class AnnotationListComponent implements OnInit, OnDestroy {
   annotationClass = Annotation.AnnotationClassEnum;
   changingLocale = false;
   open: boolean[] = undefined;
+  showLinks = false;
 
   constructor() { }
 
