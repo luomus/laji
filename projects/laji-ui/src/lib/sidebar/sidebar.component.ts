b5286a14065533f46ad9e5f00683640ccd08d0ef
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-01-21 15:10:51 +0200

    observation form open btn desc

--- projects/laji-ui/src/lib/sidebar/sidebar.component.ts
+++ projects/laji-ui/src/lib/sidebar/sidebar.component.ts
@@ -59,6 +59,7 @@ export class SidebarComponent implements OnDestroy, AfterViewInit {
 
   @Input() position: 'left' | 'right' = 'left';
   @Input() staticWidth: number;
+  @Input() menuTitle: string;
 
   sidebarMinWidth = 50;
 
