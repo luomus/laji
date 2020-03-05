5d0e1d582765f8d750d5f58d6526743826a00ee7
aorin <meeri.rannisto@gmail.com>
2020-03-05 16:04:09 +0200

    can move between kerttu views

--- src/app/+theme/kerttu/kerttu-main-view/kerttu-main-view.component.ts
+++ src/app/+theme/kerttu/kerttu-main-view/kerttu-main-view.component.ts
@@ -1,6 +1,11 @@
-import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import { IKerttuState, KerttuFacade, Step } from '../kerttu.facade';
 import {Observable} from 'rxjs';
+import {switchMap, take} from 'rxjs/operators';
+import {Profile} from '../../../shared/model/Profile';
+import {UserService} from '../../../shared/service/user.service';
+import {PersonApi} from '../../../shared/api/PersonApi';
+import {KerttuApi} from '../kerttu-api';
 
 @Component({
   selector: 'laji-kerttu-main-view',
@@ -24,19 +29,53 @@ export class KerttuMainViewComponent implements OnInit {
     {name: 'done', label: 'theme.kerttu.step4', returnState: Step.done}
   ];
 
+  step = Step;
+
   constructor(
+    private kerttuApi: KerttuApi,
     private kerttuFacade: KerttuFacade,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private userService: UserService,
+    private personService: PersonApi
   ) {
     this.vm$ = kerttuFacade.vm$;
   }
 
   ngOnInit() {
     this.kerttuFacade.clear();
+
+    this.userService.isLoggedIn$.pipe(take(1)).subscribe(() => {
+      this.kerttuApi.getStatus(this.userService.getToken()).subscribe((status) => {
+        this.kerttuFacade.goToStep(status);
+      });
+    });
   }
 
   activate(step: Step) {
-    this.kerttuFacade.goToStep(step);
-    this.cdr.markForCheck();
+    this.kerttuApi.setStatus(this.userService.getToken(), step).subscribe(() => {
+      this.kerttuFacade.goToStep(step);
+      this.cdr.markForCheck();
+    });
+  }
+
+  saveAndGoToNext(currentStep: Step) {
+    if (currentStep === Step.fillExpertise) {
+      this.activate(Step.annotateLetters);
+    } else if (currentStep === Step.annotateLetters) {
+      this.activate(Step.annotateRecordings);
+    } else if (currentStep === Step.annotateRecordings) {
+      this.activate(Step.done);
+    }
+  }
+
+  saveProfile() {
+    this.personService.personFindProfileByToken(this.userService.getToken()).pipe(
+      switchMap((profile: Profile) => {
+        // something here
+        return this.personService.personUpdateProfileByToken(profile, this.userService.getToken());
+      })
+    ).subscribe(() => {
+
+    });
   }
 }
