275d6763367ec406f54c46bb5fe7d8f1c0e475d7
Mikko Heikkinen <hawk@biomi.org>
2020-04-07 06:57:08 +0000

    Fix waterbird form id - environment.beta.ts edited online with Bitbucket

--- src/environments/environment.beta.ts
+++ src/environments/environment.beta.ts
@@ -33,7 +33,7 @@ export const environment = {
   valioForm: 'MHL.57',
   saveObservations: {
     citizenScienceForms: ['JX.652', 'MHL.6', 'MHL.51', 'MHL.25'],
-    birdMonitoringForms: ['MHL.1', 'MHL.61', 'MHL.3'],
+    birdMonitoringForms: ['MHL.1', 'MHL.65', 'MHL.3'],
     researchProjects: ['MHL.33', 'MHL.50', 'MHL.57', 'MHL.45', 'MHL.59']
   },
   apiBase: 'https://beta.laji.fi/api',
