895df174ef66626d2b54d1c42b8269fcb343cedc
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-04-09 08:16:41 +0300

    change the way forceLogin works

--- src/app/shared/service/authenticated-http.interceptor.ts
+++ src/app/shared/service/authenticated-http.interceptor.ts
@@ -1,10 +1,9 @@
-import { Observable, throwError as observableThrowError } from 'rxjs';
-import { Inject, Injectable } from '@angular/core';
+import { Observable, of, throwError as observableThrowError } from 'rxjs';
+import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { UserService } from './user.service';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { WINDOW } from '@ng-toolkit/universal';
-import { switchMap, tap } from 'rxjs/operators';
+import { catchError, take, tap, timeout } from 'rxjs/operators';
 import { SessionStorage } from 'ngx-webstorage';
 
 
@@ -27,12 +26,17 @@ export class AuthenticatedHttpInterceptor implements HttpInterceptor {
             const secPass = (Date.now() - (this.lastCheck || 0)) / 1000;
             if (secPass > 60 && environment.forceLogin && (err.status === 401 || err.status === 403)) {
               this.lastCheck = Date.now();
-              return this.userService.checkLogin().pipe(
-                switchMap(() => observableThrowError(err))
-              );
-            } else {
-              return observableThrowError(err);
+              this.userService.isLoggedIn$.pipe(
+                timeout(10000),
+                catchError(() => of(false)),
+                take(1)
+              ).subscribe(loggedIn => {
+                if (!loggedIn) {
+                  this.userService.redirectToLogin();
+                }
+              });
             }
+            return observableThrowError(err);
           }
         })
     );
