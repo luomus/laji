bb75aeeb3b2c0e28eea0efb0b1ee54b058150cd8
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-04-09 07:18:24 +0300

    [VIR] use user services checkLogin method when request fails

--- src/app/shared/service/authenticated-http.interceptor.ts
+++ src/app/shared/service/authenticated-http.interceptor.ts
@@ -4,13 +4,18 @@ import { environment } from '../../../environments/environment';
 import { UserService } from './user.service';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { WINDOW } from '@ng-toolkit/universal';
-import { tap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { SessionStorage } from 'ngx-webstorage';
 
 
 @Injectable()
 export class AuthenticatedHttpInterceptor implements HttpInterceptor {
 
-  constructor(@Inject(WINDOW) private window: Window) { }
+  @SessionStorage() private lastCheck;
+
+  constructor(
+    private userService: UserService
+  ) { }
 
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -19,8 +24,12 @@ export class AuthenticatedHttpInterceptor implements HttpInterceptor {
         (event: HttpEvent<any>) => {},
         (err: any) => {
           if (err instanceof HttpErrorResponse) {
-            if (environment.forceLogin && (err.status === 401 || err.status === 403)) {
-              this.window.location.href = UserService.getLoginUrl();
+            const secPass = (Date.now() - (this.lastCheck || 0)) / 1000;
+            if (secPass > 60 && environment.forceLogin && (err.status === 401 || err.status === 403)) {
+              this.lastCheck = Date.now();
+              return this.userService.checkLogin().pipe(
+                switchMap(() => observableThrowError(err))
+              );
             } else {
               return observableThrowError(err);
             }
