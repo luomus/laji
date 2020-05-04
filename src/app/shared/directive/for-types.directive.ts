e1bf59595522110bbc58fda342c92cba3c943007
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-05-04 09:57:58 +0300

    refactor ForTypesDirective

--- src/app/shared/directive/for-types.directive.ts
+++ src/app/shared/directive/for-types.directive.ts
@@ -1,35 +1,27 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnChanges, SimpleChanges, TemplateRef, ViewContainerRef } from '@angular/core';
 import { environment } from '../../../environments/environment';
-
-interface ILajiForTypesInput {
-  types: string[];
-  exclude: boolean;
-}
 
 @Directive({
   selector: '[lajiForTypes]'
 })
-export class ForTypesDirective {
+export class ForTypesDirective implements OnChanges {
+
+  @Input() lajiForTypesExclude = false;
+  @Input() lajiForTypes = [];
+
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef
   ) { }
 
-  @Input() set lajiForTypes(types: string[] | ILajiForTypesInput) {
-    let rev = false;
-    if (!(types instanceof Array)) {
-      types = <ILajiForTypesInput>types;
-      rev = types.exclude;
-      types = types.types;
-    }
+  ngOnChanges(changes: SimpleChanges) {
     if (
-      (!rev && types.includes(environment.type))
-      || (rev && !types.includes(environment.type))
+        (!this.lajiForTypesExclude && this.lajiForTypes.includes(environment.type))
+        || (this.lajiForTypesExclude && !this.lajiForTypes.includes(environment.type))
     ) {
       this.viewContainer.createEmbeddedView(this.templateRef);
     } else {
       this.viewContainer.clear();
     }
   }
-
 }
