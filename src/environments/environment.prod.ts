5553416a38cfe883fa267eca961fa1bededf1585
Olli Raitio <olli.raitio@helsinki.fi>
2020-05-26 13:03:48 +0300

    enable syke butterfly form for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   forceLogin: false,
   disableAnalytics: false,
   systemID: 'KE.389',
-  formWhitelist: ['MHL.51', 'JX.519', 'MHL.9', 'MHL.53', 'MHL.25', 'JX.652', 'MHL.1', 'MHL.3', 'MHL.6', 'MHL.33', 'MHL.50', 'MHL.57', , 'MHL.65', 'MHL.45', 'MHL.75'],
+  formWhitelist: ['MHL.51', 'JX.519', 'MHL.9', 'MHL.53', 'MHL.25', 'JX.652', 'MHL.1', 'MHL.3', 'MHL.6', 'MHL.33', 'MHL.50', 'MHL.57', , 'MHL.65', 'MHL.45', 'MHL.75', 'MHL.59'],
   massForms: ['JX.519', 'JX.652', 'MHL.33A'],
   defaultForm: 'JX.519',
   nafiForm: 'MHL.6',
@@ -32,7 +32,7 @@ export const environment = {
   saveObservations: {
     citizenScienceForms: ['JX.652', 'MHL.6', 'MHL.53', 'MHL.25'],
     birdMonitoringForms: ['MHL.1', 'MHL.3', 'MHL.65', 'MHL.75'],
-    researchProjects: ['MHL.33', 'MHL.50', 'MHL.57', 'MHL.45']
+    researchProjects: ['MHL.33', 'MHL.50', 'MHL.57', 'MHL.45', 'MHL.59']
   },
   loginUrl: 'https://login.laji.fi/login',
   selfPage: 'https://login.laji.fi/self',
