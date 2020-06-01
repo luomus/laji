76d641f53532fdc0361fc60c468c9e1e0979a0be
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-06-02 01:06:38 +0300

    show obsolete names on taxon card

--- src/app/shared/taxon-name/taxon-name.component.ts
+++ src/app/shared/taxon-name/taxon-name.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'laji-taxon-name',
@@ -16,6 +17,7 @@ export class TaxonNameComponent {
     vernacularName?: string | {[lang: string]: string};
     scientificNameAuthorship?: string;
     alternativeVernacularName?;
+    obsoleteVernacularName?;
   };
   @Input() taxonID: string;
   @Input() addLink = true;
@@ -23,8 +25,20 @@ export class TaxonNameComponent {
   @Input() warningOnMissingTaxonID = false;
   @Input() showScientificNameFirst = false;
 
+  constructor(private translate: TranslateService) {}
 
   onTaxonLinkClick(event: MouseEvent) {
     event.stopPropagation();
+  }
+
+  getAlternativeNames() {
+    const alternativeNames: string[] = [];
+    if (this.taxon.alternativeVernacularName) {
+      alternativeNames.push(...this.taxon.alternativeVernacularName[this.translate.currentLang]);
+    }
+    if (this.taxon.obsoleteVernacularName) {
+      alternativeNames.push(...this.taxon.obsoleteVernacularName[this.translate.currentLang]);
+    }
+    return alternativeNames.reduce((prev, curr) => prev += (', ' + curr));
   }
 }
