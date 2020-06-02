f6359cff4b053e6f3fa2c7261c506811de443b03
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-06-02 12:31:30 +0300

    VIR show correct root collections in prod

--- projects/vir/src/environments/environment.prod.ts
+++ projects/vir/src/environments/environment.prod.ts
@@ -28,7 +28,7 @@ export const environment = {
     birdMonitoringForms: ['MHL.65', 'MHL.33', 'MHL.6'],
     researchProjects: ['MHL.1', 'MHL.50', 'MHL.57']
   },
-  rootCollections: ['HR.128'],
+  rootCollections: ['HR.3552', 'HR.3551', 'HR.1915'],
   loginUrl: '/user/viranomaiset',
   selfPage: 'https://login.laji.fi/self',
   apiBase: '/api',
