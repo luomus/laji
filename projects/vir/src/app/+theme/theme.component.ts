d4434363ab619380b0b26902744a29a88b90d71b
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-08-02 21:38:30 +0300

    [vir] theme page redesign

--- projects/vir/src/app/+theme/theme.component.ts
+++ projects/vir/src/app/+theme/theme.component.ts
@@ -3,7 +3,10 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'laji-theme',
   templateUrl: './theme.component.html',
-  styleUrls: ['../../../../../src/app/+theme/theme.component.css'],
+  styleUrls: [
+    '../../../../../src/app/+theme/theme.component.css',
+    './theme.component.scss'
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ThemeComponent {
