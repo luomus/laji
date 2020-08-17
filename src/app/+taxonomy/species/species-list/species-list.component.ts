17c38325258614007610124b37dff5b777bf78d7
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-08-17 12:50:04 +0300

    browse species header changes

--- src/app/+taxonomy/species/species-list/species-list.component.ts
+++ src/app/+taxonomy/species/species-list/species-list.component.ts
@@ -46,8 +46,6 @@ export class SpeciesListComponent implements OnInit, OnChanges, OnDestroy {
   @Input() searchQuery: TaxonomySearchQuery;
   @Input() visible: boolean;
   @Input() showDownloadAndBrowse = true;
-  @Input() countStartText = '';
-  @Input() countEndText = '';
 
   loading = false;
   downloadLoading = false;
