3a734eadaac16b6c54a93e44432f5a4119ffbc32
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-09-14 11:54:11 +0300

    [VIR] tweak my downloads component

--- projects/vir/src/app/+usage/pages/usage-my-downloads/usage-my-downloads.component.ts
+++ projects/vir/src/app/+usage/pages/usage-my-downloads/usage-my-downloads.component.ts
@@ -1,12 +1,21 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IDownloadRequest, VirDownloadRequestsService } from '../../../service/vir-download-requests.service';
-import { finalize } from 'rxjs/operators';
+import { finalize, take } from 'rxjs/operators';
 
 @Component({
   selector: 'vir-usage-my-downloads',
-  templateUrl: './usage-my-downloads.component.html',
-  styleUrls: ['./usage-my-downloads.component.scss'],
+  template: `
+    <div class="container mt-6">
+      <laji-spinner [spinning]="loading" [overlay]="true">
+        <vir-data-table
+          type="user"
+          [data]="downloadRequests$ | async"
+          [showDownloadMenu]="false"
+        ></vir-data-table>
+      </laji-spinner>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UsageMyDownloadsComponent {
@@ -19,6 +28,7 @@ export class UsageMyDownloadsComponent {
   ) {
     this.loading = true;
     this.downloadRequests$ = this.virDownloadRequestsService.findMyDownloadRequests().pipe(
+      take(1),
       finalize(() => {
         this.loading = false;
         this.cdr.markForCheck();
