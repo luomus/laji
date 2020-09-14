790d34d8cc732f251ec4daf72ba3dcb1a8e4a143
Giorgio Persichini <lucia-giorgio@hotmail.it>
2020-09-14 11:48:52 +0300

    [#174730804] - alternativeVernacularName, Synonim  match option

--- src/app/shared/service/taxon-autocomplete.service.ts
+++ src/app/shared/service/taxon-autocomplete.service.ts
@@ -25,25 +25,28 @@ export class TaxonAutocompleteService {
   }
 
   getMatchingType(payload: any) {
-     const scientificName = payload['cursiveName'] ? '<i>' + payload['scientificName'] + '</i>' : payload['scientificName'];
+     const scientificName = payload['cursiveName'] ? '<span class="cursive">' + payload['scientificName'] + '</span>' : payload['scientificName'];
      const taxonRank = payload['taxonRankId'] ? ' (' + this.labelPipe.transform(payload['taxonRankId']) + ') ' : '';
      const informalGroupIcon = '<div class="informal-group-image ' + payload['informalTaxonGroups'][0].id +'"></div>';
-     const flag = '<div class="autocomplete-small-flag finnish-flag"></div>';
-     const vernacularName = payload['vernacularName'] !== '' ? payload['vernacularName'] + '(' + this.translate.currentLang + ') - ' + scientificName
+     const flag = payload['finnish'] ? '<div class="autocomplete-small-flag finnish-flag"></div>' : '<div class="autocomplete-small-flag no-border"></div>';
+     const vernacularName = payload['vernacularName'] !== '' ? payload['vernacularName'] + '(' + this.translate.currentLang + ') (' + payload['matchingName'] + ') - ' + scientificName + ' '
      : scientificName + ' (' + payload['matchingName'] + ') - ' + scientificName;
+     const otherVernacularName = payload['vernacularName'] !== '' ? payload['vernacularName'] + '(' + this.translate.currentLang + ') - ' + scientificName
+     : scientificName + ' (' + payload['matchingName'] + ') - ' + scientificName;
+     const synonim = scientificName + ' (' + payload['matchingName'] + ') ';
 
     switch (payload['nameType']) {
       case 'MX.scientificName':
-        return ''+ scientificName + '' + taxonRank + '<div class="flag-informalIcon">' + informalGroupIcon + flag + '</div>';
-      case 'MX.euringCode':
-        return "Euring Code";
+        return ''+ scientificName + '<div class="responsive_container"> ' + taxonRank + '<div class="flag_icon">' + informalGroupIcon + flag + '</div></div>';
+      case 'MX.hasSynonym':
+        return synonim + '<div class="responsive_container"> ' + taxonRank + '<div class="flag_icon">' + informalGroupIcon + flag + '</div>';
       case 'MX.birdlifeCode':
         return "birdlifeCode";
       case 'MX.vernacularName':
-        return ''+ vernacularName + '' + taxonRank + '<div class="flag-informalIcon">' + informalGroupIcon + flag + '</div>';
+        return ''+ vernacularName + '<div class="responsive_container">' + taxonRank + '<div class="flag_icon">' + informalGroupIcon + flag + '</div></div>';
       case 'MX.alternativeVernacularName':
       case 'MX.obsoleteVernacularName':
-        return "alternativeVernacularName"
+        return ''+ otherVernacularName + '<div class="responsive_container"> ' + taxonRank + '<div class="flag_icon">' + informalGroupIcon + flag + '</div></div>';
       case 'MX.tradeName':
         return "tradeName";
     }
@@ -53,16 +56,15 @@ export class TaxonAutocompleteService {
     switch (payload['nameType']) {
       case 'MX.scientificName':
         return payload['scientificName'];
-      case 'MX.euringCode':
-        return "Euring Code";
+      case 'MX.hasSynonym':
+        return payload['scientificName'];
       case 'MX.birdlifeCode':
         return "birdlifeCode";
       case 'MX.vernacularName':
-        return payload['vernacularName'] !== '' ? payload['vernacularName'] : payload['scientificName'] ;
+        return payload['vernacularName'] !== '' ? payload['vernacularName'] : payload['scientificName'];
       case 'MX.alternativeVernacularName':
-        return "alternativeVernacularName";
       case 'MX.obsoleteVernacularName':
-        return "obsoleteVernacularName";
+        return payload['vernacularName'] !== '' ? payload['vernacularName'] : payload['scientificName'];
       case 'MX.tradeName':
         return "tradeName";
     }
