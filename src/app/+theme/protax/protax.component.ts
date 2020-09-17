c6eb4724247d901b150d2f3b8d397c49bbfbab0b
aorin <meeri.rannisto@gmail.com>
2020-09-17 15:56:40 +0300

    display error message to user

--- src/app/+theme/protax/protax.component.ts
+++ src/app/+theme/protax/protax.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ProtaxApi} from './protax-api';
 import {ExportService} from '../../shared/service/export.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'laji-protax',
@@ -11,7 +12,8 @@ import {ExportService} from '../../shared/service/export.service';
 export class ProtaxComponent implements OnInit {
   constructor(
     private protaxApi: ProtaxApi,
-    private exportService: ExportService
+    private exportService: ExportService,
+    private translate: TranslateService
   ) { }
 
   ngOnInit() {
@@ -20,6 +22,12 @@ export class ProtaxComponent implements OnInit {
   analyseData(formData: FormData) {
     this.protaxApi.analyse(formData).subscribe(result => {
       this.exportService.exportArrayBuffer(result, 'protax_output', 'txt');
+    }, err => {
+      if (err.status === 400) {
+        alert(this.translate.instant('theme.protax.invalidSequence'));
+      } else {
+        alert(this.translate.instant('theme.protax.genericError'));
+      }
     });
   }
 }
