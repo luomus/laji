9e15823a7f65ad802be1a26c6e75bb7cc9a17052
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-09-21 09:33:18 +0300

    make translation script little more user friendly

--- bin/send-to-crowdin.sh
+++ bin/send-to-crowdin.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo "Fetching the latest laji-cli tools"
+docker pull luomus/laji-cli >/dev/null 2>&1
+
 echo "Sending laji.fi to crowdin"
-docker run --rm --env-file .env -v $(pwd)/../src/i18n:/data luomus/laji-cli \
+docker run --rm --env-file ${SCRIPT_PATH}/.env -v ${SCRIPT_PATH}/../src/i18n:/data luomus/laji-cli \
   crowdin:send:json LAJI \
   fi:/data/fi.json  \
   en:/data/en.json \
   sv-FI:/data/sv.json
 
 echo "Sending viranomaiset.laji.fi to crowdin"
-docker run --rm --env-file .env -v $(pwd)/../projects/vir/i18n:/data luomus/laji-cli \
+docker run --rm --env-file ${SCRIPT_PATH}/.env -v ${SCRIPT_PATH}/../projects/vir/i18n:/data luomus/laji-cli \
   crowdin:send:json VIR \
   fi:/data/fi.json  \
   en:/data/en.json \
