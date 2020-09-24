dcaa3670787be6181b7a5c69c0b4a751b342961f
Giorgio Persichini <lucia-giorgio@hotmail.it>
2020-09-24 14:09:10 +0300

    [#174814762] - add social networks meta tags title/description

--- src/app/+news/news.component.ts
+++ src/app/+news/news.component.ts
@@ -6,6 +6,7 @@ import { Logger } from '../shared/logger/logger.service';
 import { News } from '../shared/model/News';
 import { distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { NewsStore } from './news.store';
+import { BrowserModule, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'laji-news',
@@ -21,8 +22,10 @@ export class NewsComponent implements OnInit, OnDestroy {
               private newsService: NewsService,
               private cd: ChangeDetectorRef,
               private logger: Logger,
-              private store: NewsStore
+              private store: NewsStore,
+              private metaService: Meta
   ) {
+    //this.addTag(newsItem);
   }
 
   ngOnInit() {
@@ -34,7 +37,17 @@ export class NewsComponent implements OnInit, OnDestroy {
       map(params => params['id']),
       switchMap(id => this.store.state.current && this.store.state.current.id === id ?
         ObservableOf(this.store.state.current) : this.newsService.get(id))
-    ).subscribe(newsItem => this.store.setCurrent(newsItem));
+    ).subscribe(newsItem => {
+      this.store.setCurrent(newsItem);
+      this.addTag(newsItem);
+    });
+  }
+
+  addTag(news) {
+    this.metaService.addTag({ property: 'og:title', content: news.title });
+    this.metaService.addTag({ property: 'og:description', content: 'Content Title for social media' });
+    this.metaService.addTag({ property: 'twitter:title', content: news.title });
+    this.metaService.addTag({ property: 'twitter:description', content: 'Content Title for social media' });
   }
 
   ngOnDestroy() {
