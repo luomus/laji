490744916d2b8c0c6155da58d3556a87664d93a4
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2020-09-26 14:32:19 +0300

    identification cursive names

--- src/app/+taxonomy/taxon/info-card/taxon-identification/taxon-identification.component.ts
+++ src/app/+taxonomy/taxon/info-card/taxon-identification/taxon-identification.component.ts
@@ -37,7 +37,7 @@ export class TaxonIdentificationComponent implements OnInit, OnChanges {
             ...result.map(
               taxon => this.taxonomyApi.species({
                 id: taxon.id,
-                selectedFields: 'id,vernacularName,scientificName',
+                selectedFields: 'id,vernacularName,scientificName,cursiveName',
                 includeMedia: true,
                 pageSize: 8,
                 sortOrder: 'observationCountFinland DESC'
@@ -55,7 +55,6 @@ export class TaxonIdentificationComponent implements OnInit, OnChanges {
       ).subscribe(res => {
         this.taxonChildren = res;
         this.cdr.markForCheck();
-        console.log(res);
       });
       return;
     }
