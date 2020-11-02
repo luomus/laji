ec3f902f377e49bbeba8b4891d3aee2052ee7a5d
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-11-02 12:32:35 +0200

    update test

--- e2e/src/+home/home.po.ts
+++ e2e/src/+home/home.po.ts
@@ -1,11 +1,14 @@
 import { browser, by, element } from 'protractor';
 
 export class HomePage {
+
+  private pageTitleElem = element(by.css('.home-main > h1'));
+
   navigateTo(lang = 'fi') {
     return browser.get('/' + (lang !== 'fi' ? lang : '')) as Promise<void>;
   }
 
   getPageTitle() {
-    return element(by.id('main-title')).getText() as Promise<string>;
+    return this.pageTitleElem.getText() as Promise<string>;
   }
 }
