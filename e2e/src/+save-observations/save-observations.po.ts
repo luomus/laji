6b23f87907e8f0f431637cbc72e9b66066f03317
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2020-11-03 15:24:27 +0200

    wait for the form elements to be visible in the test when fetching them

--- e2e/src/+save-observations/save-observations.po.ts
+++ e2e/src/+save-observations/save-observations.po.ts
@@ -42,7 +42,10 @@ export class SaveObservationsPage {
     return this.legInput.getAttribute('value') as Promise<string>;
   }
 
-  getSimpleFormsIdByIdx(idx: number) {
+  async getSimpleFormsIdByIdx(idx: number) {
+    const EC = protractor.ExpectedConditions;
+    const hasForms = EC.presenceOf(this.simpleForms.first());
+    await browser.wait(hasForms, 1000);
     return this.simpleForms.get(idx).getText() as Promise<string>;
   }
 }
