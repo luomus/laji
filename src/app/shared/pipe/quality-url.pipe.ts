eeeb6aef94f69cb198e39828d18fbf8709ce51ce
Markus Lumme <markus.lumme@helsinki.fi>
2020-11-06 14:49:21 +0200

    Adjusted collection icons and changed their size in datatable. Removed forgotten console.log from a qualityUrl-pipe.

--- src/app/shared/pipe/quality-url.pipe.ts
+++ src/app/shared/pipe/quality-url.pipe.ts
@@ -7,7 +7,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class QualityUrlPipe implements PipeTransform {
   recordPath = 'static/images/quality-icons/record/'
   collectionPath = 'static/images/quality-icons/collection/'
-  transform(value: any, args?: any): any {
+  
+  transform(value: any): any {
     switch (value) {
       case 'EXPERT_VERIFIED':
         return this.recordPath + 'expert_verified.svg'
