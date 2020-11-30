64b4bfc748e645ccda7d25f0dab75b1f4a0e8a7b
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-11-30 11:59:46 +0200

    update error handling to return 500 response code and fix common cases for errors

--- projects/laji/src/app/+news/news.routes.ts
+++ projects/laji/src/app/+news/news.routes.ts
@@ -1,8 +1,14 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewsComponent } from './news.component';
+import { NotFoundComponent } from '../shared/not-found/not-found.component';
 
 export const newsRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: NotFoundComponent
+  },
   {
     path: ':id',
     pathMatch: 'full',
