50e2c09023f3133dc05a2600f3c9d1bff305fb30
Giorgio Persichini <lucia-giorgio@hotmail.it>
2020-12-10 14:32:07 +0200

    [#175160201] - laji-checkbox - new status excluded

--- projects/laji/src/app/shared-modules/select/checkbox/checkbox.component.ts
+++ projects/laji/src/app/shared-modules/select/checkbox/checkbox.component.ts
@@ -11,6 +11,7 @@ export class CheckboxComponent {
   @ViewChild('checkbox', { static: true }) checkbox: ElementRef<HTMLInputElement>;
 
   @Input() threeState = false;
+  @Input() hasSubCategories = false;
   @Output() valueChange = new EventEmitter();
 
   _value: boolean;
@@ -26,7 +27,10 @@ export class CheckboxComponent {
       this.stateClass = 'checked';
     } else if (value === false && this.threeState) {
       this.checkbox.nativeElement.indeterminate = true;
-      this.stateClass = 'negative';
+      this.stateClass = this.hasSubCategories ? 'negative' : 'excluded';
+    } else if (value === false && !this.threeState) {
+      this.checkbox.nativeElement.indeterminate = true;
+      this.stateClass = this.hasSubCategories ? 'negative' : 'excluded';
     } else {
       this.checkbox.nativeElement.checked = false;
       this.stateClass = 'clear';
