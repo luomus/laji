f2ec643a8386450c1989d442f942d811d33b9ef0
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2020-12-14 09:01:51 +0200

    fix IE11 support

--- projects/laji/src/polyfills.ts
+++ projects/laji/src/polyfills.ts
@@ -18,6 +18,10 @@
  * BROWSER POLYFILLS
  */
 
+
+import 'core-js/modules/es.array.includes';
+import 'core-js/modules/es.object.values';
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js'; // Run `npm install --save classlist.js`.
 /**
