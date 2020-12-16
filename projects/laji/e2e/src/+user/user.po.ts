d8b891f11d3c3212a8f57e26cf1b79c16951c98a
Olli Raitio <olli.raitio@helsinki.fi>
2020-12-16 16:50:00 +0200

    grow external login page wait for auth elem

--- projects/laji/e2e/src/+user/user.po.ts
+++ projects/laji/e2e/src/+user/user.po.ts
@@ -59,7 +59,7 @@ export class UserPage {
   async doExternalLogin(user = DEFAULT_TEST_USER) {
     await browser.waitForAngularEnabled(false);
 
-    await browser.wait(EC.visibilityOf(this.authLocal), 1000);
+    await browser.wait(EC.visibilityOf(this.authLocal), 2000);
     await this.authLocal.click();
     await this.authUsername.sendKeys(user);
     await this.authPassword.sendKeys(TEST_USERS[user].pw);
