0d7ce465e7f4c0cad05552a9a92db058f5e8411c
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2020-12-18 12:07:43 +0200

    Refactor about pages [#176219029]

--- projects/laji/src/app/+information/information.component.ts
+++ projects/laji/src/app/+information/information.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, of as ObservableOf, Subscription } from 'rxjs';
-import { TranslateService } from '@ngx-translate/core';
+import { Observable, of, of as ObservableOf, Subscription } from 'rxjs';
 import { Logger } from '../shared/logger/logger.service';
 import { Information } from '../shared/model/Information';
 import { Title } from '@angular/platform-browser';
 import { LajiApi, LajiApiService } from '../shared/service/laji-api.service';
-import { distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
-import { InformationStore } from './information.store';
+import { catchError, delay, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
+import { HeaderService } from '../shared/service/header.service';
 
 @Component({
   selector: 'laji-information',
@@ -15,55 +14,37 @@ import { InformationStore } from './information.store';
   styleUrls: ['./information.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InformationComponent implements OnDestroy {
+export class InformationComponent {
 
   information$: Observable<Information>;
-  private paramSub: Subscription;
-  private id: number;
-  private informationLoaded = false;
 
   constructor(private route: ActivatedRoute,
-              private translate: TranslateService,
               private lajiApi: LajiApiService,
               private logger: Logger,
-              private cd: ChangeDetectorRef,
-              private title: Title,
-              private store: InformationStore
+              private headerService: HeaderService,
+              private title: Title
   ) {
-
-    this.information$ = this.store.state$.pipe(
-      map(state => state.info),
-      distinctUntilChanged(),
-      filter(info => !!info),
-      tap(info => this.title.setTitle(info.title + ' | ' + this.title.getTitle()))
-    );
-
-    this.paramSub = this.route.params.pipe(
+    this.information$ = this.route.params.pipe(
       map(params => params['id']),
-      tap(id => this.id = +id),
-      switchMap(id => this.getInformation(id))
-    )
-      .subscribe(information => {
-          this.informationLoaded = true;
-          this.store.setInformation(information);
-        },
-        err => {
-          this.logger.warn('Failed to fetch root informations', err);
-          this.cd.markForCheck();
-        });
+      switchMap(id => this.lajiApi.get(LajiApi.Endpoints.information, id).pipe(
+        filter(info => info.id === id)
+      )),
+      delay(0),
+      tap(info => this.updateHeaders(info)),
+      catchError(err => {
+        this.logger.warn('Failed to fetch root informations', err);
+        return of(null);
+      })
+    );
   }
 
-  ngOnDestroy() {
-    if (this.paramSub) {
-      this.paramSub.unsubscribe();
-    }
-  }
+  private updateHeaders(info: Information): void {
+    const pageTitle = info.title + ' | ' + this.title.getTitle();
+    const paragraph = HeaderService.getDescription(info.content || '');
 
-  private getInformation(id): Observable<Information> {
-    if (this.store.state.info && this.store.state.info.id === id) {
-      return ObservableOf(this.store.state.info);
-    }
-    return this.lajiApi.get(LajiApi.Endpoints.information, id, {lang: this.translate.currentLang});
+    this.title.setTitle(pageTitle);
+    this.headerService.createTwitterCard(pageTitle);
+    this.headerService.updateMetaDescription(paragraph);
   }
 
 }
