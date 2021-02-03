39f7269997fa4345d4a6907144f4b697ab3902ea
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2021-02-03 09:41:29 +0200

    minor tweak

--- projects/laji/src/app/+user/profile/profile-please-login.component.ts
+++ projects/laji/src/app/+user/profile/profile-please-login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'laji-user',
   template: `
     <div class="container">
       <alert type="warning" style="padding-top: 20px">
