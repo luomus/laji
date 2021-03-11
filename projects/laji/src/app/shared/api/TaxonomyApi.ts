ac2980a5d3b0b6bb8cebc45ca1e926a32c4f17a0
Ville-Matti Riihikoski <ville-matti.riihikoski@helsinki.fi>
2021-03-11 07:37:52 +0200

    Revert "Taxon identification #174428349 #175142664"

--- projects/laji/src/app/shared/api/TaxonomyApi.ts
+++ projects/laji/src/app/shared/api/TaxonomyApi.ts
@@ -42,15 +42,6 @@ export class TaxonomyApi {
   constructor(protected http: HttpClient) {
   }
 
-  public taxonomyList(lang?: string, extraHttpRequestParams?: any): Observable<PagedResult<Taxonomy>> {
-    const path = this.basePath + '/taxa';
-    const queryParameters = {...Util.removeFromObject(extraHttpRequestParams)};
-    if (lang !== undefined) {
-      queryParameters['lang'] = lang;
-    }
-    return this.http.get<PagedResult<Taxonomy>>(path, {params: queryParameters});
-  }
-
   /**
    * return taxon data with the given query
    *
