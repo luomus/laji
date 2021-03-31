d0deda1482344665ce28ea7fd3551639ff12bf77
Giorgio Persichini <lucia-giorgio@hotmail.it>
2021-03-31 15:05:33 +0300

    create new api for the page - [#177505436]

--- projects/laji/src/app/+viewer/viewer-media/viewer-media.component.ts
+++ projects/laji/src/app/+viewer/viewer-media/viewer-media.component.ts
@@ -1,35 +1,43 @@
 import { Component, OnDestroy, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { map, mergeMap } from 'rxjs/operators';
 import { Image } from 'projects/laji/src/app/shared/gallery/image-gallery/image.interface';
+import { IdService } from '../../shared/service/id.service';
+import { ImageApi } from '../../shared/api/ImageApi';
+import { DocumentApi } from '../../shared/api/DocumentApi';
+import { TranslateService } from '@ngx-translate/core';
+
+
 
 
 @Component({
   selector: 'laji-viewer-media',
   templateUrl: './viewer-media.component.html',
   styleUrls: ['./viewer-media.component.scss'],
+  providers: [IdService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ViewerMediaComponent implements OnInit, OnDestroy {
-  public images: Image[];
+  public images: Observable<Image>;
+  public uri: string;
   private subQuery: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private imageApi: ImageApi,
+    private translate: TranslateService,
+    ) { }
 
   ngOnInit() {
-    this.subQuery = this.route.queryParams.pipe(
-        map(params => ({
-         image: {
-           copyrightOwner : params['owner'],
-           licenseId: params['rights'],
-           fullURL: params['uri']
-         }
-        })
-      )
-    ).subscribe(params => {
-      this.images = [params['image']];
+    this.subQuery = this.route.queryParams.subscribe(params => {
+      this.uri = IdService.getId(params['uri']);
+      console.log(this.uri);
+
     });
+
+    this.images = this.imageApi.imageFindById(this.uri, this.translate.currentLang);
+    console.log(this.images);
 
   }
 
