bd682215bb6ab95ffbcf8b64c2e54cc8eec50c6d
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2021-04-13 11:16:44 +0300

    make info modal/popover keyboard navigable

--- projects/laji/src/app/shared-modules/info/info.module.ts
+++ projects/laji/src/app/shared-modules/info/info.module.ts
@@ -4,13 +4,15 @@ import { InfoComponent } from './info/info.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { UtilitiesModule } from '../utilities/utilities.module';
+import { LajiUiModule } from 'projects/laji-ui/src/public-api';
 
 @NgModule({
   imports: [
     CommonModule,
     PopoverModule,
     ModalModule,
-    UtilitiesModule
+    UtilitiesModule,
+    LajiUiModule
   ],
   declarations: [InfoComponent],
   exports: [InfoComponent]
