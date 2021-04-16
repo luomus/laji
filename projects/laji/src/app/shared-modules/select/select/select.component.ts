4ec25475275a27b1190535f7c9afa6eb0ae80250
Giorgio Persichini <lucia-giorgio@hotmail.it>
2021-04-16 07:24:21 +0300

    fix issue selects with filter(not the best) - [#177759152]

--- projects/laji/src/app/shared-modules/select/select/select.component.ts
+++ projects/laji/src/app/shared-modules/select/select/select.component.ts
@@ -215,7 +215,8 @@ export class SelectComponent<T extends idType|SelectOption = string> implements 
       });
     });
 
-    this.open = this.open || !!this.selectedOptions.length;
+    // this.open = this.open || !!this.selectedOptions.length;
+    this.open = this.useFilter ? this.open : this.open || !!this.selectedOptions.length;
   }
 
   private isSelectOptions(option: idType|SelectOption): option is SelectOption {
