f24baf1539a451ce36d7a3782af6ea06f5cb62ed
Markus Lumme <markus.lumme@helsinki.fi>
2021-04-29 16:08:28 +0300

    Style adjustments.

--- projects/laji/src/app/shared-modules/document-viewer/abundance-row/abundance-row.component.ts
+++ projects/laji/src/app/shared-modules/document-viewer/abundance-row/abundance-row.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
 @Component({
   selector: 'laji-abundance-row',
   template: `
-    <p>{{abundanceString}} [<span class="abundance-unit">{{abundanceUnit}}</span>]</p>
+    <p>{{abundanceString}}<span class="abundance-unit">[{{abundanceUnit}}]</span></p>
   `,
   styleUrls: ['./abundance-row.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
