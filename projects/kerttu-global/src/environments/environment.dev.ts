f8f034155831911e929c7fc5f4f90d864f398c64
aorin <meeri.rannisto@gmail.com>
2021-05-07 14:33:24 +0300

    add environment file for kerttu global dev site

---
+++ projects/kerttu-global/src/environments/environment.dev.ts
@@ -0,0 +1,18 @@
+/* tslint:disable:max-line-length */
+import { Global } from './global';
+
+export const environment = {
+  type: Global.type.kerttuGlobal,
+  base: 'https://kerttu-global-dev.laji.fi',
+  production: true,
+  forceLogin: true,
+  disableAnalytics: true,
+  sourceKotka: 'KE.3',
+  systemID: 'KE.1181',
+  apiBase: '/api',
+  loginCheck: '',
+  loginUrl: 'https://fmnh-ws-test.it.helsinki.fi/laji-auth/login',
+  selfPage: 'https://fmnh-ws-test.it.helsinki.fi/laji-auth/self',
+  kerttuApi: 'https://staging-kerttu-backend.laji.fi',
+  protaxApi: 'https://protax-api-protax-api-staging.rahtiapp.fi',
+};
