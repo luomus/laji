bcdd58809faf7d1cb16370b35db19979c75517c7
aorin <meeri.rannisto@gmail.com>
2021-05-24 18:10:38 +0300

    remove other languages than en from kerttu global

--- projects/kerttu-global/src/app/kerttu-global-shared/service/lazy-translate-loader.ts
+++ projects/kerttu-global/src/app/kerttu-global-shared/service/lazy-translate-loader.ts
@@ -4,6 +4,7 @@ import { map, switchMap } from 'rxjs/operators';
 
 export class LazyTranslateLoader implements TranslateLoader {
   getTranslation(lang: string): Observable<any> {
+    lang = 'en';
     return from(import(`../../../../../laji/src/i18n/${lang}.json`)).pipe(
       switchMap(base => from(import(`../../../../i18n/${lang}.json`)).pipe(
         map(local => ({...base, ...local}))
