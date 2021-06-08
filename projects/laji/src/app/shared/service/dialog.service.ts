5e755f84c7e5e338b6e23ef453e6b759e4cc5c94
Olli Raitio <olli.raitio@helsinki.fi>
2021-06-08 16:23:38 +0300

    improve dialog service typings

--- projects/laji/src/app/shared/service/dialog.service.ts
+++ projects/laji/src/app/shared/service/dialog.service.ts
@@ -5,6 +5,31 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 import { ConfirmModalComponent } from './confirm-modal.component';
 import { map, take } from 'rxjs/operators';
 
+interface DialogConfig {
+  message: string;
+  promptValue?: string;
+  showCancel?: boolean;
+  prompt?: boolean;
+  confirmLabel?: string;
+}
+
+interface AlertConfig extends Pick<DialogConfig, 'message'> {
+  showCancel: false;
+}
+
+interface ConfirmConfig extends Pick<DialogConfig, 'message' | 'confirmLabel'> {
+  showCancel: true;
+}
+
+interface PromptConfig extends Pick<DialogConfig, 'message' | 'confirmLabel' | 'promptValue'> {
+  showCancel: true;
+  prompt: true;
+}
+
+interface InitialState extends DialogConfig {
+  promptValue: string;
+}
+
 @Injectable({providedIn: 'root'})
 export class DialogService {
 
@@ -17,26 +42,24 @@ export class DialogService {
     if (this.platformService.isServer) {
       return ObservableOf(onServer);
     }
-    return this.createDialog(message, false);
+    return this.createDialog<AlertConfig, boolean>({message, showCancel: false});
   }
 
   confirm(message: string, confirmLabel?: string, onServer = false): Observable<boolean> {
     if (this.platformService.isServer) {
       return ObservableOf(onServer);
     }
-    return this.createDialog(message, true, confirmLabel);
+    return this.createDialog<ConfirmConfig, boolean>({message, showCancel: true, confirmLabel});
   }
 
   prompt(message: string, confirmLabel?: string, _default?: string): Observable<string | null> {
-    return this.createDialog(message, true, confirmLabel, true, _default);
+    return this.createDialog<PromptConfig, string | null>({message, showCancel: true, confirmLabel, prompt: true, promptValue: _default});
   }
 
-  private createDialog(message: string, showCancel?: boolean, confirmLabel?: string): Observable<boolean>;
-  private createDialog(message: string, showCancel: boolean, confirmLabel: string | undefined, prompt: true, promptDefault?: string): Observable<string | null>;
-  private createDialog(message: string, showCancel = true, confirmLabel?: string, prompt = false, promptDefault?: string): Observable<boolean | string | null> {
-      const initialState = {message, prompt, promptValue: promptDefault ?? ''} as any;
-      if (typeof confirmLabel === 'string') {
-      initialState.confirmLabel = confirmLabel;
+  private createDialog<T extends DialogConfig, R = (boolean | (string | null))>(options: T): Observable<R> {
+    const initialState: InitialState = {showCancel: true, prompt: false,  promptValue: '', ...options};
+    if (typeof options.confirmLabel === 'string') {
+      initialState.confirmLabel = options.confirmLabel;
     }
     const modalRef = this.modalService.show(ConfirmModalComponent, {
         backdrop: 'static',
@@ -45,7 +68,7 @@ export class DialogService {
     });
 
     return modalRef.onHide.pipe(
-      map(() => modalRef.content.value),
+      map(() => modalRef.content.value as unknown as R),
       take(1)
     );
   }
