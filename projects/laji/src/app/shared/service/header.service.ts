1b333fd4127743e9119f4f006f78834e5fc4d691
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2021-06-11 11:16:41 +0300

    refactor HeaderService

--- projects/laji/src/app/shared/service/header.service.ts
+++ projects/laji/src/app/shared/service/header.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable, OnDestroy, Renderer2, RendererFactory2, ViewEncapsulation } from '@angular/core';
 import { ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
-import { Observable, of as ObservableOf, Subscription } from 'rxjs';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { DOCUMENT, Location } from '@angular/common';
 import { environment } from '../../../environments/environment';
 import { Global } from '../../../environments/global';
@@ -9,7 +9,6 @@ import { PlatformService } from './platform.service';
 import { Meta, Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 import { LocalizeRouterService } from '../../locale/localize-router.service';
-import { RouteDataService } from './route-data.service';
 
 let MAIN_TITLE: string;
 switch (environment.type) {
@@ -25,62 +24,92 @@ switch (environment.type) {
 }
 const MAIN_DESCRIPTION = 'footer.intro1';
 
-const ALL_META_KEYS = [
-  'description',
-  'og:description',
-  'twitter:description'
-];
+interface ILinkElement {
+  rel: string;
+  href?: string;
+  hreflang?: string;
+}
 
-const ALL_IMAGE_KEYS = [
-  'og:image',
-  'twitter:image'
-];
+interface IHeaders {
+  title?: string;
+  'twitter:title'?: string;
+  'og:title'?: string;
+  description?: string;
+  'og:description'?: string;
+  'twitter:description'?: string;
+  'og:image'?: string;
+  'twitter:image'?: string;
+  'twitter:card'?: string;
+  'og:url'?: string;
+  canonicalUrl?: ILinkElement;
+  alternativeLinks?: ILinkElement[];
+}
 
-const TWITTER_CARD = [
-  'twitter:card',
-  'twitter:title'
-];
+export const getDescription = (html: string): string => {
+  const firstParagraph = html
+    .split('</p>')[0]
+    .split(/<p.*?>/).pop()
+    .replace(/<[^>]*>?/gm, '');
 
+  if (firstParagraph.length <= 180) {
+    return firstParagraph;
+  }
+  const words = firstParagraph.substring(0, 181).split(' ');
+  let description = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    if (description.length + words[i].length + 1 <= 177) {
+      description += ` ${words[i]}`;
+    } else {
+      break;
+    }
+  }
+  return `${description}...`;
+};
+
+const getRouteTitles = (routeSnapshot: ActivatedRouteSnapshot): string[] => {
+  const titles: string[] = [];
+  if (routeSnapshot.data && routeSnapshot.data['title']) {
+    titles.push(routeSnapshot.data['title'] ?? '');
+  }
+  if (routeSnapshot.firstChild) {
+    return [...getRouteTitles(routeSnapshot.firstChild), ...titles];
+  }
+  return titles;
+};
+
+const removeDuplicates = <T>(arr: T[]) => Array.from(new Set<T>(arr));
+
+const getDeepestChildValue = (routeSnapshot: ActivatedRouteSnapshot, key = 'meta', empty: any = {}): any => {
+  const value = routeSnapshot.data && typeof routeSnapshot.data[key] !== 'undefined' ? routeSnapshot.data[key] : empty;
+  if (routeSnapshot.firstChild) {
+    return getDeepestChildValue(routeSnapshot.firstChild, key, value);
+  }
+  return value;
+};
+
+/**
+ * 1. On NavigationEnd infer headers from route
+ * 2. In components overwrite headers by calling setHeaders
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderService implements OnDestroy {
-
-  private routeSub: Subscription;
+  private inferredHeaders: IHeaders;
   private currentRoute: string;
   private renderer: Renderer2;
-
-  public static getDescription(html: string): string {
-    const firstParagraph = html
-      .split('</p>')[0]
-      .split(/<p.*?>/).pop()
-      .replace(/<[^>]*>?/gm, '');
-
-    if (firstParagraph.length <= 180) {
-      return firstParagraph;
-    }
-    const words = firstParagraph.substring(0, 181).split(' ');
-    let description = words[0];
-
-    for (let i = 1; i < words.length; i++) {
-      if (description.length + words[i].length + 1 <= 177) {
-        description += ` ${words[i]}`;
-      } else {
-        break;
-      }
-    }
-    return `${description}...`;
-  }
+  private routeSub: Subscription;
 
   constructor(
-    @Inject(DOCUMENT) private document,
+    @Inject(DOCUMENT) private document: Document,
     private router: Router,
-    private location: Location,
+    private titleService: Title,
+    private metaService: Meta,
     private platformService: PlatformService,
-    private title: Title,
     private translateService: TranslateService,
     private localizeRouterService: LocalizeRouterService,
-    private metaService: Meta,
+    private location: Location,
     rendererFactory: RendererFactory2
   ) {
     this.renderer = rendererFactory.createRenderer(this.document, {
@@ -89,138 +118,150 @@ export class HeaderService implements OnDestroy {
       styles: [],
       data: {}
     });
+
+    const title = this.translateService.instant(MAIN_TITLE);
+    const description = this.translateService.instant(MAIN_DESCRIPTION);
+
+    this.inferredHeaders = {
+      title: title,
+      'og:title': title,
+      'twitter:title': title,
+      description: description,
+      'og:description': description,
+      'twitter:description': description,
+      'og:image': '',
+      'twitter:image': '',
+      'twitter:card': 'summary_large_image',
+      'og:url': '',
+      canonicalUrl: {
+        rel: 'canonical',
+      },
+      alternativeLinks: [
+        {
+          rel: 'alternative',
+          hreflang: 'fi'
+        },
+        {
+          rel: 'alternative',
+          hreflang: 'sv'
+        },
+        {
+          rel: 'alternative',
+          hreflang: 'en'
+        }
+      ]
+    };
   }
 
-  public startRouteListener(): void {
+  public initialize() {
+    const canonicalEl = this.renderer.createElement('link');
+    this.renderer.setAttribute(canonicalEl, 'rel', this.inferredHeaders.canonicalUrl.rel);
+    this.renderer.appendChild(this.document.head, canonicalEl);
+
+    this.inferredHeaders.alternativeLinks.forEach(link => {
+      const el = this.renderer.createElement('link');
+      this.renderer.setAttribute(el, 'rel', link.rel);
+      this.renderer.setAttribute(el, 'hreflang', link.hreflang);
+      this.renderer.appendChild(this.document.head, el);
+    });
+
+    this.metaService.addTags([
+      {property: 'og:title'},
+      {property: 'og:description'},
+      {property: 'og:image'},
+      {property: 'og:url'},
+      {name: 'twitter:title'},
+      {name: 'twitter:description'},
+      {name: 'twitter:image'},
+      {name: 'twitter:card'}
+    ]);
+
+    this.startRouteListener();
+  }
+
+  public setHeaders(headers: IHeaders): void {
+    if (!this.platformService.isBrowser) { return; }
+
+    Object.entries(headers).forEach(([key, value]) => {
+      switch (key) {
+        case 'title':
+          this.titleService.setTitle(value);
+          break;
+        case 'description':
+        case 'og:title':
+        case 'og:description':
+        case 'og:image':
+        case 'og:url':
+          this.metaService.updateTag({property: key, content: value});
+          break;
+        case 'twitter:description':
+        case 'twitter:image':
+        case 'twitter:title':
+        case 'twitter:card':
+          this.metaService.updateTag({name: key, content: value});
+          break;
+        case 'canonicalUrl':
+          this.updateLink(value);
+          this.metaService.updateTag({property: 'og:url', content: value.href});
+          break;
+        case 'alternativeLinks':
+          value.forEach(v => this.updateLink(v));
+          break;
+      }
+    });
+  }
+
+  private updateLink(link: ILinkElement) {
+    const el = link.hreflang
+               ? this.document.querySelector(`link[rel='${link.rel}'][hreflang='${link.hreflang}']`)
+               : this.document.querySelector(`link[rel='${link.rel}']`);
+    if (!el) {
+      return;
+    }
+    if (link.href) {
+      this.renderer.setAttribute(el, 'href', link.href);
+    }
+    if (link.hreflang) {
+      this.renderer.setAttribute(el, 'hreflang', link.hreflang);
+    }
+  }
+
+  private startRouteListener(): void {
+    if (!this.platformService.isBrowser) { return; }
+
     this.routeSub = this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(() => this.location.path() || '/'),
       filter(newRoute => this.currentRoute !== newRoute)
-    ).subscribe((newRoute) => {
+    ).subscribe(newRoute => {
       this.currentRoute = newRoute;
 
-      // Set page title
-      this.getDeepestTitle(this.router.routerState.snapshot.root).pipe(
-        map(titles => [...titles, MAIN_TITLE]),
-        map(titles => Array.from(new Set<string>(titles))),
-        switchMap(titles => this.translateService.get(titles))
-      ).subscribe(pageTitle => {
-        this.title.setTitle(Object.keys(pageTitle).map(key => decodeURI(pageTitle[key])).join(' | '));
-      });
+      const titles = removeDuplicates(
+        [...getRouteTitles(this.router.routerState.snapshot.root), MAIN_TITLE]
+      );
+      const titlesTranslated = this.translateService.instant(titles);
+      const titleString = Object.keys(titlesTranslated).map(key => decodeURI(titlesTranslated[key])).join(' | ');
+      this.inferredHeaders.title = titleString;
 
-      // Set page meta tags
-      RouteDataService.getDeepest<object>(this.router.routerState.snapshot.root).pipe(
-        map(meta => ({description: MAIN_DESCRIPTION, ...meta}))
-      ).subscribe(meta => {
-        const ArraysMeta = [...ALL_META_KEYS, ...ALL_IMAGE_KEYS, ...TWITTER_CARD];
-        ArraysMeta.map((key) => {
-          const propertySelector = ((key === 'twitter:card' || key === 'twitter:title' ) ? `name='${key}'` : `property='${key}'`);
-          if (meta?.[key]) {
-            this.metaService.updateTag({property: key, content: this.translateService.instant(meta[key])}, propertySelector);
-          } else {
-            this.metaService.removeTag(propertySelector);
-          }
-        });
-      });
+      const c = getDeepestChildValue(this.router.routerState.snapshot.root, 'canonical', '');
+      const canonicalUrl = c ? this.localizeRouterService.translateRoute(c, this.translateService.currentLang) : newRoute;
+      this.inferredHeaders.canonicalUrl = {href: canonicalUrl, rel: 'canonical'};
 
-      // Set canonical link
-      RouteDataService.getDeepest<string>(this.router.routerState.snapshot.root, 'canonical', '').subscribe(canonical => {
-        this.updateCanonicalUrl(canonical ? this.localizeRouterService.translateRoute(canonical, this.translateService.currentLang) : newRoute);
-      });
+      this.inferredHeaders.alternativeLinks = this.localizeRouterService.locales.map(
+        lang => ({
+          href: environment.base + this.localizeRouterService.translateRoute(newRoute, lang),
+          hreflang: lang,
+          rel: 'alternative'
+        })
+      );
 
-      this.updateAlternativeLinks(newRoute);
-    });
-  }
-
-  public createTwitterCard(title) {
-    this.removeMetaTags(TWITTER_CARD);
-    this.metaService.addTag({name: 'twitter:card', content: 'summary_large_image'});
-    TWITTER_CARD.forEach((key) => {
-      if (key !== 'twitter:card') {
-        this.metaService.addTag({ name: key, content: title });
-      }
-    });
-  }
-
-  public updateMetaDescription(description) {
-    this.removeMetaTags(ALL_META_KEYS);
-    ALL_META_KEYS.forEach((key) => {
-      this.metaService.addTag({ property: key, content: description });
-    });
-  }
-
-  public updateFeatureImage(image) {
-    this.removeMetaTags(ALL_IMAGE_KEYS);
-    ALL_IMAGE_KEYS.forEach((key) => {
-      this.metaService.addTag({ property: key, content: image });
+      this.setHeaders(this.inferredHeaders);
     });
   }
 
   ngOnDestroy() {
-    this.routeSub.unsubscribe();
-  }
-
-  private updateCanonicalUrl(path: string) {
-    this.removeElements('link[rel="canonical"]');
-    this.addLink({
-      href: environment.base + path,
-      rel: 'canonical'
-    });
-  }
-
-  private updateAlternativeLinks(path: string) {
-    this.removeElements('link[rel="alternative"]');
-    this.localizeRouterService.locales.forEach(lang => {
-      this.addLink({
-        href: environment.base + this.localizeRouterService.translateRoute(path, lang),
-        hreflang: lang,
-        rel: 'alternative'
-      });
-    });
-  }
-
-  private removeMetaTags(metaTagsDescription) {
-    this.metaService.removeTag('property="twitter:card"');
-    RouteDataService.getDeepest<object>(this.router.routerState.snapshot.root).pipe(
-      map(meta => ({description: MAIN_DESCRIPTION, ...meta}))
-    ).subscribe(meta => {
-      metaTagsDescription.forEach((key) => {
-        const propertySelector = `property='${key}'`;
-        if (meta?.[key]) {
-          this.metaService.removeTag(propertySelector);
-        }
-      });
-    });
-  }
-
-  private removeElements(selector: string) {
-    if (this.platformService.isBrowser) {
-      const alternatives = this.document.querySelectorAll(selector);
-      if (alternatives.length) {
-        alternatives.forEach(elem => this.renderer.removeChild(this.document.head, elem));
-      }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
     }
   }
-
-  private addLink(options: {[key: string]: string}): void {
-    const linkElt = this.renderer.createElement('link');
-    Object.keys(options).forEach(key => {
-      this.renderer.setAttribute(linkElt, key, options[key]);
-    });
-    this.renderer.appendChild(this.document.head, linkElt);
-  }
-
-  private getDeepestTitle(routeSnapshot: ActivatedRouteSnapshot): Observable<string[]> {
-    const title = [];
-    if (routeSnapshot.data && routeSnapshot.data['title']) {
-      title.push(routeSnapshot.data['title'] || '');
-    }
-    if (routeSnapshot.firstChild) {
-      return this.getDeepestTitle(routeSnapshot.firstChild).pipe(
-        map(label => [...label, ...title])
-      );
-    }
-    return ObservableOf(title);
-  }
-
 }
