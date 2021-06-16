24789206d6ea22763cec94f3ec74dc2cb36c5857
aorin <meeri.rannisto@gmail.com>
2021-06-16 10:56:15 +0300

    import map shows popup on hover

--- projects/laji/src/app/shared-modules/spreadsheet/importer/import-map/import-map.component.ts
+++ projects/laji/src/app/shared-modules/spreadsheet/importer/import-map/import-map.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy, ViewChild, Input, OnChanges } from '@angular/core';
-import { LajiMapComponent } from '@laji-map/laji-map.component';
 import { LajiMapOptions, LajiMapTileLayerName, LajiMapDataOptions } from '@laji-map/laji-map.interface';
+import { TranslateService } from '@ngx-translate/core';
 import * as Hash from 'object-hash';
 
 @Component({
@@ -10,12 +10,13 @@ import * as Hash from 'object-hash';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImportMapComponent implements OnChanges {
-  @ViewChild(LajiMapComponent, { static: true }) lajiMap: LajiMapComponent;
+  @ViewChild('popup', { static: true }) popupComponent;
   @Input() data: {[key: string]: any}[] = [];
   @Input() colMap: {[key: string]: string} = {};
   @Input() geometryField = 'gatherings[*].geometry';
   @Input() color = '#00aa00';
   @Input() height = '100%';
+  @Input() popupFields = ['gatheringEvent.dateBegin', 'gatherings[*].units[*].identifications[*].taxon'];
 
   mapOptions: LajiMapOptions = {
     tileLayerName: LajiMapTileLayerName.maastokartta,
@@ -27,27 +28,25 @@ export class ImportMapComponent implements OnChanges {
 
   mapData: LajiMapDataOptions;
 
+  private groupedData: {[key: string]: any}[][] = [];
+  private popupColumns: string[] = [];
+
+  constructor(
+    private translateService: TranslateService
+  ) { }
+
   ngOnChanges() {
+    this.popupColumns = this.getColumns(this.popupFields);
     this.mapData = this.getMapData();
   }
 
   getMapData(): LajiMapDataOptions {
-    const geometryCol = Object.keys(this.colMap).filter(col => this.colMap[col] === this.geometryField)?.[0];
+    const geometryCol = this.getColumn(this.geometryField);
     if (!geometryCol) {
       return {};
     }
 
-    const geometries = {};
-    this.data.forEach((dataItem) => {
-      const geometry = dataItem[geometryCol];
-      if (!geometry) {
-        return;
-      }
-      const hash = Hash.sha1(geometry);
-      if (!geometries[hash]) {
-        geometries[hash] = geometry;
-      }
-    });
+    this.groupedData = this.groupDataByGeometry(this.data, geometryCol);
 
     return {
       getFeatureStyle: () => {
@@ -60,11 +59,47 @@ export class ImportMapComponent implements OnChanges {
       },
       featureCollection: {
         type: 'FeatureCollection',
-        features: Object.keys(geometries).map(hash => ({
+        features: this.groupedData.map(items => ({
           type: 'Feature',
-          geometry: geometries[hash]
+          geometry: items[0][geometryCol]
         }))
-      }
+      },
+      getPopup: ({featureIdx, feature}, cb: (elem: string | HTMLElement) => void) => {
+        const data = this.groupedData[featureIdx];
+
+        return data.map(dataItem => {
+          const label = '<strong>' + this.translateService.instant('excel.batch') + ' ' + dataItem._doc + ':</strong> ';
+          const value = this.popupColumns.map(col => dataItem[col]).filter(text => !!text).join(' ');
+
+          return label + value;
+        }).join('<br>');
+      },
+      cluster: true
     };
+  }
+
+  private groupDataByGeometry(data: {[key: string]: any}[], geometryCol: string) {
+    const groupedData = {};
+    data.forEach((dataItem) => {
+      const geometry = dataItem[geometryCol];
+      if (!geometry) {
+        return;
+      }
+      const hash = Hash.sha1(geometry);
+      if (!groupedData[hash]) {
+        groupedData[hash] = [];
+      }
+      groupedData[hash].push(dataItem);
+    });
+
+    return Object.keys(groupedData).map(hash => groupedData[hash]);
+  }
+
+  private getColumns(fields: string[]) {
+    return fields.map(field => this.getColumn(field)).filter(col => !!col);
+  }
+
+  private getColumn(field: string) {
+    return Object.keys(this.colMap).filter(col => this.colMap[col] === field)?.[0];
   }
 }
