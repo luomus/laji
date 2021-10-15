dbe9a126bda2417c3e0cd62cde83a91e622e5fd6
Olli Raitio <olli.raitio@helsinki.fi>
2021-10-15 13:48:44 +0300

    rm haseka-feedbback, rm unused haseka imports, vihko terms of service to cms [finishes #179932912]

--- projects/laji/src/app/+haseka/haseka.module.ts
+++ projects/laji/src/app/+haseka/haseka.module.ts
@@ -11,14 +11,11 @@ import { LajiFormModule } from '@laji-form/laji-form.module';
 import { HasekaComponent } from './haseka.component';
 import { routing } from './haseka.routes';
 import { HaSeKaFormComponent } from './form/haseka-form.component';
-import { HaSeKaTermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 import { LatestDocumentsModule } from '../shared-modules/latest-documents/latest-documents.module';
 import { HasekaTermsComponent } from './terms/haseka-terms.component';
-import { HasekaFeedbackComponent } from './haseka-feedback/haseka-feedback.component';
 import { AppComponentModule } from '../shared-modules/app-component/app-component.module';
 import { LajiUiModule } from '../../../../laji-ui/src/public-api';
 import { VihkoHomeComponent } from './vihko-home/vihko-home.component';
-import { SurveyBoxModule } from '../shared-modules/survey-box/survey-box.module';
 import { TechnicalNewsModule } from '../shared-modules/technical-news/technical-news.module';
 import { TemplateHasekaFormComponent } from './template-haseka-form/template-haseka-form.component';
 
@@ -36,14 +33,12 @@ import { TemplateHasekaFormComponent } from './template-haseka-form/template-has
     LatestDocumentsModule,
     AppComponentModule,
     LajiUiModule,
-    SurveyBoxModule,
     TechnicalNewsModule
   ],
   declarations: [
-    HasekaComponent, HaSeKaFormComponent, HaSeKaTermsOfServiceComponent,
+    HasekaComponent, HaSeKaFormComponent,
     OwnSubmissionsComponent, TemplatesComponent,
     HasekaTermsComponent,
-    HasekaFeedbackComponent,
     VihkoHomeComponent,
     TemplateHasekaFormComponent,
   ]
