84f6cb182ca95db4086eef1a476a98e8e8c1bb56
aorin <meeri.rannisto@gmail.com>
2021-11-01 14:38:41 +0200

    [kerttu global] fix pie chart not always showing translations

--- projects/kerttu-global/src/app/+results/validation-pie-chart/validation-pie-chart.component.ts
+++ projects/kerttu-global/src/app/+results/validation-pie-chart/validation-pie-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, ChangeDetectorRef } from '@angular/core';
 import { IValidationStat } from '../../kerttu-global-shared/models';
 import { Color } from 'ng2-charts';
 import { TranslateService } from '@ngx-translate/core';
@@ -10,7 +10,7 @@ import { TranslateService } from '@ngx-translate/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ValidationPieChartComponent {
-  labels: string[];
+  labels: string[] = ['', '', ''];
   colors: Color[] = [
     { backgroundColor: ['#e87d7d', '#f0e675', '#94e56c'] }
   ];
@@ -31,12 +31,13 @@ export class ValidationPieChartComponent {
   }
 
   constructor(
-    private traslate: TranslateService
+    private translate: TranslateService,
+    private cd: ChangeDetectorRef
   ) {
-    this.labels = [
-      this.traslate.instant('results.pie.group1'),
-      this.traslate.instant('results.pie.group2'),
-      this.traslate.instant('results.pie.group3')
-    ]
+    const translationKeys = ['results.pie.group1', 'results.pie.group3', 'results.pie.group3'];
+    this.translate.get(translationKeys).subscribe(translations => {
+      this.labels = translationKeys.map(key => translations[key]);
+      this.cd.markForCheck();
+    });
   }
 }
