768815022109d67d21fe049bd11d7827d4341d41
aorin <meeri.rannisto@gmail.com>
2021-11-17 14:39:03 +0200

    fix merge conflict

--- projects/laji/src/environments/environment.embedded.ts
+++ projects/laji/src/environments/environment.embedded.ts
@@ -14,9 +14,6 @@ export const environment = {
   selfPage: 'https://login.laji.fi/self',
   kerttuApi: 'https://kerttu-backend.laji.fi',
   protaxApi: 'https://protax-api.rahtiapp.fi',
-<<<<<<< HEAD
+  geoserver: 'https://geoserver.laji.fi',
   defaultLang: 'fi'
-=======
-  geoserver: 'https://geoserver.laji.fi'
->>>>>>> development
 };
