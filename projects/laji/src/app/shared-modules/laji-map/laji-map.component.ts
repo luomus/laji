0b79dea03a7db80504689a1a33bff701dbefa9e6
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2021-12-07 17:06:14 +0200

    temp fix type error

--- projects/laji/src/app/shared-modules/laji-map/laji-map.component.ts
+++ projects/laji/src/app/shared-modules/laji-map/laji-map.component.ts
@@ -109,8 +109,8 @@ export class LajiMapComponent implements OnDestroy, OnChanges, AfterViewInit {
         }
       };
     }
-    if (environment.geoserver) {
-      options.lajiGeoServerAddress = environment.geoserver;
+    if ((environment as any).geoserver) {
+      options.lajiGeoServerAddress = (environment as any).geoserver;
     }
     this._options = options;
     this.initMap();
