15af07f2e7014f2403dab28a3fecd2cf72b70754
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2021-12-16 12:26:39 +0200

    update arcgis cms page id

--- projects/vir/src/app/component/footer/footer.component.ts
+++ projects/vir/src/app/component/footer/footer.component.ts
@@ -17,6 +17,7 @@ const informationWhitelist = [
   // Services and instructions
   '803', // tietovarasto
   '806c1', '806', // API / technical
+  '5138',
   // Muuta
   '2814' // punaisen kirjan verkkopalvelu
 ];
