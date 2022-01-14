6273fbb419ed88befc929390ab12b360842ae34c
aorin <meeri.rannisto@gmail.com>
2022-01-14 16:47:31 +0200

    [kerttu global] add placeholder headers

--- projects/kerttu-global/src/app/+identification/identification-instructions/identification-instructions.component.ts
+++ projects/kerttu-global/src/app/+identification/identification-instructions/identification-instructions.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'bsg-identification-instructions',
   template: `
     <div class="container laji-page">
+      <h2>Instructions</h2>
       <laji-info-page [page]="{'en': ''} | multiLang"></laji-info-page>
     </div>
   `
