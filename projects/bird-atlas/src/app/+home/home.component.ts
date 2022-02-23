eacaa5022a125332d853ed67c35455b6d1bb7bb9
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-02-23 14:51:05 +0200

    [ba] clean up redundant stuff

--- projects/bird-atlas/src/app/+home/home.component.ts
+++ projects/bird-atlas/src/app/+home/home.component.ts
@@ -1,9 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Information } from 'projects/laji-api-client/src/public-api';
-import { Observable, Subject } from 'rxjs';
-import { startWith, switchMap, takeUntil } from 'rxjs/operators';
-import { ApiService, Lang } from '../core/api.service';
+import { startWith, switchMap } from 'rxjs/operators';
+import { ApiService } from '../core/api.service';
 import { cmsIds } from '../locale/cms-ids';
 
 @Component({
@@ -12,19 +10,10 @@ import { cmsIds } from '../locale/cms-ids';
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  private unsubscribe$ = new Subject<void>();
-  homeContent$: Observable<Information>;
+export class HomeComponent {
+  homeContent$ = this.translate.onLangChange.pipe(
+    startWith({lang: this.translate.currentLang}),
+    switchMap(event => this.api.getInformation(cmsIds['home'][event.lang]))
+  );
   constructor(private translate: TranslateService, private api: ApiService) {}
-  ngOnInit() {
-    this.homeContent$ = this.translate.onLangChange.pipe(
-      startWith({lang: this.translate.currentLang}),
-      switchMap(event => this.api.getInformation(cmsIds['home'][event.lang])),
-      takeUntil(this.unsubscribe$)
-    );
-  }
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
 }
