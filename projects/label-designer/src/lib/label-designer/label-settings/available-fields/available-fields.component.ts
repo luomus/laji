067853cbce531e0b8f3b44fca7cfb47c532d13ac
aorin <meeri.rannisto@gmail.com>
2022-02-24 14:23:40 +0200

    [LD] available fields selector also works when there are duplicate field names or when a field is missing a field name

--- projects/label-designer/src/lib/label-designer/label-settings/available-fields/available-fields.component.ts
+++ projects/label-designer/src/lib/label-designer/label-settings/available-fields/available-fields.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { FieldType, ILabelField } from '../../../label-designer.interface';
 
+interface ILabelFieldWithIdx extends ILabelField {
+  idx: number;
+}
+
 /**
  * @internal
  */
@@ -14,20 +18,27 @@ export class AvailableFieldsComponent {
 
   @Input() includeTextFields = true;
   @Input() placeholder = 'add';
-  @Input() availableFields: ILabelField[];
   @Input() value = '';
 
   @Output() valueChange = new EventEmitter<ILabelField>();
 
   fieldType = FieldType;
+  _availableFields: ILabelFieldWithIdx[] = [];
+
+  @Input() set availableFields(availableFields: ILabelField[]) {
+    this._availableFields = (availableFields || []).map((field, idx) => ({
+      ...field,
+      idx: idx
+    }));
+  }
 
   doValueSelect(event: Event): void {
     const select = event.target as HTMLSelectElement;
     const value = select.value;
     select.value = '';
-    const idx = this.availableFields.findIndex((v) => v.field === value);
-    if (idx > -1) {
-      this.valueChange.emit({...this.availableFields[idx]});
+    const idx = parseInt(value);
+    if (!isNaN(idx)) {
+      this.valueChange.emit({...this._availableFields[idx]});
     } else {
       this.valueChange.emit(null);
     }
