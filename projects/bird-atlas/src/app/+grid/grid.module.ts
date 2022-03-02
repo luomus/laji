d613236cbb2096c4f3044aeb9bc6581bc8b2906d
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-03-02 10:30:59 +0200

    [ba] add intro texts

--- projects/bird-atlas/src/app/+grid/grid.module.ts
+++ projects/bird-atlas/src/app/+grid/grid.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { GridIndexComponent } from './grid-index/grid-index.component';
 import { GridInfoComponent } from './grid-info/grid-info.component';
 import { routing } from './grid.routes';
@@ -7,7 +8,8 @@ import { routing } from './grid.routes';
 @NgModule({
   imports: [
     routing,
-    CommonModule
+    CommonModule,
+    TranslateModule
   ],
   declarations: [GridIndexComponent, GridInfoComponent]
 })
