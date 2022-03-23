5fc5d80e2d0a992b00a6d706115871b6d1c29077
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-03-23 17:37:22 +0200

    [ba] grid-index filter

--- projects/bird-atlas/src/app/+grid/grid-index/grid-index.component.ts
+++ projects/bird-atlas/src/app/+grid/grid-index/grid-index.component.ts
@@ -1,8 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TableColumn } from '@swimlane/ngx-datatable';
 import { datatableClasses } from 'projects/bird-atlas/src/styles/datatable-classes';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 import { AtlasApiService, AtlasGridQueryElem } from '../../core/atlas-api.service';
+
+type DatatableRow = any;
 
 @Component({
   selector: 'ba-grid-index',
@@ -10,8 +14,11 @@ import { AtlasApiService, AtlasGridQueryElem } from '../../core/atlas-api.servic
   styleUrls: ['./grid-index.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GridIndexComponent {
-  rows$ = this.atlasApi.getGrid();
+export class GridIndexComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+  private search$ = new Subject<string>();
+  private rows: DatatableRow[] = [];
+
   cols: TableColumn[] = [{
     prop: 'coordinates',
     name: 'YKJ',
@@ -23,13 +30,41 @@ export class GridIndexComponent {
     resizeable: false,
     sortable: true
   }];
+  filteredRows$ = new Subject<DatatableRow[]>();
   datatableClasses = datatableClasses;
 
   constructor(private router: Router, private route: ActivatedRoute, private atlasApi: AtlasApiService) {}
+
+  ngOnInit(): void {
+    this.atlasApi.getGrid().subscribe(g => {
+      this.rows = g;
+      this.filteredRows$.next(this.rows);
+    });
+    this.search$.pipe(
+      debounceTime(200),
+      takeUntil(this.unsubscribe$)
+    ).subscribe(s => {
+      const filterStr = s.toLowerCase();
+      this.filteredRows$.next(
+        this.rows.filter(
+          r => (r.name + r.coordinates).toLowerCase().includes(filterStr)
+        )
+      );
+    });
+  }
 
   onActivate(event: { type: string; row: AtlasGridQueryElem }) {
     if(event.type === 'click') {
       this.router.navigate([event.row.coordinates], { relativeTo: this.route });
     }
+  }
+
+  onSearchKeyUp(event) {
+    this.search$.next(event.target.value);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
