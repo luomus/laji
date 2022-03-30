91452b36bcd7c7718f98077e506a3f24a4afb749
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-03-30 16:00:25 +0300

    [ba] reduce margins

--- projects/bird-atlas/src/app/+species/species.component.ts
+++ projects/bird-atlas/src/app/+species/species.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ba-species',
   template: `
-<div class="container my-6">
+<div class="container mb-6">
   <router-outlet></router-outlet>
 </div>
 `
