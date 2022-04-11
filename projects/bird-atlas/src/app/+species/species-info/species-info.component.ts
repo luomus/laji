9fe989189eeeef67209ffbb2154f53084c93b313
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-04-11 17:29:59 +0300

    [ba] species-info data typing

--- projects/bird-atlas/src/app/+species/species-info/species-info.component.ts
+++ projects/bird-atlas/src/app/+species/species-info/species-info.component.ts
@@ -1,16 +1,21 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from 'projects/bird-atlas/src/env/environment';
 import { Taxon } from 'projects/laji-api-client/src/public-api';
 import { HeaderService } from 'projects/laji/src/app/shared/service/header.service';
-import { forkJoin, of } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
-import { AtlasApiService } from '../../core/atlas-api.service';
+import { AtlasApiService, AtlasTaxon } from '../../core/atlas-api.service';
 import { BreadcrumbId, BreadcrumbService } from '../../core/breadcrumb.service';
 
 const capitalize = (str: string): string => str.charAt(0).toUpperCase() + str.substring(1);
+
+interface SpeciesInfoData {
+  taxon: AtlasTaxon;
+  map: SafeHtml;
+}
 
 @Component({
   selector: 'ba-species-info',
@@ -19,40 +24,7 @@ const capitalize = (str: string): string => str.charAt(0).toUpperCase() + str.su
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SpeciesInfoComponent {
-  data$ = this.route.paramMap.pipe(
-    tap(() => {
-      this.breadcrumbs.setBreadcrumbName(BreadcrumbId.SpeciesInfo, undefined);
-      this.loading = true;
-      this.cdr.markForCheck();
-    }),
-    switchMap(params => forkJoin({
-      taxon: this.atlasApi.getTaxon(params.get('id')).pipe(
-        catchError(err => {
-          console.error(err);
-          return of(undefined);
-        })
-      ),
-      map: this.atlasApi.getSpeciesMap(params.get('id')).pipe(
-        map(html => this.sanitizer.bypassSecurityTrustHtml(html)),
-        catchError(err => {
-          console.error(err);
-          return of(undefined);
-        })
-      )
-    })),
-    tap(data => {
-      const name: string = data.taxon.vernacularName[this.translate.currentLang];
-      this.breadcrumbs.setBreadcrumbName(
-        BreadcrumbId.SpeciesInfo,
-        name.charAt(0).toUpperCase() + name.substring(1)
-      );
-      this.headerService.setHeaders({
-        title: `${capitalize(data.taxon.vernacularName[this.translate.currentLang])} | ${this.translate.instant('ba.header.title')}`
-      });
-      this.loading = false;
-      this.cdr.detectChanges();
-    })
-  );
+  data$: Observable<SpeciesInfoData>;
 
   loading = true;
 
@@ -64,7 +36,42 @@ export class SpeciesInfoComponent {
     private sanitizer: DomSanitizer,
     private headerService: HeaderService,
     private cdr: ChangeDetectorRef
-  ) {}
+  ) {
+    this.data$ = this.route.paramMap.pipe(
+      tap(() => {
+        this.breadcrumbs.setBreadcrumbName(BreadcrumbId.SpeciesInfo, undefined);
+        this.loading = true;
+        this.cdr.markForCheck();
+      }),
+      switchMap(params => forkJoin({
+        taxon: this.atlasApi.getTaxon(params.get('id')).pipe(
+          catchError(err => {
+            console.error(err);
+            return of(undefined);
+          })
+        ),
+        map: this.atlasApi.getSpeciesMap(params.get('id')).pipe(
+          map(html => this.sanitizer.bypassSecurityTrustHtml(html)),
+          catchError(err => {
+            console.error(err);
+            return of(undefined);
+          })
+        )
+      })),
+      tap(data => {
+        const name: string = data.taxon.vernacularName[this.translate.currentLang];
+        this.breadcrumbs.setBreadcrumbName(
+          BreadcrumbId.SpeciesInfo,
+          name.charAt(0).toUpperCase() + name.substring(1)
+        );
+        this.headerService.setHeaders({
+          title: `${capitalize(data.taxon.vernacularName[this.translate.currentLang])} | ${this.translate.instant('ba.header.title')}`
+        });
+        this.loading = false;
+        this.cdr.detectChanges();
+      })
+    );
+  }
 
   getForeignVernacularNames(taxon: Taxon) {
     return ['fi', 'sv', 'en'].filter(
