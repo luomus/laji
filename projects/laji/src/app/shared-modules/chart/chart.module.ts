effb9c131d5744beccf2bf0a63b021c0443f4ea0
Olli Raitio <olli.raitio@helsinki.fi>
2022-04-12 10:48:45 +0300

    import utilities-dumb-directives where needed in laji project (broken by build atlas PR#271)

--- projects/laji/src/app/shared-modules/chart/chart.module.ts
+++ projects/laji/src/app/shared-modules/chart/chart.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ChartComponent } from './chart.component';
 import { ChartsModule } from 'ng2-charts';
 import { UtilitiesModule } from '../utilities/utilities.module';
+import { UtilitiesDumbDirectivesModule } from '../utilities/directive/dumb-directives/utilities-dumb-directives.module';
 
 
 
@@ -12,6 +13,7 @@ import { UtilitiesModule } from '../utilities/utilities.module';
     CommonModule,
     ChartsModule,
     UtilitiesModule,
+    UtilitiesDumbDirectivesModule
   ],
   exports: [ChartComponent]
 })
