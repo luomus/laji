8f826c42144df848fe5b92f5519a410ffcaf558d
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-04-20 13:04:54 +0300

    [ba] close navbar menu when a link is clicked

--- projects/bird-atlas/src/app/navbar/navbar.component.ts
+++ projects/bird-atlas/src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'ba-navbar',
@@ -6,10 +9,25 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+
   openMenu = false;
+
+  constructor(router: Router) {
+    router.events.pipe(takeUntil(this.unsubscribe$)).subscribe(e => {
+      if (e instanceof NavigationStart) {
+        this.openMenu = false;
+      }
+    });
+  }
 
   toggleMenu() {
     this.openMenu = !this.openMenu;
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
