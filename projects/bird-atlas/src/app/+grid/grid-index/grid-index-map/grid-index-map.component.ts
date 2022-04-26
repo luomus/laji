635cad2ab86e19f857d48ea13145b50407f18093
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-04-26 09:32:19 +0300

    [ba] grid-index map navigation

--- projects/bird-atlas/src/app/+grid/grid-index/grid-index-map/grid-index-map.component.ts
+++ projects/bird-atlas/src/app/+grid/grid-index/grid-index-map/grid-index-map.component.ts
@@ -1,5 +1,6 @@
+import { E } from '@angular/cdk/keycodes';
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, NgZone, OnDestroy, Output, ViewChild } from '@angular/core';
-import { TileLayerName, LajiMap } from 'laji-map';
+import { TileLayerName, LajiMap, DataOptions } from 'laji-map';
 import { convertYkjToGeoJsonFeature } from 'projects/laji/src/app/root/coordinate-utils';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
@@ -9,21 +10,6 @@ const gridSqToFeature = (square: AtlasGridSquare) => {
   const latLngStr = square.coordinates.split(':');
   return convertYkjToGeoJsonFeature(latLngStr[0], latLngStr[1]);
 };
-
-const getMapData = (grid: AtlasGrid) => ({
-  featureCollection: {
-    features: [
-      ...grid.map(square => gridSqToFeature(square))
-    ],
-    type: 'FeatureCollection'
-  },
-  getFeatureStyle: () => ({
-    weight: 5,
-    opacity: 1,
-    fillOpacity: 0.3,
-    color: '#00aa00'
-  })
-});
 
 @Component({
   selector: 'ba-grid-index-map',
@@ -35,7 +21,7 @@ export class GridIndexMapComponent implements AfterViewInit, OnDestroy {
   private unsubscribe$ = new Subject<void>();
 
   @Input() set atlasGrid(grid: AtlasGrid) {
-    this.mapData$.next(getMapData(grid));
+    this.mapData$.next(this.getMapData(grid));
   }
   @Output() selectYKJ = new EventEmitter<string>();
 
@@ -64,6 +50,28 @@ export class GridIndexMapComponent implements AfterViewInit, OnDestroy {
     ).subscribe(mapData => {
       this.map.setData(mapData);
     });
+  }
+
+  getMapData(grid: AtlasGrid): DataOptions {
+    return {
+      featureCollection: {
+        features: [
+          ...grid.map(square => <any>gridSqToFeature(square))
+        ],
+        type: 'FeatureCollection'
+      },
+      getFeatureStyle: () => ({
+        weight: 2,
+        opacity: 1,
+        fillOpacity: 0.2,
+        color: '#2691d9'
+      }),
+      on: {
+        click: (e, d) => {
+          this.selectYKJ.emit((<any>d.feature.geometry).coordinateVerbatim);
+        }
+      }
+    };
   }
 
   ngOnDestroy(): void {
