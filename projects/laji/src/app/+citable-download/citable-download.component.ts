7f2f715c9db7a1ff6d0e3b83c50fca74e623b1f4
aorin <meeri.rannisto@gmail.com>
2022-04-28 12:34:57 +0300

    set title to citable download pages

--- projects/laji/src/app/+citable-download/citable-download.component.ts
+++ projects/laji/src/app/+citable-download/citable-download.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { WarehouseApi } from '../shared/api/WarehouseApi';
 import { DownloadRequestResponse, isDownloadRequest, asDownloadRequest } from '../shared-modules/download-request/models';
+import { HeaderService } from '../shared/service/header.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   template: `
@@ -32,14 +34,30 @@ export class CitableDownloadComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private warehouseApi: WarehouseApi
+    private warehouseApi: WarehouseApi,
+    private headerService: HeaderService,
+    private translate: TranslateService
   ) { }
 
   ngOnInit() {
     this.downloadRequest$ = this.route.params.pipe(
       map(params => params['id']),
       tap(id => { this.id = id; }),
-      switchMap(id => this.warehouseApi.downloads(id))
+      switchMap(id => this.warehouseApi.downloads(id)),
+      tap(response => this.updateHeaders(response))
     );
+  }
+
+  updateHeaders(response: DownloadRequestResponse) {
+    const key = (
+      response.found &&
+      response.downloadType === 'AUTHORITIES_API_KEY'
+    ) ? 'downloadRequest.apiKey' : 'downloadRequest.fileDownload';
+
+    const title = this.translate.instant(key);
+
+    this.headerService.setHeaders({
+      title: title + ' | ' + this.headerService.getInferred().title
+    });
   }
 }
--- projects/laji/src/app/app-routing.modules.ts
+++ projects/laji/src/app/app-routing.modules.ts
@@ -37,7 +37,7 @@ const baseRoutes: Routes = [
   {path: 'ui-components', loadChildren: () => import('./+ui-components/ui-components.module').then(m => m.UiComponentsModule), data: {preload: false}},
   {path: 'save-observations', loadChildren: () => import('./+save-observations/save-observations.module').then(m => m.SaveObservationsModule)},
   {path: 'project', loadChildren: () => import('./+project-form/project-form.module').then(m => m.ProjectFormModule)},
-  {path: 'citation', loadChildren: () => import('./+citable-download/citable-download.module').then(m => m.CitableDownloadModule)}
+  {path: 'citation', loadChildren: () => import('./+citable-download/citable-download.module').then(m => m.CitableDownloadModule), data: {preload: false}}
 ];
 
 const rootRouting = {
