a3fa8895c186a3d15e42b1bd736c8832e3b0d8d7
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-05-06 15:05:37 +0300

    [ba] add association template files

--- projects/bird-atlas/src/app/+association/association-index/association-index.component.ts
+++ projects/bird-atlas/src/app/+association/association-index/association-index.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: ``
+  templateUrl: 'association-index.component.html'
 })
 export class AssociationIndexComponent {
 
--- projects/bird-atlas/src/app/+association/association-info/association-info.component.ts
+++ projects/bird-atlas/src/app/+association/association-info/association-info.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: ``
+  templateUrl: 'association-info.component.html'
 })
 export class AssociationInfoComponent {
 
