0e7c4d3ce15549abbf0703b3e63c3ab8dbb4cf10
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-05-26 15:30:13 +0300

    [ba] bird-society-index

--- projects/bird-atlas/src/app/+bird-society/bird-society-index/bird-society-index.component.ts
+++ projects/bird-atlas/src/app/+bird-society/bird-society-index/bird-society-index.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AtlasApiService } from '../../core/atlas-api.service';
 
 @Component({
   templateUrl: 'bird-society-index.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BirdSocietyIndexComponent {
-
+  birdSocieties$ = this.atlasApi.getBirdSocieties();
+  constructor(private atlasApi: AtlasApiService) {}
 }
--- projects/bird-atlas/src/app/core/atlas-api.service.ts
+++ projects/bird-atlas/src/app/core/atlas-api.service.ts
@@ -13,8 +13,12 @@ export type AtlasActivityCategory =
   | 'MY.atlasActivityCategoryEnum3'
   | 'MY.atlasActivityCategoryEnum4'
   | 'MY.atlasActivityCategoryEnum5';
+export interface KeyValuePair<K, T> {
+  key: K;
+  value: T;
+}
 export interface AtlasGridSquare {
-  birdAssociationArea: {key: string; value: string};
+  birdAssociationArea: KeyValuePair<string, string>;
   coordinates: string;
   id: string;
   level1: number;
@@ -24,23 +28,20 @@ export interface AtlasGridSquare {
   level5: number;
   name: string;
   data?: {
-    atlasClass: {key: string; value: string};
-    atlasCode: {key: string; value: string};
+    atlasClass: KeyValuePair<string, string>;
+    atlasCode: KeyValuePair<string, string>;
     speciesId: string;
     speciesName: string;
   }[];
   atlas?: number;
   atlasClassSum?: number;
-  activityCategory?: {key: AtlasActivityCategory; value: string};
+  activityCategory?: KeyValuePair<AtlasActivityCategory, string>;
   speciesCount?: number;
 };
 export type AtlasGrid = AtlasGridSquare[];
 export interface BirdSociety {
   gridSquares: AtlasGrid;
-  birdAssociationArea: {
-    key: string;
-    value: string;
-  };
+  birdAssociationArea: KeyValuePair<string, string>;
   activityCategories: Record<AtlasActivityCategory, {
     name: string;
     squareSum: number;
@@ -87,6 +88,12 @@ export class AtlasApiService {
   getGridSquare(gridId: string): Observable<AtlasGridSquare> {
     const url = `${BASE_URL}/grid/${gridId}/atlas`;
     return <Observable<AtlasGridSquare>>this.http.get(url);
+  }
+
+  @cacheReturnObservable(86400000) // 1 day
+  getBirdSocieties(): Observable<KeyValuePair<string, string>[]> {
+    const url = `${BASE_URL}/birdAssociation`;
+    return <Observable<KeyValuePair<string, string>[]>>this.http.get(url);
   }
 
   @cacheReturnObservable(86400000) // 1 day
