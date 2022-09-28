77dc2ff3a75a8381f6be2ecb0a70e5a959f63960
Markus Lumme <markusjuhani.lumme@gmail.com>
2022-09-28 15:33:21 +0300

    Added dataset-metadata page to PAP.

--- projects/vir/src/app/+theme/theme-routing.module.ts
+++ projects/vir/src/app/+theme/theme-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DatasetMetadataComponent } from '../../../../laji/src/app/shared-modules/dataset-metadata/dataset-metadata.component';
 import { ThemeComponent } from './theme.component';
 
-
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component: ThemeComponent}
+  {path: '', pathMatch: 'full', component: ThemeComponent},
+  {path: 'dataset-metadata', pathMatch: 'full', component: DatasetMetadataComponent, data: {title: 'theme.dataset-metadata'}},
+  {path: 'dataset-metadata/:collectionId', pathMatch: 'full', component: DatasetMetadataComponent, data: {title: 'theme.dataset-metadata'}},
 ];
 
 @NgModule({
