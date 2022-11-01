034cfff637f6749828ce23b1f1e38629fad84e8f
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2022-11-01 22:57:35 +0200

    observation accuracy viz description

--- projects/laji/src/app/shared-modules/laji-map/laji-map.module.ts
+++ projects/laji/src/app/shared-modules/laji-map/laji-map.module.ts
@@ -4,12 +4,14 @@ import { LajiMapComponent } from './laji-map.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TranslateModule } from '@ngx-translate/core';
 import { LajiMapLegendComponent } from './visualization/legend.component';
+import { InfoModule } from '../info/info.module';
 
 @NgModule({
   imports: [
     CommonModule,
     BsDropdownModule,
-    TranslateModule
+    TranslateModule,
+    InfoModule
   ],
   declarations: [ LajiMapComponent, LajiMapLegendComponent ],
   exports: [ LajiMapComponent ]
