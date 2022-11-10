5517ba7fff2e03ee428c272b57bcf61ab96eeb4c
Olli Raitio <olli.raitio@helsinki.fi>
2022-11-10 15:14:40 +0200

    revert more open-state handling of laji-select

--- projects/laji/src/app/shared-modules/select/select/select.component.ts
+++ projects/laji/src/app/shared-modules/select/select/select.component.ts
@@ -238,9 +238,6 @@ export class SelectComponent<T extends IdType|SelectOption = string> implements 
     this.selectedOptions = selectedOptions;
     this.unselectedOptions = unselectedOptions;
 
-    if (this.open !== undefined) { // Don't touch if not initialized yet. 'open' is initialized at OnInit.
-      this.open = this.open || !!this.selectedOptions.length;
-    }
     this.onChange?.(selected);
     this.onTouch?.(selected);
     this.cd.markForCheck();
