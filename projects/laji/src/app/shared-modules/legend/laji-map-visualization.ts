dbc0c0f1c3c6e7bfd2397547216072cb502eefef
Olli Raitio <olli.raitio@helsinki.fi>
2023-01-19 17:22:37 +0200

    tighten observation map visualization type so that it has always getFeatureStyle

--- projects/laji/src/app/shared-modules/legend/laji-map-visualization.ts
+++ projects/laji/src/app/shared-modules/legend/laji-map-visualization.ts
@@ -1,13 +1,13 @@
 import { GetFeatureStyleOptions } from 'laji-map';
 import { MarkerCluster, PathOptions } from 'leaflet';
 
+export interface LajiMapVisualizationItem {
+  label: string;
+  categories: LajiMapVisualizationCategory[];
+  getFeatureStyle?: (opt: GetFeatureStyleOptions) => PathOptions;
+  getClusterStyle?: (childCount: number, featureIdxs: number[], cluster: MarkerCluster) => PathOptions;
+  getClusterClassName?: (childCount: number, featureIdxs: number[], cluster: MarkerCluster) => string;
+}
+
 export interface LajiMapVisualizationCategory { color: string; label?: string };
-export type LajiMapVisualization<T extends string> = Record<T,
-  {
-    label: string;
-    categories: LajiMapVisualizationCategory[];
-    getFeatureStyle?: (opt: GetFeatureStyleOptions) => PathOptions;
-    getClusterStyle?: (childCount: number, featureIdxs: number[], cluster: MarkerCluster) => PathOptions;
-    getClusterClassName?: (childCount: number, featureIdxs: number[], cluster: MarkerCluster) => string;
-  }
->;
+export type LajiMapVisualization<T extends string> = Record<T, LajiMapVisualizationItem>;
