f2ddc520fd99d2b79e30869845d39e2ad9e34149
Olli Raitio <olli.raitio@helsinki.fi>
2023-02-07 12:34:04 +0200

    fix observation search date e2e tests

--- projects/laji/e2e/src/+observation/observation.e2e-spec.ts
+++ projects/laji/e2e/src/+observation/observation.e2e-spec.ts
@@ -1,6 +1,5 @@
-import { browser } from 'protractor';
 import { UserPage } from '../+user/user.po';
-import { EC, isDisplayed } from '../../helper';
+import { isDisplayed } from '../../helper';
 import { ObservationPage } from './observation.po';
 const page = new ObservationPage();
 const user = new UserPage();
