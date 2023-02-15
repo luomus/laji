669fb37957642eb6d6f68067d30cc1c1b92b6e5f
Olli Raitio <olli.raitio@helsinki.fi>
2023-02-15 20:35:27 +0200

    project-edit redirects unlogged user to login

--- projects/laji/src/app/+project-form-edit/project-form-edit.routes.ts
+++ projects/laji/src/app/+project-form-edit/project-form-edit.routes.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProjectFormEditComponent } from './project-form-edit.component';
 import { ModuleWithProviders } from '@angular/core';
 import { ProjectFormModule } from '../+project-form/project-form.module';
+import { OnlyLoggedIn } from '../shared/route/only-logged-in';
 
 export const routes: Routes = [
   {
     path: '',
+    canActivate: [OnlyLoggedIn],
     component: ProjectFormEditComponent,
     children: [{
       path: '',
