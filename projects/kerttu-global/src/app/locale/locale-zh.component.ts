a97c75b8900df4ed41d60bc80dc939ad20872d2d
aorin <meeri.rannisto@gmail.com>
2023-03-01 09:52:12 +0200

    [bsg] start working on localization

---
+++ projects/kerttu-global/src/app/locale/locale-zh.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { LocaleComponent } from 'projects/laji/src/app/locale/locale.component';
+import * as moment from 'moment';
+// import 'moment/locale/zh-tw';
+import { PlatformService } from 'projects/laji/src/app/root/platform.service';
+
+@Component({
+  selector: 'bsg-locale-zh',
+  template: '<router-outlet></router-outlet>'
+})
+export class LocaleZhComponent extends LocaleComponent {
+
+  constructor(
+    protected platformService: PlatformService
+  ) {
+    super(platformService);
+    // moment.locale('zh-tw');
+    this.setLocale('zh');
+  }
+
+}
