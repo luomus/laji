fad952f157b9de3eaa03f58abc81585b4a2ba355
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2023-05-23 10:38:41 +0300

    table taxon name search

--- projects/bird-atlas/src/app/+bird-society/bird-society-info/bird-society-info-species-table/bird-society-info-species-table.component.ts
+++ projects/bird-atlas/src/app/+bird-society/bird-society-info/bird-society-info-species-table/bird-society-info-species-table.component.ts
@@ -1,6 +1,12 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, Output, TemplateRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component,
+  ElementRef,
+  EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, TemplateRef, ViewChild
+} from '@angular/core';
 import { TableColumn } from '@swimlane/ngx-datatable';
 import { AtlasTaxon } from '../../../core/atlas-api.service';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { debounceTime, filter, takeUntil } from 'rxjs/operators';
 
 type AugmentedAtlasTaxon = AtlasTaxon & {
   classCounts: {
@@ -17,17 +23,37 @@ type AugmentedAtlasTaxon = AtlasTaxon & {
   styleUrls: ['bird-society-info-species-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BirdSocietyInfoSpeciesTableComponent implements OnChanges, AfterViewInit {
+export class BirdSocietyInfoSpeciesTableComponent implements OnChanges, AfterViewInit, OnDestroy, OnInit {
   @ViewChild('alignRight') alignRightTemplate: TemplateRef<any>;
+  @ViewChild('textfield') textfieldElement: ElementRef<HTMLInputElement>;
 
   @Input() taxa: AtlasTaxon[];
   @Output() rowClick = new EventEmitter<AtlasTaxon | null>();
 
-  rows: AugmentedAtlasTaxon[];
+  filteredRows$ = new Subject<AugmentedAtlasTaxon[]>();
   cols: TableColumn[];
   selected: AtlasTaxon[] = [];
 
+  private unsubscribe$ = new Subject<void>();
+  private search$ = new BehaviorSubject<string>(undefined);
+  private rows: AugmentedAtlasTaxon[];
+
   constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.search$.pipe(
+      filter(s => s !== undefined),
+      debounceTime(200),
+      takeUntil(this.unsubscribe$)
+    ).subscribe(s => {
+      const filterStr = s.toLowerCase();
+      this.filteredRows$.next(
+        this.rows.filter(
+          r => (r.vernacularName.fi + r.scientificName).toLowerCase().includes(filterStr)
+        )
+      );
+    });
+  }
 
   ngAfterViewInit(): void {
     this.cols = [{
@@ -59,6 +85,7 @@ export class BirdSocietyInfoSpeciesTableComponent implements OnChanges, AfterVie
 
   ngOnChanges() {
     this.rows = this.taxa.map(t => (<AugmentedAtlasTaxon>{...t, classCounts: {certain: t.classCounts['MY.atlasClassEnumD'], all: t.classCounts.all}}));
+    this.search$.next(this.textfieldElement?.nativeElement?.value ?? '');
   }
 
   selectCheck(row: AtlasTaxon) {
@@ -87,5 +114,14 @@ export class BirdSocietyInfoSpeciesTableComponent implements OnChanges, AfterVie
     const csvContent = 'data:text/csv;charset=utf-8,' + _rows.map(e => e.join(',')).join('\n');
     const encodedUri = encodeURI(csvContent);
     window.open(encodedUri);
+  }
+
+  onSearchKeyUp(event) {
+    this.search$.next(event.target.value);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
