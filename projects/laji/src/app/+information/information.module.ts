09f1cba83430c28153e452d763ff8f6e3c2e85ec
Markus Lumme <markusjuhani.lumme@gmail.com>
2023-07-28 11:05:39 +0300

    Added tecnical news to taxon, species search and about pages.

--- projects/laji/src/app/+information/information.module.ts
+++ projects/laji/src/app/+information/information.module.ts
@@ -4,9 +4,9 @@ import { SharedModule } from '../shared/shared.module';
 import { routing } from './information.routes';
 import { InformationComponent } from './information.component';
 import { LajiUiModule } from 'projects/laji-ui/src/public-api';
-
+import { TechnicalNewsModule } from '../shared-modules/technical-news/technical-news.module';
 @NgModule({
-  imports: [routing, SharedModule, RouterModule, LajiUiModule],
+  imports: [routing, SharedModule, RouterModule, LajiUiModule, TechnicalNewsModule],
   declarations: [InformationComponent],
 })
 export class InformationModule {}
