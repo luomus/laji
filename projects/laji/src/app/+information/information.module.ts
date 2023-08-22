a51c3411b848011b920b8567750d39a1d5b0ca7b
Markus Lumme <markusjuhani.lumme@gmail.com>
2023-08-22 10:21:55 +0300

    Requested style fix and forgotten test value removed.

--- projects/laji/src/app/+information/information.module.ts
+++ projects/laji/src/app/+information/information.module.ts
@@ -5,6 +5,7 @@ import { routing } from './information.routes';
 import { InformationComponent } from './information.component';
 import { LajiUiModule } from 'projects/laji-ui/src/public-api';
 import { TechnicalNewsModule } from '../shared-modules/technical-news/technical-news.module';
+
 @NgModule({
   imports: [routing, SharedModule, RouterModule, LajiUiModule, TechnicalNewsModule],
   declarations: [InformationComponent],
