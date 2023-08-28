e7d10df22cd3a8e723fa3dd237a406a376d920ee
aorin <meeri.rannisto@gmail.com>
2023-08-28 14:32:18 +0300

    disable ssr for laji form builder

---
+++ projects/laji/src/app/+project-form-edit/laji-form-builder/laji-form-builder.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
+import { LajiFormBuilderComponent } from './laji-form-builder.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+  ],
+  declarations: [LajiFormBuilderComponent],
+  exports: [LajiFormBuilderComponent]
+})
+export class LajiFormBuilderModule { }
