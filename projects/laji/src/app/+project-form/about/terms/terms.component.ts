d84c784bea86187874b983736da1222da91eac26
Olli Raitio <olli.raitio@helsinki.fi>
2023-09-26 12:51:07 +0300

    disable ngx bootstrap (DO NOT MERGE!)

--- projects/laji/src/app/+project-form/about/terms/terms.component.ts
+++ projects/laji/src/app/+project-form/about/terms/terms.component.ts
@@ -1,6 +1,5 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { LocalStorage } from 'ngx-webstorage';
-import { ModalDirective } from 'ngx-bootstrap/modal';
 import { UserService } from '../../../shared/service/user.service';
 import { take } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
@@ -18,7 +17,6 @@ export class TermsComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() modal = false;
   @Input() dismissLabel = 'Ok';
 
-  @ViewChild('modal') public modalComponent: ModalDirective;
 
   modalIsVisible = false;
   private showModalSub: Subscription;
@@ -36,23 +34,9 @@ export class TermsComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    if (!this.modalComponent) {
-      return;
-    }
-
-    this.modalComponent.onShown.subscribe(() => { this.modalIsVisible = true; });
-    this.modalComponent.onHidden.subscribe(() => { this.modalIsVisible = false; });
-    this.showModalSub = this.userService.isLoggedIn$.pipe(take(1)).subscribe(isLoggedIn => {
-      if (isLoggedIn) {
-        this.modalComponent.show();
-      }
-    });
   }
 
   toggleInfo() {
     this.vihkoSettings = {showIntro: !this.vihkoSettings.showIntro};
-    if (this.modalComponent && this.modalIsVisible) {
-      this.modalComponent.hide();
-    }
   }
 }
