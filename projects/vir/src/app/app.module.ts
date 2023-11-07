c914eec044a34d05ef98e48e2e80b95ccc454535
Olli Raitio <olli.raitio@helsinki.fi>
2023-11-07 16:07:55 +0200

    fix vir app module

--- projects/vir/src/app/app.module.ts
+++ projects/vir/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { APP_ID, ErrorHandler, NgModule } from '@angular/core';
 import { APP_BASE_HREF, CommonModule, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { NgtUniversalModule } from '@ng-toolkit/universal';
 import { NgxWebstorageModule } from 'ngx-webstorage';
 import { TransferHttpCacheModule } from '@nguniversal/common';
 import { ToastrModule } from 'ngx-toastr';
@@ -45,7 +44,6 @@ export function createLoggerLoader(loggerApi: LoggerApi): ILogger {
     BrowserModule,
     CommonModule,
     HttpClientModule,
-    NgtUniversalModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
