9f32db3b3a644bcbe392b58963cc40ce4bb289ed
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2023-12-18 09:24:22 +0200

    rm comments

--- projects/laji/test/+error/error.page.ts
+++ projects/laji/test/+error/error.page.ts
@@ -1,17 +1,11 @@
-// import { by, element } from 'protractor';
-
-import {Locator, Page} from '@playwright/test';
+import { Page } from '@playwright/test';
 
 export class ErrorPage {
-
   errorDialog = this.page.locator('.toast-error');
 
   constructor(private page: Page) { }
 
-  // private errorDialog = element(by.css('.toast-error'));
-  //
   async isPresentErrorDialog() {
     return await this.errorDialog.isVisible();
-    // return this.errorDialog.isPresent() as Promise<boolean>;
   }
 }
