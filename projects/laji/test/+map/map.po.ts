c331eb16373bfb41cec371ae884f918151aec287
Olli Raitio <olli.raitio@helsinki.fi>
2024-03-20 12:04:37 +0200

    fix playwright map po

--- projects/laji/test/+map/map.po.ts
+++ projects/laji/test/+map/map.po.ts
@@ -2,14 +2,12 @@ import { Locator, Page } from '@playwright/test';
 import { MapPageObject } from '@luomus/laji-map/test-export/test-utils';
 
 export class MapPage {
-  private page: Page;
   public $closeTile: Locator;
 
-  constructor(page: Page) {
-    this.page = page;
+  constructor(private page: Page) {
     this.$closeTile = page.locator('img[src="https://proxy.laji.fi/mml_wmts/maasto/wmts/1.0.0/maastokartta/default/ETRS-TM35FIN/15/24319/13521.png"]');
   }
-  public readonly map = new MapPageObject();
+  public readonly map = new MapPageObject(this.page);
 
   navigateToMapWithObservationData() {
     return this.page.goto('/map?coordinates=%2B61.578877%2B023.546876%2F');
