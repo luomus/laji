02a78452e55f0673377e76e39be3f48acf931d28
rpulkka <robert.pulkka@gmail.com>
2024-04-26 13:53:53 +0300

    Pass taxonOptions as regular input.

--- projects/laji/src/app/+project-form/results/bird-point-count-result/bird-point-count-result-chart/bird-point-count-result-chart.component.ts
+++ projects/laji/src/app/+project-form/results/bird-point-count-result/bird-point-count-result-chart/bird-point-count-result-chart.component.ts
@@ -54,7 +54,7 @@ export class BirdPointCountResultChartComponent implements OnInit, OnDestroy {
   readonly taxon$ = new BehaviorSubject<string | undefined>(undefined);
   @Input() set collections(v: string[]) { this.collections$.next(v); };
   @Input() set taxon(v: string | undefined) { this.taxon$.next(v); };
-  @Input() taxonOptions$: Observable<{ label: string; value: string }[]>;
+  @Input() taxonOptions: { label: string; value: string }[];
 
   @Output() taxonChange = new EventEmitter<string>();
 
