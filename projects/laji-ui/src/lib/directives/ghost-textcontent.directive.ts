0485ffd2cdc3791c87e707ba8478b6f8c2e878d0
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2024-08-29 13:37:56 +0300

    move from deprecated DOMSubtreeModified event to using MutationObserver

--- projects/laji-ui/src/lib/directives/ghost-textcontent.directive.ts
+++ projects/laji-ui/src/lib/directives/ghost-textcontent.directive.ts
@@ -9,21 +9,29 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class GhostTextContentDirective implements OnInit, OnDestroy {
   @Input() disableTextContentGhost = false;
-  destroyListener: () => void;
+
+  mutationObserver: MutationObserver;
+
   constructor(private el: ElementRef, private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: any) {}
+
   ngOnInit() {
-    if (isPlatformBrowser(this.platformId) && !this.disableTextContentGhost && (!this.el.nativeElement || this.el.nativeElement.textContent.trim().length === 0)) {
+    if (
+      isPlatformBrowser(this.platformId)
+      && !this.disableTextContentGhost
+      && (!this.el.nativeElement || this.el.nativeElement.textContent.trim().length === 0)
+    ) {
       this.renderer.addClass(this.el.nativeElement, 'lu-ghost-textcontent');
-      this.destroyListener = this.renderer.listen(this.el.nativeElement, 'DOMSubtreeModified', () => {
+      this.mutationObserver = new MutationObserver((mutationList, mutationObserver) => {
         if (this.el.nativeElement.textContent.trim().length > 0) {
           this.renderer.removeClass(this.el.nativeElement, 'lu-ghost-textcontent');
+          mutationObserver.disconnect();
         }
       });
+      this.mutationObserver.observe(this.el.nativeElement, { characterData: true, subtree: true });
     }
   }
+
   ngOnDestroy() {
-    if (this.destroyListener) {
-      this.destroyListener();
-    }
+    this.mutationObserver?.disconnect();
   }
 }
