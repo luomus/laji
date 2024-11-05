cddeb24fe6ffe463e1c9419e0cb2752399e5def4
rpulkka <robert.pulkka@gmail.com>
2024-11-05 15:26:40 +0200

    Strict TS config - Week 3: New result services [#586]

--- projects/laji/src/app/+project-form/results/bird-point-count-result/bird-point-count-result.component.ts
+++ projects/laji/src/app/+project-form/results/bird-point-count-result/bird-point-count-result.component.ts
@@ -33,12 +33,12 @@ type State = ChartState | MapState;
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BirdPointCountResultComponent implements OnInit, OnDestroy {
-  @Input() form: Form.SchemaForm;
+  @Input() form!: Form.SchemaForm;
 
   Tabs = Tabs; // eslint-disable-line @typescript-eslint/naming-convention
-  state$: Observable<State>;
+  state$!: Observable<State>;
   collections: string[] = ['HR.157'];
-  taxonOptions$: Observable<{ label: string; value: string }[]>;
+  taxonOptions$!: Observable<{ label: string; value: string }[]>;
   isChartState = (state: State): state is ChartState => state.tab === Tabs.chart;
   isMapState = (state: State): state is MapState => state.tab === Tabs.map;
   mapQuery: WarehouseQueryInterface = {
@@ -46,7 +46,7 @@ export class BirdPointCountResultComponent implements OnInit, OnDestroy {
     gatheringCounts: true, cache: true, countryId: ['ML.206']
   };
 
-  private defaultTabSubscription: Subscription;
+  private defaultTabSubscription!: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -83,7 +83,7 @@ export class BirdPointCountResultComponent implements OnInit, OnDestroy {
       map(res => res.results),
       map(taxa => taxa.map(t => ({
         label: (t.vernacularName ? t.vernacularName + ' - ' : '') + (t.scientificName ? t.scientificName : ''),
-        value: t.id
+        value: t.id ?? ''
       }))),
       map(pairs => [{ label: this.translate.instant('result.map.taxon.empty.label'), value: '' }].concat(pairs))
     );
