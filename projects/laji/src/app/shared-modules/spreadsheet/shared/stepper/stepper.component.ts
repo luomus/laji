cbf5c7ae186ba698ff2f54cd22e5982d9d034fc3
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2025-03-15 09:27:16 +0200

    extract laji ui stepper from spreadsheet stepper component

--- projects/laji/src/app/shared-modules/spreadsheet/shared/stepper/stepper.component.ts
+++ projects/laji/src/app/shared-modules/spreadsheet/shared/stepper/stepper.component.ts
@@ -2,45 +2,46 @@ import {ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Inp
 import { TranslateService } from '@ngx-translate/core';
 import { Step } from '../../spreadsheet.facade';
 
+const defaultMapping = {
+  [Step.empty]: 'file',
+  [Step.sheetLoadError]: 'file',
+  [Step.fileAlreadyUploadedPartially]: 'file',
+  [Step.fileAlreadyUploaded]: 'file',
+  [Step.invalidFileType]: 'file',
+  [Step.invalidFormId]: 'file',
+  [Step.importingFile]: 'file',
+  [Step.colMapping]: 'colMapping',
+  [Step.dataMapping]: 'valueMapping',
+  [Step.importReady]: 'send',
+  [Step.validating]: 'send',
+  [Step.invalidData]: 'send',
+  [Step.importing]: 'send',
+  [Step.doneWithErrors]: 'done',
+  [Step.doneOk]: 'done'
+};
+
+const defaultSteps = [
+  {name: 'file', label: 'excel.step1', returnState: Step.empty},
+  {name: 'colMapping', label: 'excel.step2', returnState: Step.colMapping},
+  {name: 'valueMapping', label: 'excel.step3', returnState: Step.dataMapping},
+  {name: 'send', label: 'excel.step4', returnState: Step.empty},
+  {name: 'done', label: 'excel.step5', returnState: Step.importReady}
+];
+
 @Component({
   selector: 'laji-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StepperComponent implements OnInit, OnChanges {
-
-  active!: number;
+  @Input() state!: Step;
+  @Input() mapping = defaultMapping;
+  @Input() steps: {name: string; label: string; returnState: number}[] = defaultSteps;
   @Output() activate = new EventEmitter<number>();
   @Output() title = new EventEmitter<string>();
 
-  @Input() state!: Step;
-
-  @Input() mapping = {
-    [Step.empty]: 'file',
-    [Step.sheetLoadError]: 'file',
-    [Step.fileAlreadyUploadedPartially]: 'file',
-    [Step.fileAlreadyUploaded]: 'file',
-    [Step.invalidFileType]: 'file',
-    [Step.invalidFormId]: 'file',
-    [Step.importingFile]: 'file',
-    [Step.colMapping]: 'colMapping',
-    [Step.dataMapping]: 'valueMapping',
-    [Step.importReady]: 'send',
-    [Step.validating]: 'send',
-    [Step.invalidData]: 'send',
-    [Step.importing]: 'send',
-    [Step.doneWithErrors]: 'done',
-    [Step.doneOk]: 'done'
-  };
-
-  @Input() steps: {name: string; label: string; returnState: number}[] = [
-    {name: 'file', label: 'excel.step1', returnState: Step.empty},
-    {name: 'colMapping', label: 'excel.step2', returnState: Step.colMapping},
-    {name: 'valueMapping', label: 'excel.step3', returnState: Step.dataMapping},
-    {name: 'send', label: 'excel.step4', returnState: Step.empty},
-    {name: 'done', label: 'excel.step5', returnState: Step.importReady}
-  ];
+  currentStep = 0;
+  luStepperSteps!: string[];
 
   private labels = false;
 
@@ -50,6 +51,7 @@ export class StepperComponent implements OnInit, OnChanges {
   ) { }
 
   ngOnInit() {
+    this.updateSteps();
     this.translateService.get(this.steps.map(step => step.label))
       .subscribe(labels => {
         this.steps.forEach(step => {
@@ -66,21 +68,28 @@ export class StepperComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     if (changes.state) {
       const mappedState = (this.mapping as any)[this.state] || 'file';
-      this.active = this.steps.findIndex((item) => item.name === mappedState);
+      this.currentStep = this.steps.findIndex((item) => item.name === mappedState);
       this.sendActive();
+    }
+
+    if (changes.steps) {
+      this.updateSteps();
     }
   }
 
-  backTo(idx: number) {
-    if (idx < this.active) {
+  onStepBack(idx: number) {
+    if (idx < this.currentStep) {
       this.activate.emit(this.steps[idx].returnState);
     }
   }
 
-  private sendActive() {
-    if (this.active > -1 && this.labels) {
-      this.title.emit(this.steps[this.active].label);
-    }
+  private updateSteps() {
+    this.luStepperSteps = this.steps.map(step => this.translateService.instant(step.label) || '');
   }
 
+  private sendActive() {
+    if (this.currentStep > -1 && this.labels) {
+      this.title.emit(this.steps[this.currentStep].label);
+    }
+  }
 }
