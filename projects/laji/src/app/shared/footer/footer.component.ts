fdf847ab3d373784968b45545672fc6c9aac07f3
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2025-03-29 16:15:15 +0200

    update footer links

--- projects/laji/src/app/shared/footer/footer.component.ts
+++ projects/laji/src/app/shared/footer/footer.component.ts
@@ -1,13 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { FooterService } from '../service/footer.service';
+
+interface MaybeMultiLang { fi: string; sv?: string; en?: string };
+
+const infoLajiFiLangUrlMap: Record<string, MaybeMultiLang> = {
+  readMore: { fi: 'https://info.laji.fi/', sv: 'https://info.laji.fi/sv/', en: 'https://info.laji.fi/en/' },
+  contact: { fi: 'https://info.laji.fi/etusivu/lajitietokeskus-pahkinankuoressa/yhteystiedot/' },
+  privacyPolicy: { fi: 'https://info.laji.fi/etusivu/lajitietokeskus-pahkinankuoressa/tietosuojaseloste/' },
+  accessibility: { fi: 'https://info.laji.fi/etusivu/lajitietokeskus-pahkinankuoressa/saavutettavuusseloste/' },
+  dataPolicy: { fi: 'https://info.laji.fi/etusivu/lajitietokeskus-pahkinankuoressa/aineistopolitiikka/' },
+  restApi: { fi: 'https://info.laji.fi/etusivu/api/' },
+  rApi: { fi: 'https://info.laji.fi/etusivu/r-paketti/' },
+  gis: { fi: 'https://info.laji.fi/etusivu/paikkatieto/' },
+  support: { fi: 'https://info.laji.fi/' },
+  faq: { fi: 'https://info.laji.fi/ukk/' },
+  dataUsers: { fi: 'https://info.laji.fi/etusivu/oppaat/opas-datan-omistajille/' },
+  dataHolders: { fi: 'https://info.laji.fi/etusivu/oppaat/opas-datan-kayttajille/' },
+  researchers: { fi: 'https://info.laji.fi/etusivu/oppaat/opas-tutkijoille/' },
+  natureObservers: { fi: 'https://info.laji.fi/etusivu/oppaat/opas-luontoharrastajille/' },
+  developers: { fi: 'https://info.laji.fi/etusivu/oppaat/opas-kehittajille/' },
+  media: { fi: 'https://info.laji.fi/etusivu/oppaat/opas-medialle/' },
+  taxa: { fi: 'https://info.laji.fi/etusivu/taksonomian-hallinta/' },
+  observations: { fi: 'https://info.laji.fi/etusivu/havaintotieto/' },
+  gisServices: { fi: 'https://info.laji.fi/etusivu/paikkatieto/' },
+  citizenScience: { fi: 'https://info.laji.fi/kansalaistiede-ja-lajien-seuranta/' },
+  identification: { fi: 'https://info.laji.fi/etusivu/lajintunnistus/' },
+  apis: { fi: 'https://info.laji.fi/etusivu/oppaat/opas-kehittajille/' },
+};
 
 @Component({
   selector: 'laji-footer',
   styleUrls: ['./footer.component.scss'],
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
   @Input() onFrontPage = false;
 
-  constructor(public footerService: FooterService) {}
+  footerVisible$: Observable<boolean>;
+
+  constructor(footerService: FooterService, private translate: TranslateService, private cdr: ChangeDetectorRef) {
+    this.footerVisible$ = footerService.footerVisible$;
+  }
+
+  ngAfterViewInit() {
+    this.translate.onLangChange.subscribe(() => this.cdr.markForCheck());
+  }
+
+  getInfoLajiFiUrl(key: keyof typeof infoLajiFiLangUrlMap): string {
+    return infoLajiFiLangUrlMap[key][this.translate.currentLang as keyof typeof infoLajiFiLangUrlMap[typeof key]]
+      ?? infoLajiFiLangUrlMap[key].fi;
+  }
 }
