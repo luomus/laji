9b6cee5a61592b5235218a811fdbf2a0b3b68f71
aorin <meeri.rannisto@gmail.com>
2025-05-20 12:45:49 +0300

    [bsg] add bat instructions page

--- projects/kerttu-global/src/app/+bat-identification/bat-identification-instructions/bat-identification-instructions.component.ts
+++ projects/kerttu-global/src/app/+bat-identification/bat-identification-instructions/bat-identification-instructions.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bsg-bat-identification-instructions',
   template: `
-    <laji-info-page [page]="{'en': '6576', 'es': '7755', 'fr': '7789'} | multiLang"></laji-info-page>
+    <laji-info-page [page]="{'en': '8774', 'es': '8774', 'fr': '8774'} | multiLang"></laji-info-page>
   `
 })
 export class BatIdentificationInstructionsComponent {}
