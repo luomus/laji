d2096358e6794e150909997506d6e6df263f9908
rpulkka <robert.pulkka@gmail.com>
2025-06-09 09:27:58 +0300

    Temporary fix to mobile form tests.

--- projects/laji/test/+project-form/project-form.spec.ts
+++ projects/laji/test/+project-form/project-form.spec.ts
@@ -177,8 +177,12 @@ test.describe('Project form', () =>  {
       });
 
       test('canceling document save redirects to about page and keeps lang if no history', async () => {
+        await page.getByText('Accept and record observations').click();
+        await page.locator('.use-button').click();
+        await expect(page.locator('laji-project-form-form')).toBeVisible();
+        await page.mouse.click(300, 300);
+        await page.getByText('Choose this location').click();
         await page.locator('.media-add-modal .cancel').click();
-        await page.locator('.floating-buttons-container button').last().click();
         await page.locator('laji-form-footer .btn-danger').click();
 
         await expect(page.locator('laji-about')).toBeVisible();
