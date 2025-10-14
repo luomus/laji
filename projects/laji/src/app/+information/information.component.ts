57f12eb5256a11f32fd683163d5b16d5cbcd70c9
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2025-10-14 10:39:36 +0300

    redirect about page 404s to info.laji.fi

--- projects/laji/src/app/+information/information.component.ts
+++ projects/laji/src/app/+information/information.component.ts
@@ -2,11 +2,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Logger } from '../shared/logger/logger.service';
-import { Information } from '../shared/model/Information';
 import { Title } from '@angular/platform-browser';
 import { LajiApi, LajiApiService } from '../shared/service/laji-api.service';
 import { catchError, delay, filter, map, switchMap, tap } from 'rxjs/operators';
 import { getDescription, HeaderService } from '../shared/service/header.service';
+import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
+import { components } from 'projects/laji-api-client-b/generated/api';
+import { PlatformService } from '../root/platform.service';
+
+type Information = components['schemas']['Information'];
 
 @Component({
   selector: 'laji-information',
@@ -19,19 +23,23 @@ export class InformationComponent {
   information$: Observable<Information|null>;
 
   constructor(private route: ActivatedRoute,
-              private lajiApi: LajiApiService,
+              private api: LajiApiClientBService,
               private logger: Logger,
               private headerService: HeaderService,
-              private title: Title
+              private title: Title,
+              private platform: PlatformService,
   ) {
     this.information$ = this.route.params.pipe(
       map(params => params['id']),
-      switchMap(id => this.lajiApi.get(LajiApi.Endpoints.information, id).pipe(
+      switchMap(id => this.api.get('/information/{id}', { path: { id } }).pipe(
         filter(info => info.id === id)
       )),
       delay(0),
       tap(info => this.updateHeaders(info)),
       catchError(err => {
+        if (err?.error?.statusCode === 404 && this.platform.isBrowser) {
+          window.location.assign('https://info.laji.fi/');
+        }
         this.logger.warn('Failed to fetch root informations', err);
         return of(null);
       })
