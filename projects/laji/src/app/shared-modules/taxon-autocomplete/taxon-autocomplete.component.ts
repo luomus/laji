luomus/laji

commit 74715ac09afe59b7aa235cdf8f57819fd0c635e2

autocomplete requests done with new api client

projects/laji/src/app/shared-modules/taxon-autocomplete/taxon-autocomplete.component.ts
@@ -16,10 +16,11 @@ import {
   ViewChild
 } from '@angular/core';
 import { Observable, of, of as ObservableOf, Subscription, timer } from 'rxjs';
-import { TranslateService } from '@ngx-translate/core';
-import { Autocomplete } from '../../shared/model/Autocomplete';
-import { LajiApi, LajiApiService } from '../../shared/service/laji-api.service';
 import { TaxonAutocompleteService } from '../../shared/service/taxon-autocomplete.service';
+import { components } from 'projects/laji-api-client-b/generated/api.d';
+import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
+
+type TaxonAutocompleteResponse = components['schemas']['TaxonAutocompleteResponse'];
 
 @Component({
   selector: 'laji-taxon-autocomplete',
@@ -47,13 +48,13 @@ export class TaxonAutocompleteComponent implements AfterViewInit, OnDestroy {
   @Input() whiteList?: string[];
   @Input() blackList?: string[];
   @Output() finish = new EventEmitter<void>();
-  @Output() taxonSelect = new EventEmitter<Autocomplete>();
+  @Output() taxonSelect = new EventEmitter<TaxonAutocompleteResponse>();
 
   @ViewChild('input') inputEl!: ElementRef;
 
   dataSource: Observable<any>;
   value: string | undefined = '';
-  result?: Autocomplete;
+  result?: TaxonAutocompleteResponse;
   loading = false;
   taxonSub?: Subscription;
 
@@ -61,8 +62,7 @@ export class TaxonAutocompleteComponent implements AfterViewInit, OnDestroy {
   private destroyBlurListener?: () => void;
 
   constructor(
-    private lajiApi: LajiApiService,
-    private translateService: TranslateService,
+    private api: LajiApiClientBService,
     private cdr: ChangeDetectorRef,
     private taxonAutocompleteService: TaxonAutocompleteService,
     private renderer: Renderer2
@@ -111,46 +111,44 @@ export class TaxonAutocompleteComponent implements AfterViewInit, OnDestroy {
     }
   }
 
-  getTaxa(token: string, onlyExact = false): Observable<any> {
-    if (!token || token.length < this.tokenMinLength) {
+  getTaxa(query: string, onlyExact = false): Observable<any> {
+    if (!query || query.length < this.tokenMinLength) {
       return of([]);
     }
     this.loading = true;
     this.cdr.markForCheck();
     return timer(onlyExact ? this.index * 100 : 0).pipe(
-      switchMap(() => this.lajiApi.get(LajiApi.Endpoints.autocomplete, 'taxon', {
-        q: token,
-        limit: '' + this.limit,
-        includePayload: true,
-        lang: this.translateService.currentLang,
-        matchType: LajiApi.AutocompleteMatchType.partial,
+      switchMap(() => this.api.get('/autocomplete/taxa', { query: {
+        query,
+        limit: this.limit,
+        matchType: 'partial',
         informalTaxonGroup: this.informalTaxonGroup,
         excludeNameTypes: this.excludeNameTypes,
         onlyFinnish: this.onlyFinnish,
         onlyInvasive: this.onlyInvasive,
         onlySpecies: this.onlySpecies
-      })),
-      map(data => {
+      }})),
+      map(({ results }) => {
         if (this.whiteList) {
           // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          data = data.filter(item => this.whiteList!.includes(item.key as any));
+          results = results.filter(item => this.whiteList!.includes(item.key as any));
         }
         if (this.blackList) {
           // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          data = data.filter(item => !this.blackList!.includes(item.key as any));
+          results = results.filter(item => !this.blackList!.includes(item.key as any));
         }
         if (onlyExact) {
-          if (data[0] && data[0].payload.matchType && data[0].payload.matchType === 'exactMatches' && (
-            !data[1] || data[1].payload.matchType && data[1].payload.matchType !== 'exactMatches'
+          if (results[0] && results[0].type && results[0].type === 'exactMatches' && (
+            !results[1] || results[1].type && results[1].type !== 'exactMatches'
           )) {
-            return data[0];
+            return results[0];
           }
           return {};
         }
-        return data.map(item => {
+        return results.map(item => {
           let groups = '';
-          if (item.payload && item.payload.informalTaxonGroups) {
-            groups = item.payload.informalTaxonGroups.reduce((prev: string, curr: any) => prev + ' ' + curr.id, groups);
+          if (item.informalGroups) {
+            groups = item.informalGroups.reduce((prev: string, curr: any) => prev + ' ' + curr.id, groups);
           }
           (item as any)['groups'] = groups;
           return item;
@@ -193,11 +191,12 @@ export class TaxonAutocompleteComponent implements AfterViewInit, OnDestroy {
     if (!this.allowEmpty && !this.value) {
       return;
     }
-    this.onTaxonSelect(<Autocomplete>{key: '', value: this.value, payload: {
-        key: '',
-        cursiveName: false,
-        vernacularName: this.value
-      }});
+    this.onTaxonSelect(<TaxonAutocompleteResponse>{
+      key: '',
+      value: this.value,
+      cursiveName: false,
+      vernacularName: this.value
+    });
   }
 
 }

projects/laji/src/app/+observation/observation.facade.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { WarehouseQueryInterface } from '../shared/model/WarehouseQueryInterface';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { catchError, distinctUntilChanged, map, share, switchMap, take, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { distinctUntilChanged, map, switchMap, take, tap } from 'rxjs/operators';
 import { hotObjectObserver } from '../shared/observable/hot-object-observer';
 import { BrowserService } from '../shared/service/browser.service';
-import { LajiApi, LajiApiService } from '../shared/service/laji-api.service';
-import { TranslateService } from '@ngx-translate/core';
 import { UserService } from '../shared/service/user.service';
-import { Autocomplete } from '../shared/model/Autocomplete';
 import { FooterService } from '../shared/service/footer.service';
-import { WarehouseApi } from '../shared/api/WarehouseApi';
 import { ObservationDataService } from './observation-data.service';
 import { SearchQueryService } from './search-query.service';
 import { Util } from '../shared/service/util.service';
 import { LocalStorage } from 'ngx-webstorage';
+import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
+import { components } from 'projects/laji-api-client-b/generated/api.d';
+
+type TaxonAutocompleteResponse = components['schemas']['TaxonAutocompleteResponse'];
 
 interface IPersistentState {
   showSearchButtonInfo: boolean;
@@ -32,7 +32,7 @@ interface IObservationState extends IPersistentState {
   loadingUnits: boolean;
 }
 
-interface ITaxonAutocomplete extends Autocomplete {
+export interface ITaxonAutocomplete extends TaxonAutocompleteResponse {
   groups: string;
 }
 
@@ -103,8 +103,7 @@ export class ObservationFacade {
 
   constructor(
     private browserService: BrowserService,
-    private lajiApi: LajiApiService,
-    private translateService: TranslateService,
+    private api: LajiApiClientBService,
     private userService: UserService,
     private footerService: FooterService,
     private observationDataService: ObservationDataService
@@ -174,19 +173,17 @@ export class ObservationFacade {
     this.updatePersistentState({...this.persistentState, showSearchButtonInfo: false});
   }
 
-  taxaAutocomplete(token: string, informalTaxonGroupId: string[]|undefined, limit: number): Observable<ITaxonAutocomplete[]> {
-    return this.lajiApi.get(LajiApi.Endpoints.autocomplete, 'taxon', {
-      q: token,
-      limit: '' + limit,
-      includePayload: true,
-      lang: this.translateService.currentLang,
+  taxaAutocomplete(query: string, informalTaxonGroupId: string[] | undefined, limit: number): Observable<ITaxonAutocomplete[]> {
+    return this.api.get('/autocomplete/taxa', { query: {
+      query,
+      limit,
       informalTaxonGroup: informalTaxonGroupId?.toString(),
       excludeNameTypes: 'MX.hasMisspelledName,MX.hasMisappliedName'
-    } as LajiApi.Query.AutocompleteQuery).pipe(
-      map<Autocomplete[], ITaxonAutocomplete[]>(data => data.map(item => {
+    } }).pipe(
+      map(data => data.results.map(item => {
         let groups = '';
-        if (item.payload && item.payload.informalTaxonGroups) {
-          groups = item.payload.informalTaxonGroups.reduce((prev: string, curr: any) => prev + ' ' + curr.id, groups);
+        if (item.informalGroups) {
+          groups = item.informalGroups.reduce((prev: string, curr: any) => prev + ' ' + curr.id, groups);
         }
         return {...item, groups};
       }))
@@ -215,16 +212,6 @@ export class ObservationFacade {
       map(data => data.speciesCount!),
       tap(countTaxa => this.updateState({..._state, loadingTaxa: false, countTaxa})),
       tap(() => this.browserService.triggerResizeEvent())
-    );
-  }
-
-  private count(src: Observable<any>, loadingKey: keyof IObservationState, countKey:  keyof IObservationState) {
-    return src.pipe(
-      map(result => result.total),
-      catchError(() => of(null)),
-      distinctUntilChanged(),
-      tap((cnt) => this.updateState({..._state, [loadingKey]: false, [countKey]: cnt})),
-      share()
     );
   }
 

projects/laji/src/app/+theme/sound-identification/sound-identification-table/sound-identification-table.component.html
@@ -12,14 +12,14 @@
 
   <ng-template let-row="row" #clipSpan>
     <span>
-      {{row.start_time + "-" + row.end_time + "s"}} 
+      {{row.start_time + "-" + row.end_time + "s"}}
     </span>
   </ng-template>
 
   <ng-template let-row="row" #vernacularName>
-    <ng-container *ngIf="(getVernacularName(row.scientific_name) | async | multiLang); let vernacularName; else spinner">
+    <ng-container *ngIf="(getVernacularName(row.scientific_name) | async); let vernacularName; else spinner">
       <span>
-        {{vernacularName}} 
+        {{vernacularName}}
       </span>
     </ng-container>
   </ng-template>

projects/laji/src/app/+theme/sound-identification/sound-identification-table/sound-identification-table.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy, ViewChild, TemplateRef, Input } from '@angular/core';
 import { IdentificationData } from '../sound-identification-api';
 import { Observable } from 'rxjs';
-import { LajiApi, LajiApiService } from 'projects/laji/src/app/shared/service/laji-api.service';
-import { map, shareReplay, tap } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { DatatableColumn } from '../../../shared-modules/datatable/model/datatable-column';
+import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
 
 @Component({
   selector: 'laji-sound-identification-table',
@@ -19,17 +19,11 @@ export class SoundIdentificationTableComponent implements OnInit {
   @Input() loading = false;
 
   columns!: DatatableColumn[];
-  vernacularCache: {
-    [key: string]: Observable<{
-      'fi'?: string;
-      'en'?: string;
-      'sv'?: string;
-    }>;
-  } = {};
+  vernacularCache: Record<string, Observable<string>> = {};
   lang!: string;
 
   constructor(
-    private lajiApiService: LajiApiService,
+    private api: LajiApiClientBService
   ) { }
 
   ngOnInit() {
@@ -61,15 +55,12 @@ export class SoundIdentificationTableComponent implements OnInit {
 
   getVernacularName(scientificName: string) {
     if (!this.vernacularCache[scientificName]) {
-      this.vernacularCache[scientificName] = this.lajiApiService
-        .get(LajiApi.Endpoints.autocomplete, 'taxon',
-          {
-            q: scientificName,
-            lang: 'multi',
-            includePayload: true,
-            matchType: LajiApi.AutocompleteMatchType.exact
-          } as LajiApi.Query.AutocompleteQuery).pipe(
-          map(data => data[0]?.payload.vernacularName),
+      this.vernacularCache[scientificName] = this.api.get('/autocomplete/taxa', { query: {
+            query: scientificName,
+            matchType: 'exact',
+        selectedFields: 'vernacularName'
+      }}).pipe(
+          map(data => data.results[0]?.vernacularName),
           shareReplay(1)
         );
     }

projects/laji/src/app/shared-modules/annotations/annotation-form-new/annotation-form-new.component.ts
@@ -8,7 +8,6 @@ import { AnnotationService } from '../../document-viewer/service/annotation.serv
 import { Observable, Subscription } from 'rxjs';
 import { Logger } from '../../../shared/logger/logger.service';
 import { TranslateService } from '@ngx-translate/core';
-import { LajiApi, LajiApiService } from '../../../shared/service/laji-api.service';
 import { AnnotationTag } from '../../../shared/model/AnnotationTag';
 import { Global } from '../../../../environments/global';
 import { IdService } from '../../../shared/service/id.service';
@@ -110,7 +109,6 @@ export class AnnotationFormNewComponent implements OnInit , OnChanges, AfterCont
   constructor(
     private annotationService: AnnotationService,
     private loggerService: Logger,
-    private lajiApi: LajiApiService,
     private api: LajiApiClientBService,
     private translate: TranslateService,
     private cd: ChangeDetectorRef,
@@ -136,18 +134,17 @@ export class AnnotationFormNewComponent implements OnInit , OnChanges, AfterCont
     this.cd.detectChanges();
   }
 
-  public getTaxa(token: string): Observable<any> {
-    return this.lajiApi.get(LajiApi.Endpoints.autocomplete, 'taxon', {
-      q: token,
-      limit: '10',
-      includePayload: true
-    }).pipe(
-      map(data => data.map((item: any) => {
+  public getTaxa(query: string) {
+    return this.api.get('/autocomplete/taxa', { query: {
+      query,
+      limit: 10
+    }}).pipe(
+      map(data => data.results.map(item => {
         let groups = '';
-        if (item.payload && item.payload.informalTaxonGroups) {
-          groups = item.payload.informalTaxonGroups.reduce((prev: string, curr: InformalTaxonGroup) => prev + ' ' + curr.id, groups);
+        if (item.informalGroups) {
+          groups = item.informalGroups.reduce((prev: string, curr: InformalTaxonGroup) => prev + ' ' + curr.id, groups);
         }
-        item['groups'] = groups;
+        (item as any)['groups'] = groups;
         return item;
       })));
   }

projects/laji/src/app/shared-modules/find-person/find-person.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Person } from '../../shared/model/Person';
-import { LajiApi, LajiApiService } from '../../shared/service/laji-api.service';
-import { mergeMap } from 'rxjs/operators';
+import { map, mergeMap } from 'rxjs/operators';
+import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
 
 @Component({
   selector: 'laji-find-person',
@@ -20,7 +20,9 @@ export class FindPersonComponent implements OnInit {
   typeaheadLoading = false;
   value = '';
 
-  constructor(private lajiApi: LajiApiService) { }
+  constructor(
+    private api: LajiApiClientBService
+  ) { }
 
   ngOnInit() {
     this.dataSource = Observable.create((observer: any) => {
@@ -31,10 +33,8 @@ export class FindPersonComponent implements OnInit {
   }
 
   public getPerson(token: string): Observable<any> {
-    return this.lajiApi.get(LajiApi.Endpoints.autocomplete, 'person', {
-        q: token,
-        limit: '' + this.limit
-      });
+    return this.api.get('/autocomplete/persons', { query: { query: token, pageSize: this.limit } })
+    .pipe(map(({ results }) => results));
   }
 
   personSelected(event: any) {

projects/laji/src/app/shared-modules/spreadsheet/importer/cell-value-mapping/special-friend/special-friend.component.ts
@@ -41,6 +41,9 @@ export class SpecialFriendComponent implements OnInit {
     this.friendsService.allFriends()
       .subscribe((friends) => {
         friends.forEach(friend => {
+          if (!friend.value) {
+            return;
+          }
           validValues.push(friend.value);
           validIds.push(friend.key);
         });

projects/laji/src/app/shared-modules/taxon-select/taxon-select.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component,
 EventEmitter, Input, Output, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { Observable, of as ObservableOf, Subscription } from 'rxjs';
-import { distinctUntilChanged, switchMap, take } from 'rxjs/operators';
-import { TranslateService } from '@ngx-translate/core';
-import { LajiApi, LajiApiService } from '../../shared/service/laji-api.service';
+import { distinctUntilChanged, map, switchMap, take } from 'rxjs/operators';
 import { TaxonAutocompleteService } from '../../shared/service/taxon-autocomplete.service';
 import { BrowserService } from 'projects/laji/src/app/shared/service/browser.service';
+import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
 
 
 @Component({
@@ -64,8 +63,7 @@ export class TaxonSelectComponent implements OnInit, OnDestroy {
   public screenWidthSub?: Subscription;
 
   constructor(
-    private lajiApi: LajiApiService,
-    private translate: TranslateService,
+    private api: LajiApiClientBService,
     private cdr: ChangeDetectorRef,
     private browserService: BrowserService,
     private taxonAutocompleteService: TaxonAutocompleteService
@@ -163,13 +161,11 @@ export class TaxonSelectComponent implements OnInit, OnDestroy {
     this.typeahead.nativeElement.blur();
   }
 
-  public getTaxa(token: string): Observable<any> {
-    return this.lajiApi.get(LajiApi.Endpoints.autocomplete, 'taxon', {
-      q: token,
-      includePayload: true,
-      limit: '' + this.typeaheadLimit,
-      lang: this.translate.currentLang,
+  public getTaxa(query: string) {
+    return this.api.get('/autocomplete/taxa', { query: {
+      query,
+      limit: this.typeaheadLimit,
       ...this.searchParams
-    });
+    } }).pipe(map(({results}) => results));
   }
 }

commit ddcb482575e41e4f85ed1f1b925fe2bbad3bb695

fix taxon-autocomplete matchType (regression from 74715ac0)

projects/laji/src/app/shared-modules/taxon-autocomplete/taxon-autocomplete.component.ts
@@ -121,7 +121,7 @@ export class TaxonAutocompleteComponent implements AfterViewInit, OnDestroy {
       switchMap(() => this.api.get('/autocomplete/taxa', { query: {
         query,
         limit: this.limit,
-        matchType: 'partial',
+        matchType: 'exact,partial',
         informalTaxonGroup: this.informalTaxonGroup,
         excludeNameTypes: this.excludeNameTypes,
         onlyFinnish: this.onlyFinnish,