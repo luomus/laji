5650d10c6d0d5c53f1e195b4b2093adc0ac1b428
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2025-11-10 15:01:56 +0200

    improve trait view styles

--- projects/laji/src/app/+trait-db/trait-db-traits/trait-db-trait/trait-db-trait.component.ts
+++ projects/laji/src/app/+trait-db/trait-db-traits/trait-db-trait/trait-db-trait.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { components } from 'projects/laji-api-client-b/generated/api';
 import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
@@ -9,7 +9,9 @@ import { UserService } from '../../../shared/service/user.service';
 export type Trait = components['schemas']['Trait'];
 
 @Component({
-  templateUrl: './trait-db-trait.component.html'
+  templateUrl: './trait-db-trait.component.html',
+  styleUrls: ['./trait-db-trait.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TraitDbTraitComponent implements OnInit {
   trait$!: Observable<Trait>;
@@ -31,4 +33,3 @@ export class TraitDbTraitComponent implements OnInit {
     this.loggedIn$ = this.userService.isLoggedIn$;
   }
 }
-
