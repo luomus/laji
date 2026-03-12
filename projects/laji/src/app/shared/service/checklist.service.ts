cc3c59f9465e7c35e2c8d780feada74e99221afc
aorin <meeri.rannisto@gmail.com>
2026-03-12 12:36:07 +0200

    refactor checklist service to use api v1

--- projects/laji/src/app/shared/service/checklist.service.ts
+++ projects/laji/src/app/shared/service/checklist.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
-import { LajiApi, LajiApiService } from './laji-api.service';
 import { map } from 'rxjs';
 import { AbstractCachedHttpService } from './abstract-cached-http.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Checklist } from '../model/Checklist';
+import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
 
 @Injectable({providedIn: 'root'})
 export class ChecklistService extends AbstractCachedHttpService<Checklist> {
 
   constructor(
-    private lajiApi: LajiApiService,
+    private api: LajiApiClientBService,
     private translate: TranslateService
   ) {
     super('name');
   }
 
   getAllAsLookUp(lang: string = this.translate.getCurrentLang()) {
-    return this.fetchLookup(this.lajiApi.getList(LajiApi.Endpoints.checklists, {lang, page: 1, pageSize: 1000}).pipe(
+    return this.fetchLookup(this.api.get('/checklists', { query: { page: 1, pageSize: 1000 } }).pipe(
       map(paged => paged.results),
     ), lang);
   }
