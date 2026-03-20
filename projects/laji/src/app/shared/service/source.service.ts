922c9be443f521bbd4b43cb29c76fe263499a6d3
rpulkka <robert.pulkka@gmail.com>
2026-03-20 11:28:39 +0200

    Use TranslateService in SourceService.

--- projects/laji/src/app/shared/service/source.service.ts
+++ projects/laji/src/app/shared/service/source.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LajiApiClientBService } from 'projects/laji-api-client-b/src/laji-api-client-b.service';
+import { TranslateService } from '@ngx-translate/core';
 import { map } from 'rxjs';
 import { AbstractCachedHttpService } from './abstract-cached-http.service';
 
@@ -8,14 +9,14 @@ import { AbstractCachedHttpService } from './abstract-cached-http.service';
 @Injectable({providedIn: 'root'})
 export class SourceService extends AbstractCachedHttpService<string> {
 
-  constructor(private api: LajiApiClientBService) {
+  constructor(
+    private api: LajiApiClientBService,
+    private translate: TranslateService
+  ) {
     super('name');
   }
 
-  getAllAsLookUp(lang?: string): Observable<{[id: string]: string}> {
-    if (!lang) {
-      lang = this.currentLang || 'fi';
-    }
+  getAllAsLookUp(lang: string = this.translate.getCurrentLang()): Observable<{[id: string]: string}> {
     return this.fetchLookup(this.api.get('/sources', { query: { page: 1, pageSize: 1000 } }).pipe(
       map(paged => paged.results)
     ), lang);
